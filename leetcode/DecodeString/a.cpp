5f24fd5e4f7bbb9a5f36ce3ae6911295eb5abcb9
David Sun <iamslash@gmail.com>
2020-11-27 06:57:21 +0900

    Accept DecodeString

--- leetcode/DecodeString/a.cpp
+++ leetcode/DecodeString/a.cpp
@@ -1,43 +1,41 @@
-/* Copyright (C) 2018 by iamslash */
+/* Copyright (C) 2020 by iamslash */
 
 #include <cstdio>
 #include <string>
 #include <stack>
+#include <map>
 
-// 0ms 100.00%
+using namespace std;
+
+//                     i
+//    s: 3 [ a 2 [ c ] ]
+//  num: 
+// stck: {0, accaccacc}
+
+// 0ms 100.00% 6.9MB 50.07%
+// stack
+// O(N) O(N)
 class Solution {
  public:
-  std::string decodeString(std::string s) {
-    std::stack<std::pair<int, std::string>> stck;
+  string decodeString(string s) {
+    stack<pair<int, string>> stck;
     stck.push({0, ""});
-    std::string k = "";
-    for (int i = 0; i < s.size(); ++i) {
-      if (s[i] >= '0' && s[i] <= '9') {
-        k += s[i];
-      } else if (s[i] == '[') {
-        stck.push({std::stoi(k), ""});
-        k = "";
-      } else if (s[i] == ']') {
-        auto pr1 = stck.top(); stck.pop();
-        auto& pr2 = stck.top();
-        for (int i = 0; i < pr1.first; ++i) {
-          pr2.second += pr1.second;
-        }        
+    string num;
+    for (char c : s) {
+      if (c >= '0' && c <= '9') {
+        num += c;
+      } else if (c == '[') {
+        stck.push({stoi(num), ""});
+        num = "";
+      } else if (c == ']') {
+        auto pr = stck.top(); stck.pop();
+        for (int i = 0; i < pr.first; ++i) {
+          stck.top().second += pr.second;
+        }
       } else {
-        auto& pr = stck.top();
-        pr.second += s[i];
+        stck.top().second += c;
       }
     }
-
     return stck.top().second;
   }
 };
-
-int main() {
-  Solution sln;
-  printf("%s\n", sln.decodeString("3[a]2[bc]").c_str());
-  printf("%s\n", sln.decodeString("3[a2[c]]").c_str());
-  printf("%s\n", sln.decodeString("2[abc]3[cd]ef").c_str());
-  
-  return 0;
-}
