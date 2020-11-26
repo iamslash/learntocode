386e3b4b57d7b3bd073c02e9cf955a0d491e7c22
David Sun <iamslash@gmail.com>
2020-11-26 09:21:59 +0900

    Accept DesignBrowserHistory

--- leetcode2/DesignBrowserHistory/20201125.Solution.java
+++ leetcode2/DesignBrowserHistory/20201125.Solution.java
@@ -2,32 +2,33 @@
 
 import java.util.*;
 
-// 49ms 44.56% 47MB 78.14%
+// 1 2 3 4
+
+// 99ms 40.88% 111.5MB 100.00%
 // linked list
-//   visit: O(1) O(N)
-//    back: O(1) O(N)
-// forward: O(1) O(N)
+//   visit: O(1) O(N);
+//    back: O(1) O(N);
+// forward: O(1) O(N);
 class BrowserHistory {
 	private List<String> history = new LinkedList<>();
 	int pos = 0;
 	public BrowserHistory(String homepage) {
 		history.add(homepage);
-		pos = 0;
 	}
     
 	public void visit(String url) {
 		history.subList(pos+1, history.size()).clear();
 		history.add(url);
-		pos++;
+		++pos;
 	}
     
 	public String back(int steps) {
-		pos = Math.max(0, pos-steps);
+		pos = Math.max(0, pos - steps);
 		return history.get(pos);
 	}
     
 	public String forward(int steps) {
-		pos = Math.min(history.size()-1, pos+steps);
+		pos = Math.min(history.size() - 1, pos + steps);
 		return history.get(pos);
 	}
 }
