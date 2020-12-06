309b482a8e86a93cfcc40d97a78a3403528dc702
David Sun <iamslash@gmail.com>
2020-12-06 10:18:33 +0900

    Accept WordBreak

---
+++ leetcode2/WordBreak/README.md
@@ -0,0 +1,36 @@
+# Problem
+
+[Word Break](https://leetcode.com/problems/word-break/)
+
+문자열 `s` 가 주어진다. 문자열의 모음 `W[]` 가 주어진다. 
+`s` 가 `W[]` 의 문자열들로 구성되는지 검증하라.
+
+# Idea
+
+[CoinChange @ learntocode](/leetcode/CoinChange/README.md) 와
+비슷하다. `s` 가 총 금액이고 `W[]` 가 동전의 모음이라고 생각하고
+접근하자.
+
+먼저 `vector<bool> C(n+1)` 를 선언하여 `C[i]` 에 `s[i]` 로 시작하는
+문자열이 `W[]` 로 구성될 수 있는지를 저장한다. `C[n]` 은 seed value
+이기 때문에 `true` 로 초기화 한다.
+
+인덱스 `i` 를 `[n-1..0]` 동안 순회하면서 다음을 반복한다. 
+
+* `W[]` 를 `word` 로 순회하면서 다음을 반복한다.
+  * `i + word.size() <= n` 이고 `C[i + word.size()]` 이고
+    `strncmp(&s[i], word.c_str(), word.size()) == 0` 이면 `C[i] =
+    true` 를 수행한다. 그리고 더이상 `W[]` 를 순회할 이유가
+    없다. 반복을 멈춘다.
+
+모든 반복을 마치면 `C[0]` 이 답이다.
+
+# Implementation
+
+* [c++11](a.cpp)
+
+# Complexity
+
+```
+O(SW) O(S)
+```
--- leetcode2/WordBreak/a.cpp
+++ leetcode2/WordBreak/a.cpp
@@ -1,84 +1,32 @@
-/* Copyright (C) 2018 by iamslash */
+/* Copyright (C) 2020 by iamslash */
 
 #include <cstdio>
-#include <string>
 #include <vector>
+#include <string>
+#include <cstring>
 
-//           i
-// s: leetcode
-// D: leet code
-//
-// Iterative way
-// 4ms 84.58%
-// class Solution {
-//  public:
-//   bool wordBreak(std::string s, std::vector<std::string>& D) {
-//     int n = s.size();
-//     std::vector<bool> C(n+1, false);
-//     C[n] = true;
+using namespace std;
 
-//     for (int i = n-1; i >= 0; --i) {
-//       for (const std::string& d : D) {
-//         if ((i + d.size() <= n) &&
-//             (s.substr(i, d.size()).compare(d) == 0)) {
-//           C[i] = C[i+d.size()];
-//           if (C[i])
-//             break;
-//         }
-//       }
-//     }
-//     return C[0];
-//   }
-// };
-
-// Recursive way
-// 0ms 100.00%
+// 4ms 95.66% 7.9MB 94.70%
+// iterative dynamic programming
+// O(SW) O(S)
 class Solution {
- private:
-  std::vector<bool> m_C;
-  bool solve(std::string s, std::vector<std::string>& D, int i) {
-    for (const auto& word : D) {
-      int j = i + word.size();
-      if (j > s.size())
-        continue;
-      bool match = true;
-      for (int k = 0; k < word.size(); ++k) {
-        if (s[i+k] != word[k]) {
-          match = false;
+ public:
+  bool wordBreak(string s, vector<string>& wordDict) {
+    // C[i]: can be segmented from index i
+    int n = s.size();
+    vector<bool> C(n+1, false);
+    C[n] = true;
+    for (int i = n-1; i >= 0; --i) {
+      for (string& word : wordDict) {
+        if (i + word.size() <= n &&
+            C[i + word.size()] &&
+            !strncmp(&s[i], word.c_str(), word.size())) {
+          C[i] = true;
           break;
         }
       }
-      if (match) {
-        if (j == s.size())
-          return true;
-        if (!m_C[j] && solve(s, D, j))
-          return true;
-      }
     }
-    m_C[i] = true;
-    return false;
-  }
-        
- public:
-  bool wordBreak(std::string s, std::vector<std::string>& D) {
-    m_C.resize(s.size(), false);
-    return solve(s, D, 0);
+    return C[0];
   }
 };
-
-void printb(bool b) {
-  printf("%s\n", b ? "true" : "false");
-}
-
-int main() {
-
-  std::string s = "leetcode";
-  std::vector<std::string> D = {"leet", "code"};
-  // std::string s = "applepenapple";
-  // std::vector<std::string> D = {"apple", "pen"};
-
-  Solution sln;
-  printb(sln.wordBreak(s, D));
-  
-  return 0;
-}
