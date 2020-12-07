6dc81c8944a3f3931b2bd5f36adad4da7bbc06f7
David Sun <iamslash@gmail.com>
2020-12-07 09:27:24 +0900

    Add ConcatenationofConsecutiveBinaryNumbers

---
+++ ConcatenationofConsecutiveBinaryNumbers/a.cpp
@@ -0,0 +1,19 @@
+/* Copyright (C) 2020 by iamslash */
+
+#include <cstdio>
+
+// n: 3
+//    1 10 11
+//
+//
+// n: 12
+//    1 10 11 100 101 110 111 1000
+
+using namespace std;
+
+class Solution {
+ public:
+  int concatenatedBinary(int n) {
+    
+  }
+};
