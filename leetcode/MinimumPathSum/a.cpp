996ba2437d0460e1327b6ef1bed72c85a30aadc9
David Sun <iamslash@gmail.com>
2020-12-20 08:31:51 +0900

    Accept MinimumPathSum

--- leetcode/MinimumPathSum/a.cpp
+++ leetcode/MinimumPathSum/a.cpp
@@ -1,56 +1,29 @@
-/* Copyright (C) 2018 by iamslash */
-// https://leetcode.com/problems/minimum-path-sum/description/
+/* Copyright (C) 2020 by iamslash */
 
 #include <cstdio>
 #include <vector>
-#include <limits>
+#include <cstdlib>
 
-// 
+using namespace std;
+
+// 8ms 99.98% 9.9MB 91.07%
+// iterative dynamic programming
+// O(HW) O(1)
 class Solution {
  public:
-  std::vector<std::vector<int>> m_C;
-  std::vector<std::vector<int>> m_B;
-  int m_sizey;
-  int m_sizex;
-  int _trav(int y, int x) {
-    // base condition
-    if (y == m_sizey - 1 &&
-        x == m_sizex - 1)
-      return m_B[y][x];
-    else if (y >= m_sizey ||
-             x >= m_sizex)
-      return std::numeric_limits<int>::max();
-    int& r = m_C[y][x];
-    if (r >= 0)
-      return r;
-
-    // recursive condition
-    r = m_B[y][x] + std::min(_trav(y + 1, x), _trav(y, x + 1));
-    if (r < 0)
-      r = std::numeric_limits<int>::max();
-    return r;
-  }
-  int minPathSum(std::vector<std::vector<int>>& B) {
-    if (B.empty() || B[0].empty())
-      return 0;
-    m_sizey = B.size();
-    m_sizex = B[0].size();
-    m_B = B;
-    m_C = std::vector<std::vector<int>>(m_sizey,
-            std::vector<int>(m_sizex, -1));
-    return _trav(0, 0);
+  int minPathSum(vector<vector<int>>& G) {
+    int h = G.size(), w = G[0].size();
+    for (int y = 0; y < h; ++y) {
+      for (int x = 0; x < w; ++x) {
+        if (y > 0 && x > 0) {
+          G[y][x] += min(G[y-1][x], G[y][x-1]);
+        } else if (y > 0) {
+          G[y][x] += G[y-1][x];
+        } else if (x > 0) {
+          G[y][x] += G[y][x-1];
+        }
+      }
+    }
+    return G[h-1][w-1];
   }
 };
-
-int main() {
-  std::vector<std::vector<int>> B = {
-    {1, 3, 1},
-    {1, 5, 1},
-    {4, 2, 1},
-  };
-  
-  Solution s;
-  printf("%d\n", s.minPathSum(B));
-  
-  return 0;
-}
