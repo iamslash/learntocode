5b006b5bfc4664d2daef11d52edc7074a33faf93
David Sun <iamslash@gmail.com>
2020-12-23 07:39:22 +0900

    Accept JumpGameVI

--- leetcode2/JumpGameVI/a.cpp
+++ leetcode2/JumpGameVI/a.cpp
@@ -7,11 +7,12 @@
 using namespace std;
 
 //    k: 2
-//               i
+//                  i
 // nums: 1 -1 -2 4 -7 3
 //                    j
-//    C: 1  0 -1 5 -2 8
+//    C: 1  0 -1 4 -3 7
 
+// 188ms 93.67% 58.5MB 76.50%
 // iterative dynamic programming
 // O(N^2) O(N)
 class Solution {
@@ -21,9 +22,9 @@ class Solution {
     vector<int> C(n, INT_MIN);
     C[0] = nums[0];
     for (int i = 0; i < n; ++i) {
-      for (int j = 1; j <= k && i + j < n; ++j) {
-        C[i+j] = max(C[i+j], C[i] + nums[i+j]);
-        if (nums[i+j] >= 0)
+      for (int j = i+1; j <= min(i+k, n-1); ++j) {
+        C[j] = max(C[j], C[i] + nums[j]);
+        if (nums[j] >= 0)
           break;
       }
     }
@@ -31,8 +32,17 @@ class Solution {
   }
 };
 
-// mono deque
-// O(N) O(N)
+
+//    k: 2
+// nums: 1 -1 -2 4 -7 3
+//                      i
+//    C: 1  0 -2 2  5 7
+//       
+//  deq: 5
+
+// 212ms 80.18% 57.1MB 86.44%
+// monotone decreasing deque, sliding window
+// O(N) O(K)
 class Solution {
  public:
   int maxResult(vector<int>& nums, int k) {
@@ -42,7 +52,7 @@ class Solution {
       if (deq.front() + k < i) {
         deq.pop_front();
       }
-      nums[i] += nums[d.front()];
+      nums[i] += nums[deq.front()];
       while (!deq.empty() && nums[deq.back()] <= nums[i]) {
         deq.pop_back();
       }
