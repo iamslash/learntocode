c05890f759511d03318887a1cb4dc37be2c19643
David Sun <iamslash@gmail.com>
2020-12-23 20:30:55 +0900

    Add CheckingExistenceofEdgeLengthLimitedPaths

---
+++ leetcode2/CheckingExistenceofEdgeLengthLimitedPaths/a.cpp
@@ -0,0 +1,68 @@
+/* Copyright (C) 2020 by iamslash */
+
+#include <cstdio>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
+// Disjoint Set
+// O(N) O(N)
+class DisjointSet {
+ private:  
+  vector<int> parent;
+ public:
+  DisjointSet(int n) : parent(n) {
+    iota(parent.begin(), parent.end(), 0);
+  }
+  void merge(int u, int v) {
+    u = find(u);
+    v = find(v);
+    if (u == v)
+      return;
+    parent[u] = v;
+  }
+  bool connected(int u, int v) {
+    return find(u) == find(v);
+  }
+  int find(int u) {
+    if (parent[u] == u)
+      return u;
+    return parent[u] = find(parent[u]);
+  }  
+};
+class Solution {
+ public:
+  vector<bool> distanceLimitedPathsExist(
+      int n,
+      vector<vector<int>>& edgeList,
+      vector<vector<int>>& queries) {
+    vector<bool> ans(queries.size());
+    for (int i = 0; i < queries.size(); ++i) {
+      queries[i].push_back(i);
+    }
+    sort(queries.begin(), queries.end(),
+         [&](vector<int>& a, vector<int>& b) {
+           return a[2] < b[2];
+         });
+    sort(edgeList.begin(), edgeList.end(),
+         [&](vector<int>& a, vector<int>& b) {
+           return a[2] < b[2];
+         });
+    DisjointSet ds(n);
+    int j = 0;
+    for (auto& q : queries) {
+      int u = q[0], v = q[1], w = q[2];
+      for (; j < edgeList.size(); ++j) {
+        auto& e = edgeList[j];
+        int x = e[0], y = e[1], z = e[2];
+        if (z >= w)
+          break;
+        ds.merge(x, y);
+      }
+      ans[q[3]] = ds.connected(u, v);
+    }
+    return ans;
+  }
+};
