39612c184a1521bd89418cfd6170346f64d7bd5b
David Sun <iamslash@gmail.com>
2021-01-01 08:08:49 +0900

    Add MaximumBinaryStringAfterChange

---
+++ leetcode2/MaximumBinaryStringAfterChange/a.cpp
@@ -0,0 +1,17 @@
+/* Copyright (C) 2021 by iamslash */
+
+#include <cstdio>
+
+// 00 -> 10
+// 10 -> 01
+
+// 000110
+// 110010
+// 111010  
+
+class Solution {
+ public:
+  string maximumBinaryString(string s) {
+    
+  }
+};
