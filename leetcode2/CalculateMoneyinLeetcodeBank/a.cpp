6e53ef621e6c2bb09e1104622992c4be958c8adc
David Sun <iamslash@gmail.com>
2021-01-11 07:34:14 +0900

    Add CalculateMoneyinLeetcodeBank

---
+++ leetcode2/CalculateMoneyinLeetcodeBank/a.cpp
@@ -0,0 +1,15 @@
+/* Copyright (C) 2021 by iamslash */
+
+#include <cstdio>
+
+// 1 2 3 4 5 6  7 : 21
+// 2 3 4 5 6 7  8 : 21 + 7
+// 3 4 5 6 7 8  9 : 21 + 7 + 7
+// 4 5 6 7 8 9 10 : 21 + 7 + 7 + 7
+
+class Solution {
+ public:
+  int totalMoney(int n) {
+    
+  }
+};
