8a3883927001dbf94453410676b48aefb131b139
David Sun <iamslash@gmail.com>
2021-01-15 07:41:36 +0900

    AddConstructtheLexicographicallyLargestValidSequence

---
+++ ConstructtheLexicographicallyLargestValidSequence/Solution.java
@@ -0,0 +1,18 @@
+// Copyright (C) 2021 by iamslash
+
+// n: 1
+//    1
+// n: 2
+//    2 1 2
+// n: 3
+//    3 1 2 3 2
+// n: 4
+//     
+// n: 5
+//    5 3 1 4 3 5 2 4 2  
+
+class Solution {
+	public int[] constructDistancedSequence(int n) {
+		
+	}
+}
