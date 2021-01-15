0a7f589b3a624855bea4e5da1c8e2bad6d7aa797
David Sun <iamslash@gmail.com>
2021-01-16 08:02:45 +0900

    Add ConstructtheLexicographicallyLargestValidSequence

--- ConstructtheLexicographicallyLargestValidSequence/Solution.java
+++ ConstructtheLexicographicallyLargestValidSequence/Solution.java
@@ -12,7 +12,24 @@
 //    5 3 1 4 3 5 2 4 2  
 
 class Solution {
+	private int[] best;
+	private int[] comb;
+	private bool dfs(int n, int i) {
+		// base
+		if (i >= n) {
+			best = comb;
+			return true;
+		}
+		// recursion
+		if (i == 0) {			
+			return dfs(n, i+1);
+		}
+		return false;
+	}
 	public int[] constructDistancedSequence(int n) {
-		
+		best = new int[n];
+		comb = new int[n];
+		dfs(n, 0);
+		return best;
 	}
 }
