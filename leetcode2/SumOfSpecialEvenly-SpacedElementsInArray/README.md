af8ec756c5c944d72d4a71e3a218749e97b0972f
David Sun <iamslash@gmail.com>
2021-01-21 07:21:21 +0900

    Accept SumOfSpecialEvenly-SpacedElementsInArray

---
+++ leetcode2/SumOfSpecialEvenly-SpacedElementsInArray/README.md
@@ -0,0 +1,22 @@
+# Problem
+
+[Sum Of Special Evenly-Spaced Elements In Array](https://leetcode.com/problems/sum-of-special-evenly-spaced-elements-in-array/)
+
+수열 `nums[], queries[][]` 가 주어진다. `queries[i] = [x_i, y_i]` 라고
+하자. `ans[]` 에 답을 저장한다. `ans[i]` 는 `sum(nums[j])` 과 같다.
+이때 `x_i <= j < n` 이고 `(j - x_i) % y_i = 0` 이다. `ans[]` 의 길이는
+`queries[][]` 의 길이와 같다. `ans` 를 구하라.
+
+# Idea
+
+too difficult
+
+# Implementation
+
+* [java8](Solution.java)
+
+# Complexity
+
+```
+O(NlgN) O(N)
+```
--- leetcode2/SumOfSpecialEvenly-SpacedElementsInArray/Solution.java
+++ leetcode2/SumOfSpecialEvenly-SpacedElementsInArray/Solution.java
@@ -1,18 +1,59 @@
 // Copyright (C) 2021 by iamslash
 
-// Time Limit Exceeded
+import java.util.*;
+
+// // Time Limit Exceeded
+// // brute force
+// // O(NQ) O(Q)
+// class Solution {
+// 	private final int MOD = 1000000007;
+// 	public int[] solve(int[] nums, int[][] queries) {		
+// 		int[] ans = new int[queries.length];
+// 		int n = nums.length;
+// 		for (int i = 0; i < queries.length; ++i) {
+// 			int u = queries[i][0];
+// 			int v = queries[i][1];
+// 			for (int j = 0; u + j * v < n; ++j) {
+// 				ans[i] = (ans[i] + nums[u + j * v]) % MOD;
+// 			}
+// 		}
+// 		return ans;
+// 	}
+// }
+
+// What's the meaning of y*1l*y ???
+// What's the difference between y*y*1l and y*1l*y ???
+// 207ms 79.46% 116MB 75.00%
+// dynamic programming
+// O(NlgN) O(N)
 class Solution {
 	private final int MOD = 1000000007;
-	public int[] solve(int[] nums, int[][] queries) {		
-		int[] ans = new int[queries.length];
-		int n = nums.length;
-		for (int i = 0; i < queries.length; ++i) {
-			int u = queries[i][0];
-			int v = queries[i][1];
-			for (int j = 0; u + j * v < n; ++j) {
-				ans[i] = (ans[i] + nums[u + j * v]) % MOD;
+	public int[] solve(int[] nums, int[][] queries) {
+		int n = nums.length, m = queries.length;
+		int[] ans = new int[m];
+		int sqrtn = (int)Math.sqrt(n) + 1;
+		int[][] C = new int[sqrtn][n];
+		for (int i = 1; i*i <= n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (j < i) {
+					C[i][n-j-1] = nums[n-j-1];
+				} else {
+					C[i][n-j-1] = (C[i][n-j-1+i] + nums[n-j-1]) % MOD;
+				}
+			}
+		}
+		for (int i = 0; i < m; ++i) {
+			int x = queries[i][0];
+			int y = queries[i][1];
+			// caveats: overflow
+			if (1l*y*y <= n) {
+				ans[i] = C[y][x];
+			} else {
+				for (int j = x; j < n; j += y) {
+					ans[i] = (ans[i] + nums[j]) % MOD;
+				}
 			}
 		}
 		return ans;
 	}
-}
+};
