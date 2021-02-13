de84cd357f9c4b84c702649538031b40f88bdfc3
David Sun <iamslash@gmail.com>
2021-02-13 21:03:42 +0900

    Add CheapestFlightsWithinKStops

--- leetcode/CheapestFlightsWithinKStops/Solution.java
+++ leetcode/CheapestFlightsWithinKStops/Solution.java
@@ -1,35 +1,13 @@
-// Copyright (C) 2020 by iamslash
+// Copyright (C) 2021 by iamslash
 
 import java.util.*;
 
-
-// 6ms 82.97% 40MB 5.64%
-// dijkstra algorithm
-// O(ElgV) O(V)
+// BFS
+// O(N) O(N)
 class Solution {
-	public int findCheapestPrice(int n, int[][] F, int src, int dst, int K) {
-		int[][] G = new int[n][n];
-		for (int[] flight : F) {
-			int u = flight[0];
-			int v = flight[1];
-			int d = flight[2];
-			G[u][v] = d;
-		}
-		Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-		pq.add(new int[]{0, src, K+1});
-		while (pq.size() > 0) {
-			int[] top = pq.poll();
-			int dist = top[0], u = top[1], ttl = top[2];
-			if (u == dst)
-				return dist;
-			if (ttl <= 0)
-				continue;
-			for (int v = 0; v < n; ++v) {
-				if (G[u][v] == 0)
-					continue;
-				pq.add(new int[]{G[u][v] + dist, v, ttl - 1});
-			}
-		}
-		return -1;
+	public int findCheapestPrice(
+			int n, int[][] flights,
+			int src, int dst, int K) {
+		
 	}
 }
