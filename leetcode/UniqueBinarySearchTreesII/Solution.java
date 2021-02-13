9cc51876015c80e738949c0e22f5f31e6f4bfc9c
David Sun <iamslash@gmail.com>
2021-02-13 20:22:01 +0900

    Accept UniqueBinarySearchTreesII

--- leetcode/UniqueBinarySearchTreesII/Solution.java
+++ leetcode/UniqueBinarySearchTreesII/Solution.java
@@ -1,8 +1,8 @@
-// Copyright (C) 2020 by iamslash
+// Copyright (C) 2021 by iamslash
 
 import java.util.*;
 
-public class TreeNode {
+class TreeNode {
 	int val;
 	TreeNode left;
 	TreeNode right;
@@ -15,35 +15,40 @@ public class TreeNode {
 	}
 }
 
-// 0ms 100.00% 39.4MB 8.97%
-// DFS
-// O(N) O(lgN)
+// i
+// 1 2 3 4 5
+
+// 0ms 100.00% 39.7MB 58.33%
+// dfs
+// O(N) O(N)
 class Solution {
 	private List<TreeNode> dfs(int fr, int to) {
 		List<TreeNode> ans = new ArrayList<>();
+		// base
+		if (fr > to) {
+			ans.add(null);
+			return ans;
+		}
 		if (fr == to) {
 			ans.add(new TreeNode(fr));
-		} else if (fr > to) {
-			ans.add(null);
-		} else {
-			for (int i = fr; i <= to; ++i) {
-				List<TreeNode> l = dfs(fr, i-1);
-				List<TreeNode> r = dfs(i+1, to);
-				for (int j = 0; j < l.size(); ++j) {
-					for (int k = 0; k < r.size(); ++k) {
-						TreeNode t = new TreeNode(i);
-						t.left = l.get(j);
-						t.right = r.get(k);
-						ans.add(t);
-					}
+			return ans;
+		}
+		// recursion
+		for (int i = fr; i <= to; ++i) {
+			List<TreeNode> l = dfs(fr, i-1);
+			List<TreeNode> r = dfs(i+1, to);
+			for (int j = 0; j < l.size(); ++j) {
+				for (int k = 0; k < r.size(); ++k) {
+					TreeNode t = new TreeNode(i);
+					t.left = l.get(j);
+					t.right = r.get(k);
+					ans.add(t);
 				}
-			}
+			}					
 		}
 		return ans;
 	}
 	public List<TreeNode> generateTrees(int n) {
-		if (n == 0)
-			return new ArrayList<>();
 		return dfs(1, n);
 	}
 }
