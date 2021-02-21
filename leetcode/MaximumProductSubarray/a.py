2cf65821b043cd8bc3d47b9657032ca265f0ba33
David Sun <iamslash@gmail.com>
2021-02-21 14:50:45 +0900

    Accept MaximumProductSubarray

--- leetcode/MaximumProductSubarray/a.py
+++ leetcode/MaximumProductSubarray/a.py
@@ -1,32 +1,26 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2020 by iamslash 
+# Copyright (C) 2021 by iamslash
 
 from typing import List
+import sys
 
-# 60ms 37.32% 14.4MB 24.66%
-# kadane's algorithm
+# 60ms 38.08% 14.2MB 95.93%
+# iterative dynamic programming
 # O(N) O(1)
 class Solution:
-    def maxProduct(self, A: List[int]) -> int:
-        last_min = A[0]
-        last_max = A[0]
-        glb_max = A[0]
-        for i in range(1, len(A)):
-            t_min = min(last_min * A[i], last_max * A[i])
-            t_max = max(last_min * A[i], last_max * A[i])
-            last_min = min(A[i], t_min)
-            last_max = max(A[i], t_max)
-            glb_max = max(glb_max, last_max)
+    def maxProduct(self, nums: List[int]) -> int:
+        glb_max, loc_max, loc_min = nums[0], nums[0], nums[0]
+        for num in nums[1:]:
+            tmp_max = max(loc_min * num, loc_max * num)
+            tmp_min = min(loc_min * num, loc_max * num)
+            loc_max = max(tmp_max, num)
+            loc_min = min(tmp_min, num)
+            glb_max = max(glb_max, loc_max)
         return glb_max
-
-# 48ms 94.52% 15MB 23.66%
-# prefix sum
-# O(N) O(N)
-class Solution:
-    def maxProduct(self, A: List[int]) -> int:
-        # reversed from A
-        R = A[::-1]
-        for i in range(1, len(A)):
-            A[i] *= A[i-1] or 1
-            R[i] *= R[i-1] or 1
-        return max(A + R)
+                          
+if __name__ == "__main__":
+    sln = Solution()
+    # 6
+    print(sln.maxProduct([2, 3, -2, 4]))
+    # 0
+    print(sln.maxProduct([-2, 0, -1]))
