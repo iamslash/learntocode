9c3c097c82797a75803f7ea22bdb145eef3a138e
David Sun <iamslash@gmail.com>
2021-02-23 08:36:06 +0900

    Accept MinimumNumberofOperationstoMoveAllBallstoEachBox

---
+++ leetcode2/MinimumNumberofOperationstoMoveAllBallstoEachBox/README.md
@@ -0,0 +1,25 @@
+# Problem
+
+[Minimum Number of Operations to Move All Balls to Each Box](https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/)
+
+문자열 `boxes[]` 가 주어진다. `boxes[i]` 는 `0` 혹은 `1` 이다. `1` 은
+공이 하나 있다는 의미이다. 공을 한칸씩 움직여서 `boxes[i]` 로 모든
+공을 옮기자. `ans[]` 를 선언하고 `ans[i]` 에 `boxes[i]` 로
+옮긴 최소횟수를 저장하자.
+
+# Idea
+
+
+
+```
+```
+
+# Implementation
+
+* [py3](a.py)
+
+# Complexity
+
+```
+O(N) O(N)
+```
--- leetcode2/MinimumNumberofOperationstoMoveAllBallstoEachBox/a.py
+++ leetcode2/MinimumNumberofOperationstoMoveAllBallstoEachBox/a.py
@@ -5,14 +5,27 @@ from typing import List
 
 #          i
 # boxes: 0 0 1 0 1  1
-#   ops: 0
-#   cnt: 0
+#   ops: 0 0
+#   cnt: 0 0
 #   ans: 0 0
 
-# partial sum
+# 92ms 100.00% 14.5MB 100.00%
+# back and forth
+# O(N) O(N)
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        pass
+        n, ops, cnt = len(boxes), 0, 0
+        ans = [0]*n
+        for i in range(0, n):
+            ans[i] = ops
+            cnt += 1 if boxes[i] == '1' else 0
+            ops += cnt
+        ops, cnt = 0, 0
+        for i in range(n-1, -1, -1):
+            ans[i] += ops
+            cnt += 1 if boxes[i] == '1' else 0
+            ops += cnt
+        return ans
         
 if __name__ == "__main__":
     sln = Solution()
