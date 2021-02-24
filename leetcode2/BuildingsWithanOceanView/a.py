0fc2381407f9744d707ae9178dc69c2468f868f5
David Sun <iamslash@gmail.com>
2021-02-25 07:19:28 +0900

    Accept BuildingsWithanOceanView

--- leetcode2/BuildingsWithanOceanView/a.py
+++ leetcode2/BuildingsWithanOceanView/a.py
@@ -1,19 +1,33 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2021 by iamslash
 
+# heights: 4 2 3 1
+#          i
+#          1 3 2 4
+#          0
+#    maxH: 0
+#     ans: 3
+
 from typing import List
 
+# 704ms 100.00% 31.1MB 100.00%
 # greedy algorithm
 # O(N) O(N)
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         ans = []
-        maxHeight = 0
-        for i, v in heights[::-1]:
+        maxHeight, n = 0, len(heights)
+        for i, v in enumerate(heights[::-1]):
             if v > maxHeight:
-                ans.append(i)
-        return ans[::-1]                
+                ans.append(n-1-i)
+                maxHeight = v
+        return ans[::-1]
                 
 if __name__ == "__main__":
     sln = Solution()
+    # [0,2,3]
+    print(sln.findBuildings([4,2,3,1]))
+    # [0,1,2,3]
     print(sln.findBuildings([4,3,2,1]))
+    # [3]
+    print(sln.findBuildings([1,3,2,4]))
