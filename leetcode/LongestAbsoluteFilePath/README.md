02e54a925d6783106f463271f32437ccb532d51c
David Sun <iamslash@gmail.com>
2021-02-27 08:07:00 +0900

    LongestAbsoluteFilePath ing...

---
+++ leetcode/LongestAbsoluteFilePath/README.md
@@ -0,0 +1,23 @@
+# Problem
+
+[Longest Absolute File Path](https://leetcode.com/problems/longest-absolute-file-path/)
+
+문자열 `input` 이 주어진다. `input` 은 영문 소문자와 숫자 그리고 `\n`,
+`\t` 로만 구성되어 있다. `\t` 의 개수는 디렉토리의 깊이를
+의미한다. 경로의 끝은 확장자가 없으면 디렉토리이고 있으면
+파일이다.파일의 경로중 가장 길이가 긴 것의 길이를 구하라.
+
+# Idea
+
+
+
+# Implementation
+
+* [c++11](a.cpp)
+* [py3](a.py)
+
+# Complexity
+
+```
+O(N) O(N)
+```
---
+++ leetcode/LongestAbsoluteFilePath/a.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 by iamslash
+
+#         
+#   input: dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext
+#                                                i
+#          dir  \tsubdir1  \tsubdir2  \t\tfile.ext
+#
+#    name: dir    subdir1    subdir2      file.ext
+#     lvl:   0          1          1             2
+#
+#  maxlen: 0
+# pathlen: 0 1 2 
+#          0 4 12
+
+class Solution:
+    def lengthLongestPath(self, input: str) -> int:
+        maxlen = 0
+        # {lvl: len}
+        pthlen = {0: 0}
+        for line in input.splitlines():
+            name = line.lstrip('\t')
+            lvl  = len(line) - len(name)
+            if '.' in name:
+                maxlen = max(maxlen, pathlen[lvl] + len(name))
+            else:
+                pathlen[lvl+1] = pathlen[lvl] + len(name) + 1
+        return maxlen
+        
+if __name__ == "__main__":
+    sln = Solution()
+    # 20
+    print(sln.lengthLongestPath("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"))
+    # 32
+    print(sln.lengthLongestPath("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"))
+    # 0
+    print(sln.lengthLongestPath("a"))
+    # 12
+    print(sln.lengthLongestPath("file1.txt\nfile2.txt\nlongfile.txt"))
