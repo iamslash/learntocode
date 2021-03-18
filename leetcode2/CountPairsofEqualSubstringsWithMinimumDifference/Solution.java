f4eca8b6f617a9ec0fad16487229c895b79129f1
David Sun <iamslash@gmail.com>
2021-03-19 07:51:01 +0900

    Add CountPairsofEqualSubstringsWithMinimumDifference

---
+++ leetcode2/CountPairsofEqualSubstringsWithMinimumDifference/Solution.java
@@ -0,0 +1,27 @@
+// Copyright (C) 2021 by iamslash
+
+import java.util.*;
+
+// abc
+//  bccdab
+
+class Solution {
+	public int countQuadruples(String s1, String s2) {
+		int min = Integer.MAX_VALUE;
+		int cnt = 0;
+		for (char c = 'a'; c <= 'z'; ++c) {
+			int j = s1.indexOf(c);
+			int a = s2.lastIndexOf(c);
+			if (j == -1 || a == -1) {
+				continue;
+			}
+			if (j - a < min) {
+				min = j - a + (cnt = 0);
+			}
+			if (j - a == min) {
+				cnt++;
+			}															 
+		}
+		return cnt;
+	}
+}
