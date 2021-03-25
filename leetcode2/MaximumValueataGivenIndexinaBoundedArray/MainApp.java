1c64baef38a4be636014d6108124f1e11b2e6730
David Sun <iamslash@gmail.com>
2021-03-26 07:52:18 +0900

    Add MaximumValueataGivenIndexinaBoundedArray

---
+++ leetcode2/MaximumValueataGivenIndexinaBoundedArray/MainApp.java
@@ -0,0 +1,28 @@
+// Copyright (C) 2021 by iamslash
+
+import java.util.*;
+
+//      n: 4
+//  index: 2
+// maxSum: 6
+//
+// 
+// 1 2 2 1
+
+//      n: 6
+//  index: 1
+// maxSum: 10
+//
+//    ans: 0
+// 1 1 1 1 1 1
+
+class Solution {
+	public int maxValue(int n, int index, int maxSum) {
+		
+	}
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
