38a9900dd4a26cf3f4cb833f7838ab23780ae179
David Sun <iamslash@gmail.com>
2021-03-26 21:30:20 +0900

    MaximumValueataGivenIndexinaBoundedArray ing...

--- leetcode2/MaximumValueataGivenIndexinaBoundedArray/MainApp.java
+++ leetcode2/MaximumValueataGivenIndexinaBoundedArray/MainApp.java
@@ -16,9 +16,23 @@ import java.util.*;
 //    ans: 0
 // 1 1 1 1 1 1
 
+// binary search right most equal
+// O(NlgN) O(1)
 class Solution {
+	private long valid(int n, int index, int a) {
+	}
 	public int maxValue(int n, int index, int maxSum) {
-		
+		maxSum -= n;
+		int l = 0, r = maxSum, m;
+		while (l < r) {
+			m = (l+r+1)/2;
+			if (valid(n, index, m) <= maxSum) {
+				l = m;
+			} else {
+				r = m-1;
+			}
+		}
+		return l + 1;
 	}
 }
 
