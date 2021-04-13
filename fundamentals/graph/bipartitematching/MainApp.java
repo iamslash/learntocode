da934e8ed4da8b57a806526a832d85f9a0e79af4
David Sun <iamslash@gmail.com>
2021-04-13 21:39:39 +0900

    Add bipartitematching

---
+++ fundamentals/graph/bipartitematching/MainApp.java
@@ -0,0 +1,47 @@
+// Copyright (C) 2021 by iamslash
+
+import java.util.*;
+
+class Solution {
+	private boolean dfs(int[][] G, int[] match, boolean[] visit, int u) {
+		int w = G[0].length;
+		for (int v = 0; v < w; ++v) {
+			if (G[u][v] == 0 || visit[v]) {
+				continue;
+			}
+			visit[v] = true;
+			if (match[v] == -1 || dfs(G, match, visit, match[v])) {
+				match[v] = u;
+				return true;
+			}
+		}
+		return false;
+	}
+	public int maximumInvitations(int[][] G) {
+		int h = G.length;
+		int w = G[0].length;
+		int[] match = new int[w];
+		Arrays.fill(match, -1);
+		int ans = 0;
+		for (int u = 0; u < h; ++u) {
+			boolean[] visit = new boolean[w];
+			if (dfs(G, match, visit, u)) {
+				ans++;
+			}
+		}
+		for (int v = 0; v < w; ++v) {
+			if (match[v] >= 0) {
+				System.out.printf("%d -> %d\n", match[v], v);
+			}
+		}
+		return ans;
+	}
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+		int[][] G = new int[][]{{1,1,1},{1,0,0},{1,0,0}};
+		Solution sln = new Solution();
+		System.out.println(sln.maximumInvitations(G));
+  }
+}
