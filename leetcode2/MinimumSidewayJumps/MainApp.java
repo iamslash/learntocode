6b3b2e678d26b97cb7272095f9c1b98e3004868a
David Sun <iamslash@gmail.com>
2021-04-16 07:46:36 +0900

    MinimumSidewayJumps ing...

--- leetcode2/MinimumSidewayJumps/MainApp.java
+++ leetcode2/MinimumSidewayJumps/MainApp.java
@@ -6,18 +6,56 @@ import java.util.*;
 // f . x . .
 // . . . x .
 
+// Time Limit Exceeded
+// BFS
+// O(N) O(N)
 class Solution {
 	public int minSideJumps(int[] obstacles) {
+		int n = obstacles.length;
 		// lane, point, side jumps
 		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[]{0,2,0});
+		q.offer(new int[]{2,0,0});
 		while (q.size() > 0) {
 			int[] item = q.poll();
-			if (item[0] >= obstacles.length) {
-				return item[2];
+			int lane = item[0];
+			int point = item[1];
+			int jumps = item[2];
+      // System.out.printf("lane: %d, point: %d, jumps: %d\n", lane, point, jumps);
+			if (point >= n-1) {
+				return jumps;
+			}
+			if (obstacles[point+1] != lane) {
+				q.offer(new int[]{lane, point+1, jumps});
+			} else {
+				for (int nlane = 1; nlane <= 3 ; ++nlane) {
+					if (lane != nlane && obstacles[point] != nlane) {
+						q.offer(new int[]{nlane, point, jumps+1});
+					}
+				}
 			}
 		}
 		return 0;
+	}
+}
+
+// iterative dynamic programming
+// O(N) O(1)
+class Solution {
+	private final int MAXVAL = 1000000;
+	public int minsideJumps(int[] obstacles) {
+		// C[i]: minimum side jumps at i lane
+		int C = {1, 0, 1};
+		for (int obs in obstacles) {
+			if (obs > 0) {
+				C[obs-1] = MAXVAL;
+			}
+			for (int i = 0; i < 3; ++i) {
+				if (obs != i + 1) {
+					C[i] = Math.min(C[i], Math.min(C[(i+1)%3], C[(i+2)%3]) + 1);
+				}
+			}
+		}
+		return Math.min(C[0], Math.min(C[1], C[2]));
 	}
 }
 
---
+++ leetcode2/MinimumSidewayJumps/README.md
@@ -0,0 +1,37 @@
+# Problem
+
+> [Minimum Sideway Jumps](https://leetcode.com/problems/minimum-sideway-jumps/)
+
+숫자의 모음 `obstacles[]` 가 주어진다. `[1..3]` 만큼의 lane 이 있고
+`obstacles.length` 만큼의 point 가 있다.  개구리는 `2` lane, `0` point
+에서 출발한다. `obstacles[i]` 는 `i` point 에서 장애물이 존재하는 lane
+을 의미한다.
+
+개구리는 자신의 위치에서 오른쪽에 장애물이 없으면 jump 할 수
+있다. 그렇지 않으면 다른 lane 으로 점프하고 side jump 를
+증가한다. 마지막 point 까지 도착했을 때 최소 side jump 를 구하라.
+
+# Idea
+
+iterative dynamic programming 으로 해결할 만하다.
+
+캐시 `int[] C = new int[]{1,0,1}` 를 선언한다. `C[i]` 는 `i` point
+에서 minimum side jump 를 의미한다. 개구리는 `2` lane, `0` point 에서
+출발하기 때문에 `C[1] == 0` 이다.
+
+obs 로 `obstacles` 를 순회하면서 다음을 반복한다. 
+
+* `obs > 0` 이면 `obs` lane 에 장애물이 있다는 의미이다. `C[obs-1]` 에 아주
+  큰 값을 저장하자. 즉, `C[obs-1] = 1000000` 을 수행한다.
+* 인덱스 `i` 를 `[0..3)` 동안 순회하면서 다음을 반복한다. 
+  * `obs != i + 1` 이면 `i+1` lane 에 장애물이 없다는 의미이다.
+
+# Implementation
+
+* [java8](MainApp.java)
+
+# Complexity
+
+```
+O(N) O(1)
+```
