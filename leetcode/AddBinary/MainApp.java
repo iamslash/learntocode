d7542625dbe7e47d1bbb7a411f895714f7b0ae52
David Sun <iamslash@gmail.com>
2021-05-15 09:29:35 +0900

    Accept AddBinary

--- leetcode/AddBinary/MainApp.java
+++ leetcode/AddBinary/MainApp.java
@@ -8,25 +8,59 @@ import java.util.*;
 //   b:  1
 //      j
 // ans:  0
+// c a b  r c
+// 0 0 0  0 0
+// 0 0 1  1 0
+// 0 1 0  1 0
+// 0 1 1  0 1
+// 1 0 0  1 0
+// 1 0 1  0 1
+// 1 1 0  0 1
+// 1 1 1  1 1
 
-// convert
+// 2ms 71.57% 37.9MB 54.95%
+// linear traversal
 // O(N) O(1)
 class Solution {
-	private String reverse(String a) {
-		StringBuffer sb = new StringBuffer(a);
+	public String addBinary(String a, String b) {
+		int n = a.length(), m = b.length();
+		StringBuffer sb = new StringBuffer();
+		int i = 0, c = 0;
+		while (i < n || i < m) {
+			int d1 = i < n ? a.charAt(n-1-i)-'0' : 0;			
+			int d2 = i < m ? b.charAt(m-1-i)-'0' : 0;
+			int num = c + d1 + d2;
+			sb.append(num%2);
+			c = num > 1 ? 1 : 0;
+			++i;
+		}
+		if (c > 0) {
+			sb.append(c);
+		}
 		return sb.reverse().toString();
 	}
+}
+
+// 1ms 100.00% 37.3MB 95.22%
+// linear traversal
+// O(N) O(1)
+class Solution {
 	public String addBinary(String a, String b) {
-		a = reverse(a);
-		b = reverse(b);
-		String r = "";
+		int n = a.length(), m = b.length();
+		StringBuilder sb = new StringBuilder();
 		int i = 0, c = 0;
-		while (i < a.length() || i < b.length()) {
-			int d1 = i < a.length() ? a.charAt(i)-'0' : 0;			
-			int d2 = i < b.length() ? b.charAt(i)-'0' : 0;
-			
+		while (i < n || i < m) {
+			int d1 = i < n ? a.charAt(n-1-i)-'0' : 0;			
+			int d2 = i < m ? b.charAt(m-1-i)-'0' : 0;
+			int num = c + d1 + d2;
+			sb.append(num%2);
+			c = num > 1 ? 1 : 0;
+			++i;
 		}
-		return reverse(r);
+		if (c > 0) {
+			sb.append(c);
+		}
+		return sb.reverse().toString();
 	}
 }
 
