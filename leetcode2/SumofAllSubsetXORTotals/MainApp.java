6242f2face6430735d8e64c1a20dc9ebfb18b763
iamslash <iamslash@gmail.com>
2021-05-16 15:49:26 +0900

    Add SumofAllSubsetXORTotals

---
+++ leetcode2/SumofAllSubsetXORTotals/MainApp.java
@@ -0,0 +1,28 @@
+// Copyright (C) 2019 by iamslash
+
+import java.util.*;
+
+//    nums: 1 3
+// subsets: 00     00
+//          01     01
+//          11     11
+//          01 11  10
+//    sums:         6
+//
+//    nums: a b c d e
+//          0
+//          a b c d e
+//          ab ac ad ae bc bd be cd ce de
+//          
+//            
+
+class Solution {
+	public int subsetXORSum(int[] nums) {
+        
+	}
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
