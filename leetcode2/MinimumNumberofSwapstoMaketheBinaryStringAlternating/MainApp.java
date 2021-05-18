68e669aa9af291e83310ae4fcf55818aca657943
David Sun <iamslash@gmail.com>
2021-05-19 08:02:55 +0900

    Add MinimumNumberofSwapstoMaketheBinaryStringAlternating

---
+++ leetcode2/MinimumNumberofSwapstoMaketheBinaryStringAlternating/MainApp.java
@@ -0,0 +1,22 @@
+// Copyright (C) 2021 by iamslash
+
+import java.util.*;
+
+//        i
+// s: 1 0 1 0 1 0
+//          j
+//
+// s: 1 1 0 0 1
+// s: 1 0 1 0 1
+//        
+
+class Solution {
+	public int minSwaps(String s) {
+		
+	}
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
