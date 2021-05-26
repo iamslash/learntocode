b0089890f2fc14210b89657c42dafd7e0e9a5d17
David Sun <iamslash@gmail.com>
2021-05-27 07:36:11 +0900

    Accept  ProductofTwoRun-LengthEncodedArrays

--- leetcode2/ProductofTwoRun-LengthEncodedArrays/MainApp.java
+++ leetcode2/ProductofTwoRun-LengthEncodedArrays/MainApp.java
@@ -14,6 +14,9 @@ import java.util.*;
 //                 j
 //      ans: 2,3 6,1 9,2
 
+// 33ms 32.08% 90.8MB 64.15%
+// linear traversal
+// O(N) O(N)
 class Solution {
 	public List<List<Integer>> findRLEArray(int[][] enc1, int[][] enc2) {
 		List<List<Integer>> ans = new ArrayList<>();
@@ -21,7 +24,22 @@ class Solution {
 		while (i < n && j < m) {
 			int prd = enc1[i][0] * enc2[j][0];
 			int cnt = Math.min(enc1[i][1], enc2[j][1]);
-			
+			// Update ans
+			if (ans.size() == 0 || ans.get(ans.size()-1).get(0) != prd) {
+				ans.add(Arrays.asList(prd, 0));
+			}
+			List<Integer> item = ans.get(ans.size()-1);
+			item.set(1, item.get(1) + cnt);
+			// Update enc1, enc2 count
+			enc1[i][1] -= cnt;
+			enc2[j][1] -= cnt;
+			// forward i, j
+			if (enc1[i][1] == 0) {
+				++i;
+			}
+			if (enc2[j][1] == 0) {
+				++j;
+			}
 		}
 		return ans;
 	}
