9819a2733c1e1df3fb08d284e3ce592276d1ea5f
David Sun <iamslash@gmail.com>
2021-06-06 06:43:23 +0900

    Add CountPairsinTwoArrays

---
+++ leetcode2/CountPairsinTwoArrays/MainApp.java
@@ -0,0 +1,26 @@
+// Copyright (C) 2021 by iamslash
+
+import java.util.*;
+
+// nums1: 2 1 2 1
+// nums2: 1 2 1 2
+
+// nums1: 1 10 6 2
+// nums2: 1  4 1 5
+
+//        1 2 6 10
+//        1 5 1  4
+
+// nums1[i] + nums1[j] > nums2[i] + nums2[j]
+// nums1[i] - nums2[i] > nums2[j] - nums1[i]
+
+class Solution {
+	public long countPairs(int[] nums1, int[] nums2) {
+		
+	}
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
