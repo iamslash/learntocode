b9ab8efe008dc5ddcba9c45162148a00c562db87
David Sun <iamslash@gmail.com>
2021-06-07 07:39:47 +0900

    Add ReductionOperationstoMaketheArrayElementsEqual

---
+++ leetcode2/ReductionOperationstoMaketheArrayElementsEqual/MainApp.java
@@ -0,0 +1,14 @@
+// Copyright (C) 2021 by iamslash
+
+import java.util.*;
+
+class Solution {
+	public int reductionOperations(int[] nums) {
+		
+	}
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
