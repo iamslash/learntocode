22a6bf936fe2ebc653f131b25358f30b1976c6bc
David Sun <iamslash@gmail.com>
2021-06-24 06:43:38 +0900

    Accept TheNumberofFullRoundsYouHavePlayed

--- leetcode2/TheNumberofFullRoundsYouHavePlayed/MainApp.kt
+++ leetcode2/TheNumberofFullRoundsYouHavePlayed/MainApp.kt
@@ -25,3 +25,21 @@ class Solution {
         return (24*60 - startQuarterMin + endQuarterMin) / 15
     }
 }
+
+// 152ms 77.78% 35.7MB 38.89%
+// math
+// O(1) O(1)
+import kotlin.math.*
+class Solution {
+    fun numberOfRounds(startTime: String, endTime: String): Int {
+        var startArr = startTime.split(":")
+        var endArr = endTime.split(":")
+        var startMin = startArr[0].toInt() * 60 + startArr[1].toInt()
+        var endMin = endArr[0].toInt() * 60 + endArr[1].toInt()
+        if (startMin > endMin) {
+            endMin += 24 * 60
+        }
+        return max(0, Math.floor(endMin / 15.00).toInt() -
+            Math.ceil(startMin / 15.00).toInt())
+    }
+}
