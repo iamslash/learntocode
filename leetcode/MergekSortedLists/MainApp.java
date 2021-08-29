56464345ea53508e1208bca6ab428ba3a6f36aaa
David Sun <iamslash@gmail.com>
2021-08-29 15:19:39 +0900

    Accept MergekSortedLists

--- leetcode/MergekSortedLists/MainApp.java
+++ leetcode/MergekSortedLists/MainApp.java
@@ -11,32 +11,73 @@ class ListNode {
 }
 
 
-// 8ms 27.18% 44.6MB 5.64%
-// priority queue
-// O(NlgN) O(N)
+// // 8ms 27.18% 44.6MB 5.64%
+// // priority queue
+// // O(NLlgL) O(N)
+// // N: count of nodes
+// // L: count of lists
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         ListNode head = new ListNode();
+//         ListNode p = head;
+//         Queue<ListNode> pq = new PriorityQueue<ListNode>((a, b) -> a.val - b.val);
+//         for (ListNode u : lists) {
+//             if (u != null) {
+//                 pq.offer(u);
+//             }
+//         }
+//         while (pq.size() > 0) {
+//             ListNode u = pq.poll();
+//             p.next = u;
+//             p = p.next;
+//             if (u.next != null) {
+//                 pq.offer(u.next);
+//             }
+//         }
+//         return head.next;
+//     }
+// }
+
+// divide and conquor
+// O(NlgL) O(lgL)
+// N: count of nodes
+// L: count of lists
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode head = new ListNode();
-        ListNode p = head;
-        Queue<ListNode> pq = new PriorityQueue<ListNode>((a, b) -> a.val - b.val);
-        for (ListNode u : lists) {
-            if (u != null) {
-                pq.offer(u);
-            }
-        }
-        while (pq.size() > 0) {
-            ListNode u = pq.poll();
-            p.next = u;
-            p = p.next;
-            if (u.next != null) {
-                pq.offer(u.next);
-            }
-        }
-        return head.next;
+       if(lists.length == 0 || lists  == null)
+            return null;
+        return recurLists(lists, 0, lists.length - 1);
     }
-}
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
+    public ListNode recurLists(ListNode[] lists, int start, int end){
+        // base
+        if(start > end)
+            return null;
+        if(start == end)
+            return lists[start];
+        // recursion
+        int m = start+(end - start) / 2; 
+        ListNode left = recurLists(lists, start, m);
+        ListNode right = recurLists(lists, m + 1, end);
+        return merge(left, right);
+    }
+    public ListNode merge(ListNode left, ListNode right){
+        ListNode ans = new ListNode();
+        ListNode iter = ans;
+        while(left != null && right != null){
+            if(left.val < right.val){
+                iter.next = left;
+                left = left.next;
+            }
+            else{
+                iter.next = right;
+                right = right.next;
+            }
+            iter = iter.next;
+        }
+        if(left != null)
+            iter.next = left;
+        if(right != null)
+            iter.next = right;
+        return ans.next;
+    }
 }
