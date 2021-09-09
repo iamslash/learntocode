c910f2cee59ee784b6a7b1e9ad68d3a58d954081
David Sun <iamslash@gmail.com>
2021-09-09 09:25:30 +0900

    Accept FindAllGroupsofFarmland

--- leetcode2/FindAllGroupsofFarmland/MainApp.java
+++ leetcode2/FindAllGroupsofFarmland/MainApp.java
@@ -39,6 +39,34 @@ class Solution {
     }
 }
 
+// 7ms 85.79% 126.5MB 37.06%
+// brute force
+// O(HW) O(N)
+class Solution {
+    public int[][] findFarmland(int[][] land) {
+        int h = land.length, w = land[0].length;
+        List<int[]> ans = new ArrayList<>();
+        for (int y = 0; y < h; ++y) {
+            for (int x = 0; x < w; ++x) {
+                if (land[y][x] == 1 &&
+                    (y == 0 || land[y-1][x] == 0) &&
+                    (x == 0 || land[y][x-1] == 0)) {
+                    int yy = y, xx = x;
+                    while (yy < h && land[yy][x] == 1) {
+                        yy++;
+                    }
+                    while (xx < w && land[y][xx] == 1) {
+                        xx++;
+                    }
+                    ans.add(new int[]{y, x, yy-1, xx-1});
+                    x = xx-1;
+                }
+            }
+        }
+        return ans.toArray(new int[0][]);
+    }
+}
+
 public class MainApp {
   public static void main(String[] args) {
   }
