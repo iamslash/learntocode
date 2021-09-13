e3269cab710ed6ec1742249b9014bed97e9836d1
David Sun <iamslash@gmail.com>
2021-09-14 07:48:24 +0900

    Add WidestPairofIndicesWithEqualRangeSum

---
+++ leetcode2/WidestPairofIndicesWithEqualRangeSum/MainApp.java
@@ -0,0 +1,22 @@
+// Copyright (C) 2021 by iamslash
+
+imoprt java.util.*;
+
+//             i    
+//      nums1: 1 1 0
+//      nums2: 0 1 1
+// sum2idxMap: 0 1
+//            -1 0
+//        sum: 0
+//        ans: 0
+
+class Solution {
+    public int widestPairOfIndices(int[] nums1, int[] nums2) {
+        
+    }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
