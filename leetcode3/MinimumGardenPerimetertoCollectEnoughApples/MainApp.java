9805e9d88856eac5cceb14690cdf3dce516d5342
David Sun <iamslash@gmail.com>
2021-09-22 08:03:19 +0900

    Add MinimumGardenPerimetertoCollectEnoughApples

---
+++ leetcode3/MinimumGardenPerimetertoCollectEnoughApples/MainApp.java
@@ -0,0 +1,31 @@
+// Copyright (C) 2021 by iamslash
+
+import java.util.*;
+
+// 0
+
+// 2 1 2
+// 1 0 1
+// 2 1 2
+
+// 4 3 2 3 4
+// 3 2 1 2 3
+// 2 1 0 1 2
+// 3 2 1 2 3
+// 4 3 2 3 4 
+
+class Solution {
+    public long minimumPerimeter(long neededApples) {
+        int cnt = 0, len = 0;
+        while (cnt < neededApples) {
+            
+            len++;
+        }
+        return len * 4;
+    }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
---
+++ leetcode3/MinimumGardenPerimetertoCollectEnoughApples/README.md
@@ -0,0 +1,25 @@
+# Problem
+
+[Minimum Garden Perimeter to Collect Enough Apples](https://leetcode.com/problems/minimum-garden-perimeter-to-collect-enough-apples/)
+
+숫자 `neededApples` 가 주어진다. `(0,0)` 을 중심으로 정사각형을
+정해보자. 각 좌표에 사과나무를 심는다. 나무에 열리는 사과의 개수는
+다음과 같이 정해진다.
+
+```
+apples = |x| + |y|
+```
+
+`neededApples` 를 얻을려면 필요한 최소 크기의 정사각형을
+생각해보자. 그때 정사각형의 지름을 구하라.
+
+# Idea
+
+# Implementation
+
+* [java8](MainApp.java)
+
+# Complexity
+
+```
+```
