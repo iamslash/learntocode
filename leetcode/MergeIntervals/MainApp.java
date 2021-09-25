ec451c05f32fa6a735275cae748c7d4e14c74214
David Sun <iamslash@gmail.com>
2021-09-25 15:55:55 +0900

    Accept MergeIntervals

--- leetcode/MergeIntervals/MainApp.java
+++ leetcode/MergeIntervals/MainApp.java
@@ -2,29 +2,55 @@
 
 import java.util.*;
 
+// // 6ms 64.61% 41.5MB 87.12%
+// // sort
+// // O(NlgN) O(N)
+// class Solution {
+//     public int[][] merge(int[][] intervals) {
+//         List<int[]> ans = new ArrayList<>();
+//         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+//         for (int[] interval : intervals) {
+//             int[] lastInterval = null;
+//             if (ans.size() > 0) {
+//                 lastInterval = ans.get(ans.size()-1);
+//             }
+//             if (lastInterval == null || (lastInterval[1] < interval[0])) {
+//                 ans.add(interval);
+//             } else {
+//                 lastInterval[1] = Math.max(lastInterval[1], interval[1]);
+//             }
+//         }
+//         return ans.toArray(new int[0][]);
+//     }
+// }
+
 //                       i
 // intervals: [[1,4],[4,5]]
-//       ans: [[1,4]
+// mergedSrc: [[1,4]
 
-// 6ms 64.61% 41.5MB 87.12%
+// 5ms 96.06% 41.4MB 87.12%
 // sort
 // O(NlgN) O(N)
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> ans = new ArrayList<>();
+        int[][] mergedSrc = new int[intervals.length][2];
+        int pos = 0;
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         for (int[] interval : intervals) {
-            int[] lastInterval = null;
-            if (ans.size() > 0) {
-                lastInterval = ans.get(ans.size()-1);
-            }
-            if (lastInterval == null || (lastInterval[1] < interval[0])) {
-                ans.add(interval);
+            if (pos == 0 || mergedSrc[pos-1][1] < interval[0]) {
+                mergedSrc[pos][0] = interval[0];
+                mergedSrc[pos][1] = interval[1];
+                ++pos;
             } else {
-                lastInterval[1] = Math.max(lastInterval[1], interval[1]);
+                mergedSrc[pos-1][1] = Math.max(mergedSrc[pos-1][1], interval[1]);
             }
         }
-        return ans.toArray(new int[0][]);
+        int[][] mergedDst = new int[pos][2];
+        for (int i = 0; i < pos; ++i) {
+            mergedDst[i][0] = mergedSrc[i][0];
+            mergedDst[i][1] = mergedSrc[i][1];
+        }
+        return mergedDst;
     }
 }
 
