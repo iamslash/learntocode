0786f9be7cedf63b97cb3dc6fe51c828237659e3
David Sun <iamslash@gmail.com>
2021-09-27 21:25:00 +0900

    Add AverageHeightofBuildingsinEachSegment

---
+++ leetcode3/AverageHeightofBuildingsinEachSegment/MainApp.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2021 by iamslash
+
+import java.util.*;
+
+//                i
+// buildings: 
+//        pq: 1,2 3,4 5,-2 10,-4
+//       ans:      
+//            
+
+// priority queue
+// O(NlgN) O(N)
+class Solution {
+	public int[][] averageHeightOfBuildings(int[][] buildings) {
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+		for (int[] bldg : buildings) {
+			pq.offer(new int[]{bldg[0], bldg[2]});
+			pq.offer(new int[]{bldg[1], -bldg[2]});			
+		}
+		Deque<int[]> ans = new ArrayDeque<>();
+		int start = 0, sum = 0, cnt = 0;
+		while (!pq.isEmpty()) {
+			int[] evt = pq.poll();
+			if (cnt != 0 && evt[0] != start) {
+				int avgh = sum / cnt;
+				if (!ans.isEmpty() &&
+						ans.peekLast()[1] == start &&
+						ans.peekLast()[2] == avgh) {
+					ans.peekLast()[1] = evt[0];
+					ans.peekLast()[2] = avgh;
+				} else {
+					ans.offerLast(new int[]{start, evt[0], avgh});
+				}
+			}
+			start = evt[0];
+			sum += evt[1];
+			cnt += evt[1] > 0 ? 1 : -1;
+		}
+		return ans.toArray(new int[0][]);		
+	}
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
