f29d9c159255c90d9c74e0e4322fa57c495c5d2e
David Sun <iamslash@gmail.com>
2021-10-04 10:08:20 +0900

    Accept NumberofPairsofStringsWithConcatenationEqualtoTarget

--- leetcode3/NumberofPairsofStringsWithConcatenationEqualtoTarget/MainApp.java
+++ leetcode3/NumberofPairsofStringsWithConcatenationEqualtoTarget/MainApp.java
@@ -7,6 +7,9 @@ import java.util.*;
 // freqMap: 7 77 777
 //          1  2   3
 
+// 2ms 100.00% 38.6MB 100.00%
+// hash map
+// O(N) O(N)
 class Solution {
     public int numOfPairs(String[] nums, String tgt) {
         Map<String, Integer> freqMap = new HashMap<>();
@@ -15,11 +18,20 @@ class Solution {
             freqMap.putIfAbsent(s, 0);
             freqMap.put(s, freqMap.get(s) + 1);
         }
-        for (String prefix : nums) {
+        for (String prefix : freqMap.keySet()) {
             if (!tgt.startsWith(prefix)) {
                 continue;
             }
-            if (prefix.length() * 2 == nums) {
+            String suffix = tgt.substring(prefix.length());
+            if (!freqMap.containsKey(suffix)) {
+                continue;
+            }
+            int prefixFreq = freqMap.get(prefix);
+            int suffixFreq = freqMap.get(suffix);
+            if (prefix.equals(suffix)) {
+                cnt += (prefixFreq * (prefixFreq-1));
+            } else {
+                cnt += prefixFreq * suffixFreq;
             }
         }
         return cnt;
