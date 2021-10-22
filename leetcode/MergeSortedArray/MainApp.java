7de4be08ea35d161154897f967080ddd600a4ca3
David Sun <iamslash@gmail.com>
2021-10-22 22:26:58 +0900

    Accept MergeSortedArray

--- leetcode/MergeSortedArray/MainApp.java
+++ leetcode/MergeSortedArray/MainApp.java
@@ -10,7 +10,6 @@ import java.util.*;
 // nums2: 2 5 6
 //        k
 
-// O(M+N) O(M+N)
 // brute force
 // O(M+N) O(M+N)
 class Solution {
@@ -30,6 +29,33 @@ class Solution {
         // System.out.println();
 		System.arraycopy(rst, 0, nums1, 0, m+n);
 	}
+}
+
+//     m: 3
+//                  i
+// nums1: 1 2 3 0 0 0
+//            j
+//     n: 3
+// nums2: 2 5 6
+//            j
+
+// 0ms 100.00% 39.5MB 26.00%
+// brute force
+// O(M+N) O(1)
+class Solution {
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int j = m-1, k = n-1;
+		for (int i = m+n-1; i >= 0; --i) {
+			if (k < 0) {
+				break;
+			}
+			if (j >= 0 && nums1[j] > nums2[k]) {
+				nums1[i] = nums1[j--];
+			} else {
+				nums1[i] = nums2[k--];
+			}
+		}
+	}	
 }
 
 public class MainApp {
