39d764d9dcf8fe0240c64efce8c94cc7cb05a121
David Sun <iamslash@gmail.com>
2021-11-15 09:41:41 +0900

    Accept TimeNeededtoBuyTickets

--- leetcode3/TimeNeededtoBuyTickets/MainApp.java
+++ leetcode3/TimeNeededtoBuyTickets/MainApp.java
@@ -9,17 +9,25 @@ import java.util.*;
 
 //       k: 1
 //          i
-// tickets: 5 1 3 1     
-//          4 0 2 0     
-//          2 0 0 0     
+// tickets: 5 1 3 1 
+//          1 0 0 0     
+//          4 1 3 1   
 
+
+// i <= k, min(A[k], A[i])
+// i  > k, min(A[k]-1, A[i])
+// 0ms 100.00% 36.9MB 55.56%
 // brute force
 // O(N) O(1)
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int sec = 0;
         for (int i = 0; i < tickets.length; ++i) {
-            sec += Math.min(tickets[i], tickets[k]);
+            if (i <= k) {
+                sec += Math.min(tickets[k], tickets[i]);
+            } else {
+                sec += Math.min(tickets[k]-1, tickets[i]);
+            }
         }
         return sec;        
     }
