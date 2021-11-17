d3e402576fffbf2ad56714d3d5a844e1487b37a4
David Sun <iamslash@gmail.com>
2021-11-17 10:05:47 +0900

    Add DescribethePainting

---
+++ leetcode3/DescribethePainting/MainApp.java
@@ -0,0 +1,14 @@
+// Copyright (C) 2021 by iamslash
+
+import java.util.*;
+
+class Solution {
+    public List<List<Long>> splitPainting(int[][] segments) {
+        
+    }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
