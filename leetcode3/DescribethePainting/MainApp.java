3841d602f2268a6d11205404200c9bd17c44e8c9
David Sun <iamslash@gmail.com>
2021-11-18 10:50:54 +0900

    DescribethePainting ing...

--- leetcode3/DescribethePainting/MainApp.java
+++ leetcode3/DescribethePainting/MainApp.java
@@ -2,9 +2,25 @@
 
 import java.util.*;
 
+// hash map
+// O(N) O(N)
 class Solution {
     public List<List<Long>> splitPainting(int[][] segments) {
-        
+        Map<Long, Long> colMap = new TreeMap<>();
+        for (int[] seg : segments) {
+            colMap.put((long)seg[0], colMap.getOrDefault((long)seg[0], 0L) + (long)seg[2]);
+            colMap.put((long)seg[1], colMap.getOrDefault((long)seg[1], 0L) - (long)seg[2]);
+        }
+        List<List<Long>> ans = new ArrayList<>();
+        long prvTs = 0, prvCol = 0;
+        for (Long key : colMap.keySet()) {
+            if (prvTs > 0) {
+                ans.add(Arrays.asList(prvTs, key, prvCol));
+            }
+            prvTs = key;
+            prvCol += colMap.get(key);
+        }
+        return ans;
     }
 }
 
