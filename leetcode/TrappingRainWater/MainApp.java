2717af01c6ec98d70c97d67e8cd781ddbbaa04e7
David Sun <iamslash@gmail.com>
2022-01-21 08:29:21 +0900

    TrappingRainWater ing...

--- leetcode/TrappingRainWater/MainApp.java
+++ leetcode/TrappingRainWater/MainApp.java
@@ -1,91 +1,33 @@
-// Copyright (C) 2021 by iamslash
+// Copyright (C) 2022 by iamslash
 
 import java.util.*;
 
-//      i
-//      5
-// 4
-//    3
-//  2  2
-//
-//   0
-//      j
-//   leftHeight: 4 
-//  rightHeight: 0
-//   leftWater: 9
-//  rightWater: 0
-//       water: 
+//                     l
+// height: 0 1 0 2 1 0 1 3 2 1 2 1
+//                       r
 
-//     i
-//   1
-//  0 0
-//    j
+//               l
+// height: 5 5 1 7 1 1 5 2 7 6
+//                         r
+//   lMax: 7
+//   rMax: 7
+//  water: 4
 
-//        i          
-//        3
-//    2    2 2 
-//  1  1 1  1 1
-// 0 0  0 
-//        j
-//   leftHeight: 3
-//  rightHeight: 2
-//   leftWater: 4
-//  rightWater: 0
-//       water: 2
-
-// 1ms 90.67% 38.6MB 68.70%
 // two pointers
 // O(N) O(1)
 class Solution {
-    public int trap(int[] H) {
-        int n = H.length, i = 0, j = n-1, trapWater = 0;
-        int leftHeight = 0, rightHeight = 0, leftWater = 0, rightWater = 0;
-        while (i < j) {
-            if (H[i] <= H[j]) {
-                if (H[i] >= leftHeight) {
-                    trapWater += leftWater;
-                    leftWater = 0;
-                } 
-                leftHeight = Math.max(leftHeight, H[i]);
-                leftWater += leftHeight - H[i];
-                ++i;
+    public int trap(int[] height) {
+        int n = height.length, water = 0, lCum = 0, rCum = 0;
+        int l = 0, r = n-1, lMax = height[0], rMax = height[n-1];
+        while (l < r) {
+            if (height[l] <= height[r]) {
+                lMax = Math.max(lMax, height[l]);
+                water += lMax - height[l++];
             } else {
-                if (H[j] >= rightHeight) {
-                    trapWater += rightWater;
-                    rightWater = 0;
-                }
-                rightHeight = Math.max(rightHeight, H[j]);
-                rightWater += rightHeight - H[j];
-                --j;
+                rMax = Math.max(rMax, height[r]);
+                water += rMax - height[r--];
             }
         }
-        return trapWater + leftWater + rightWater;
+        return water;
     }
-}
-
-// 0ms 100.00% 38.4MB 87.22%
-// two pointers
-// O(N) O(1)
-class Solution {
-    public int trap(int[] H) {
-        int i = 0, j = n-1, trapWater = 0;
-        int leftMaxH = H[i], rightMaxH = H[j];
-        while (i < j) {
-            if (leftMaxH < rightMaxH) {
-                i++;
-                leftMaxH = Math.max(leftMaxH, H[i]);
-                trapWater += leftMaxH - H[i];
-            } else {
-                j--;
-                rightMaxH = Math.max(rightMaxH, H[j]);
-                trapWater += rightMaxH - H[j];
-            }
-        }
-        return trapWater;
-    }
-}
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
 }
