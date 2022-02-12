4fb66d02e53976f7d884d47bd7a41e82e6985aa9
David Sun <iamslash@gmail.com>
2022-02-12 09:09:08 +0900

    Uncomment

--- leetcode2/MinimumDifficultyofaJobSchedule/a.cpp
+++ leetcode2/MinimumDifficultyofaJobSchedule/a.cpp
@@ -13,46 +13,46 @@ using namespace std;
 
 // 6 5 4 3 2 1
 
-// // 460ms 11.57%
-// // dynamic programming recursive way
-// // O(N^2) O(N^2)
-// class Solution {
-//  private:
-//   int64_t dfs(vector<vector<int64_t>>& C, vector<int>& J, int d, int i) {
-//     // base
-//     int n = J.size();
-//     if (d <= 0) {
-//       return INT_MAX;
-//     }
-//     if (d == 1) {
-//       return *max_element(J.begin()+i, J.end());
-//     }
-//     // memo
-//     int64_t& r = C[d][i];
-//     if (r >= 0)
-//       return r;
-//     // recursion
-//     r = INT_MAX;
-//     int maxJ = 0;
-//     for (int j = i; j + d - 1 < n; ++j) {
-//       maxJ = max(maxJ, J[j]);
-//       r = min(r, maxJ + dfs(C, J, d-1, j+1));
-//     }
-//     return r;
-//   }
-//  public:
-//   int minDifficulty(vector<int>& J, int d) {
-//     // edge case
-//     int n = J.size();
-//     if (n < d)
-//       return -1;
-//     vector<vector<int64_t>> C(d+1, vector<int64_t>(n, -1));
-//     int64_t ans = dfs(C, J, d, 0);
-//     if (ans >= INT_MAX)
-//       return -1;
-//     return int(ans);
-//   }
-// };
+// 460ms 11.57%
+// dynamic programming recursive way
+// O(N^2) O(N^2)
+class Solution {
+ private:
+  int64_t dfs(vector<vector<int64_t>>& C, vector<int>& J, int d, int i) {
+    // base
+    int n = J.size();
+    if (d <= 0) {
+      return INT_MAX;
+    }
+    if (d == 1) {
+      return *max_element(J.begin()+i, J.end());
+    }
+    // memo
+    int64_t& r = C[d][i];
+    if (r >= 0)
+      return r;
+    // recursion
+    r = INT_MAX;
+    int maxJ = 0;
+    for (int j = i; j + d - 1 < n; ++j) {
+      maxJ = max(maxJ, J[j]);
+      r = min(r, maxJ + dfs(C, J, d-1, j+1));
+    }
+    return r;
+  }
+ public:
+  int minDifficulty(vector<int>& J, int d) {
+    // edge case
+    int n = J.size();
+    if (n < d)
+      return -1;
+    vector<vector<int64_t>> C(d+1, vector<int64_t>(n, -1));
+    int64_t ans = dfs(C, J, d, 0);
+    if (ans >= INT_MAX)
+      return -1;
+    return int(ans);
+  }
+};
 
 // 44ms 78.93% 7.5MB 72.34%
 // dynamic programming iterative way
