57d8c270dd05675f043680c72ceae606bfe100f3
David Sun <iamslash@gmail.com>
2022-02-13 16:35:49 +0900

    Accept MinimumDifficultyofaJobSchedule

--- leetcode2/MinimumDifficultyofaJobSchedule/MainApp.java
+++ leetcode2/MinimumDifficultyofaJobSchedule/MainApp.java
@@ -14,22 +14,23 @@ import java.util.*;
 // O(DN^2) O(N)
 class Solution {
     public int minDifficulty(int[] jobDifficulty, int days) {
-        int n = jobDifficulty.length, MAX_JOB_SCORE_SUM = 300_000;
+        int n = jobDifficulty.length, MAX_SUM = 300_000;
         if (n < days) {
             return -1;
         }
-        // C[i]: min job difficulty starts with i-th job
+        // C[i]: min sum of job difficulty starts with i-th job
         int[] C = new int[n+1];
-        Arrays.fill(C, MAX_JOB_SCORE_SUM);
+        Arrays.fill(C, MAX_SUM);
         C[n] = 0;
 
+        // O(DN^2)
         for (int d = 0; d < days; ++d) {
             for (int i = 0; i < n-d; ++i) {
-                int maxScore = 0;
-                C[i] = MAX_JOB_SCORE_SUM;
+                int fronMaxJob = 0;
+                C[i] = MAX_SUM;
                 for (int j = i; j < n-d; ++j) {
-                    maxScore = Math.max(maxScore, jobDifficulty[j]);
-                    C[i] = Math.min(C[i], maxScore + C[j+1]);
+                    fronMaxJob = Math.max(fronMaxJob, jobDifficulty[j]);
+                    C[i] = Math.min(C[i], fronMaxJob + C[j+1]);
                 }
             }
         }        
