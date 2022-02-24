b2dc06736b43796d59396072243c57eca8c2578b
David Sun <iamslash@gmail.com>
2022-02-25 08:38:01 +0900

    Update UniqueSubstringsWithEqualDigitFrequency README

--- leetcode3/UniqueSubstringsWithEqualDigitFrequency/MainApp.java
+++ leetcode3/UniqueSubstringsWithEqualDigitFrequency/MainApp.java
@@ -82,7 +82,98 @@ class Solution {
     }
 }
 
-public class MainApp {
-  public static void main(String[] args) {
-  }
+// 93ms 90.34% 53.9MB 80.68%
+// rolling hash
+// O(N^2) O(N)
+class Solution {
+    public int equalDigitFrequency(String s) {
+        final int MOD = 1_000_000_007;
+        Set<Integer> numSet = new HashSet<>();
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int[] freqs = new int[10];
+            int uniq = 0, maxCnt = 0, rollingHash = 0;
+            for (int j = i; j < n; ++j) {
+                char c = s.charAt(j);
+                int digit = c - '0';
+                uniq += freqs[digit] == 0 ? 1 : 0;
+                freqs[digit]++;
+                maxCnt = Math.max(maxCnt, freqs[digit]);
+                rollingHash = (rollingHash * 11 + digit + 1) % MOD;
+                if (maxCnt * uniq == j - i + 1) {
+                    numSet.add(rollingHash);
+                }
+            }
+        }
+        return numSet.size();
+    }
+}
+
+// 198ms 63.07% 368.9MB 6.82%
+// trie
+// O(N^2) O(N)
+class TrieNode {
+    TrieNode[] next = new TrieNode[10];
+    boolean seen;
+}
+class Solution {
+    public int equalDigitFrequency(String s) {
+        TrieNode trie = new TrieNode();
+        int cnt = 0, n = s.length();
+        for (int i = 0; i < n; ++i) {
+            TrieNode u = trie;
+            int[] freqs = new int[10];
+            int uniq = 0, maxCnt = 0;
+            for (int j = i; j < n; ++j) {
+                int digit = s.charAt(j) - '0';
+                uniq += freqs[digit] == 0 ? 1 : 0;
+                freqs[digit]++;
+                maxCnt = Math.max(maxCnt, freqs[digit]);
+                if (u.next[digit] == null) {
+                    u.next[digit] = new TrieNode();
+                }
+                u = u.next[digit];
+                if (uniq * maxCnt == j - i + 1 && !u.seen) {
+                    cnt++;
+                    u.seen = true;
+                }
+            }
+        }
+        return cnt;
+    }
+}
+
+// 667ms 33.52% 117.9MB 43.75%
+// hash set
+// O(N^2) O(N)
+class Solution {
+    private boolean sameDigitCount(int[] freqs) {
+        int cnt = 0;
+        for (int i = 0; i < 10; ++i) {
+            if (freqs[i] > 0 && cnt > 0 && freqs[i] != cnt) {
+                return false;
+            }
+            if (freqs[i] > 0 && cnt == 0) {
+                cnt = freqs[i];
+            }
+        }
+        return true;
+    }
+    public int equalDigitFrequency(String s) {
+        Set<String> strSet = new HashSet<>();
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int[] freqs = new int[10];
+            int uniq = 0, maxCnt = 0;
+            for (int j = i; j < n; ++j) {
+                int digit = s.charAt(j) - '0';
+                uniq += freqs[digit]++ == 0 ? 1 : 0;
+                maxCnt = Math.max(maxCnt, freqs[digit]);
+                if (uniq * maxCnt == j - i + 1) {
+                    strSet.add(s.substring(i,j+1));
+                }
+            }
+        }
+        return strSet.size();
+    }
 }
---
+++ leetcode3/UniqueSubstringsWithEqualDigitFrequency/README.md
@@ -0,0 +1,53 @@
+# Problem
+
+[Unique Substrings With Equal Digit Frequency](https://leetcode.com/problems/unique-substrings-with-equal-digit-frequency/)
+
+문자열 `s` 가 주어진다. `s` 는 숫자로만 구성되어 있다. `s` 의
+substring 들중 문자열을 구성하는 숫자의 개수가 동일한 것들의 개수를
+구하라.
+
+# Idea
+
+예를 들어 `s = "1212"` 의 경우를 살펴보자.
+
+```
+s: 1212
+   1
+   12
+   1212
+    2
+    21
+```
+
+따라서 답은 `5` 이다.
+
+다음과 같이 2 개의 loop 를 구상한다.
+
+```java
+for (int i = 0; i < n; ++i) {
+  for (int j = i; j < n; ++j) {
+  }
+}
+```
+
+`i` 부터 `j` 까지의 substring 마다 구성하는 숫자의 개수가 같은지
+검사한다. 그렇다면 `Set<String> strSet` 에 추가한다. 모든 반복을
+마치면 `strSet.size()` 가 답이다.
+
+`Set<String> strSet` 대신 `Set<Integer> numSet` 을 선언하여
+`String` 대신 hash 에 해당하는 `Integer` 를 삽입해 보자. hash set
+의 time, space complexity 를 최적화할 수 있다.
+
+또한 `Set<Integer> numSet` 대신 trie 를 사용해보자. 지금까지 발견한
+substring 들을 trie 에 저장해 놓으면 마찬가지로 `Set<String> strSet`
+을 사용한 방법보다 time, space complexity 를 최적화할 수 있다.
+
+# Implementation
+
+* [java8](MainApp)
+
+# Complexity
+
+```
+O(N^2) O(N)
+```
