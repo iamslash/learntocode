b7ccaa90ecd2c5faa176101bc9885ba36cb477f5
David Sun <iamslash@gmail.com>
2022-03-12 20:58:16 +0900

    Accept SearchSuggestionsSystem

--- leetcode2/SearchSuggestionsSystem/a.cpp
+++ leetcode2/SearchSuggestionsSystem/a.cpp
@@ -1,30 +1,31 @@
-/* Copyright (C) 2020 by iamslash */
+/* Copyright (C) 2022 by iamslash */
 
 #include <cstdio>
-#include <vector>
 #include <string>
-#include <algorithm>
+#include <vector>
 
-// 48ms 81.67% 33.1MB 100.00%
-// binary search
+using namespace std;
+
+// 36ms 97.65% 23.9MB 90.73%
+// sort, binary search
 // O(NlgN) O(N)
 class Solution {
- public:
-  std::vector<std::vector<std::string>> suggestedProducts(
-      std::vector<std::string>& P, std::string S) {
-    std::vector<std::vector<std::string>> ans;
-    std::string key;
-    auto it = P.begin();
-    std::sort(it, P.end());
-    for (char c : S) {
-      ans.push_back(std::vector<std::string>());
-      key += c;
-      it = std::lower_bound(it, P.end(), key);
-      for (int i = 0; i < 3 && it+i != P.end(); i++) {
-        std::string& s = *(it + i);
-        if (s.find(key))
+public:
+  vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+    vector<vector<string>> ans;
+    auto it = products.begin();
+    sort(products.begin(), products.end());
+    string word;
+    for (char c : searchWord) {
+      word += c;
+      ans.push_back(vector<string>());
+      it = lower_bound(it, products.end(), word);
+      for (auto i = 0; i < 3 && (it + i) != products.end(); ++i) {
+        string& product = *(it + i);
+        if (product.find(word)) {
           break;
-        ans.back().push_back(s);
+        }
+        ans.back().push_back(product);
       }
     }
     return ans;
