577ddc127c2f37f39e0fefb506d2702107c7721a
David Sun <iamslash@gmail.com>
2022-03-14 08:05:20 +0900

    Add FindAllK-DistantIndicesinanArray

---
+++ leetcode3/FindAllK-DistantIndicesinanArray/MainApp.kt
@@ -0,0 +1,20 @@
+/* Copyright (C) 2022 by iamslash */
+
+import java.util.*;
+
+// sort, hash set
+class Solution {
+    fun findKDistantIndices(nums: IntArray, key: Int, k: Int): List<Int> {
+        val idxSet: Set<Int> = setOf();
+        val n = nums.size
+        for (i in 0 until n) {
+        }
+        val idxList: List<Int> = idxSet.toList()
+        idxList.sorted()
+        return idxList
+    }
+}
+
+fun main() {
+    println("Hello World")
+}
