eced0e91ca766c80ab824407a243f9d228496fc4
David Sun <iamslash@gmail.com>
2022-03-15 17:49:52 +0900

    Add Step-By-StepDirectionsFromaBinaryTreeNodetoAnother

---
+++ leetcode3/Step-By-StepDirectionsFromaBinaryTreeNodetoAnother/a.cpp
@@ -0,0 +1,25 @@
+/* Copyright (C) 2022 by iamslash */
+
+#include <cstdio>
+#include <vector>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+      vector<char> srcPath, dstPath;
+      
+    }
+};
+
+int main() {
+  return 0;
+}
