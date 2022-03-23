c696ce2d59f0d29e85d0ec38df0c30b20aa4e235
David Sun <iamslash@gmail.com>
2022-03-23 22:56:50 +0900

    Add TwoBestNon-OverlappingEvents

---
+++ leetcode3/TwoBestNon-OverlappingEvents/MainApp.java
@@ -0,0 +1,16 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+// brute force
+// O(1) O(1)
+class Solution {
+    public int maxTwoEvents(int[][] events) {
+        
+    }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
