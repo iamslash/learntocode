aca9df43d50b2195b80b903e5c74ab1785b7c5b1
David Sun <iamslash@gmail.com>
2022-04-02 18:32:37 +0900

    Add DesignHitCounter

---
+++ leetcode/DesignHitCounter/MainApp.java
@@ -0,0 +1,24 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+class HitCounter {
+
+    public HitCounter() {
+        
+    }
+    
+    public void hit(int timestamp) {
+        
+    }
+    
+    public int getHits(int timestamp) {
+        
+    }
+}
+
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
