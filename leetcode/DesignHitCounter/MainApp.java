20b18074419bc30b220e04eac56c73e2becc858d
David Sun <iamslash@gmail.com>
2022-04-02 19:24:05 +0900

    Accept DesignHitCounter

--- leetcode/DesignHitCounter/MainApp.java
+++ leetcode/DesignHitCounter/MainApp.java
@@ -2,23 +2,25 @@
 
 import java.util.*;
 
+// 1ms 98.73% 39.7MB 99.79%
+// deque
+//     hit: O(1) O(N)
+// getHits: O(1) O(N)
 class HitCounter {
-
-    public HitCounter() {
-        
+    private Deque<Integer> deq = new ArrayDeque<>();
+    private void clean(int ts) {
+        int i = ts - 300;
+        while (deq.size() > 0 && deq.peekFirst() <= i) {
+            deq.removeFirst();
+        }
+    }
+    public void hit(int ts) {
+        deq.addLast(ts);
+        clean(ts);
     }
     
-    public void hit(int timestamp) {
-        
+    public int getHits(int ts) {
+        clean(ts);
+        return deq.size();
     }
-    
-    public int getHits(int timestamp) {
-        
-    }
-}
-
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
 }
