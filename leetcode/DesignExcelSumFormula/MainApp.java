829c8a7354d0e8eeade1daa6b70044df7726ae15
David Sun <iamslash@gmail.com>
2022-04-02 22:37:24 +0900

    Update from data to sheet

--- leetcode/DesignExcelSumFormula/MainApp.java
+++ leetcode/DesignExcelSumFormula/MainApp.java
@@ -4,36 +4,37 @@ import java.util.*;
 
 // 12ms 70.30% 43.1MB 52.12%
 class Excel {
-    private Cell[][] data;
+    private Cell[][] sheet;
     public Excel(int h, char w) {
-        data = new Cell[h+1][w-'A'+1];
+        sheet = new Cell[h+1][w-'A'+1];
     }
     public void set(int r, char c, int v) {
         int y = r, x = c-'A';
-        if (data[y][x] == null) {
-            data[y][x] = new Cell(v);
+        if (sheet[y][x] == null) {
+            sheet[y][x] = new Cell(v);
         } else {
-            data[y][x].setVal(v);
+            sheet[y][x].setVal(v);
         }
     }
     public int get(int r, char c) {
         int y = r, x = c-'A';
-        if (data[y][x] == null) {
+        if (sheet[y][x] == null) {
             return 0;
         }
-        return data[y][x].getVal();
+        return sheet[y][x].getVal();
     }
     public int sum(int r, char c, String[] nums) {
         int y = r, x = c-'A';
-        if (data[y][x] == null) {
-            data[y][x] = new Cell(nums);
+        if (sheet[y][x] == null) {
+            sheet[y][x] = new Cell(nums);
         } else {
-            data[y][x].setFormula(nums);
+            sheet[y][x].setFormula(nums);
         }
-        return data[y][x].getVal();
+        return sheet[y][x].getVal();
     }
     private class Cell {
         private int val = 0;
+        // {cell: freq of cell}
         private Map<Cell, Integer> formula = new HashMap<>();
         public Cell(int val) {
             setVal(val);
@@ -70,10 +71,10 @@ class Excel {
             return pos;
         }
         private void addFormulaCell(int y, int x) {
-            if (data[y][x] == null) {
-                data[y][x] = new Cell(0);
+            if (sheet[y][x] == null) {
+                sheet[y][x] = new Cell(0);
             }
-            Cell rangeCell = data[y][x];
+            Cell rangeCell = sheet[y][x];
             formula.put(rangeCell, formula.getOrDefault(rangeCell, 0) + 1);
         }
         private int getVal() {
