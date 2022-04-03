7ecfe71d01331eb6986007b2ea59d7b000c6050a
David Sun <iamslash@gmail.com>
2022-04-03 22:04:43 +0900

    Accept FindLeavesofBinaryTree

--- leetcode/FindLeavesofBinaryTree/MainApp.java
+++ leetcode/FindLeavesofBinaryTree/MainApp.java
@@ -44,3 +44,35 @@ class Solution {
         return ans;
     }
 }
+
+// 0ms 100.00% 40.6MB 93.56%
+// DFS
+// O(N) O(lgN)
+class Solution {
+    private int dfs(List<List<Integer>> ans, TreeNode u) {
+        int lv = 0;
+        // base
+        if (u == null) {
+            return 0;
+        }
+        if (u.left == null && u.right == null) {
+            ans.get(lv).add(u.val);
+            return lv + 1;            
+        }
+        // recursion
+        int l = dfs(ans, u.left);
+        int r = dfs(ans, u.right);
+        int maxLv = Math.max(l, r);
+        while (ans.size() - 1 < maxLv) {
+            ans.add(new ArrayList<>());
+        } 
+        ans.get(maxLv).add(u.val);
+        return maxLv + 1;
+    }
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+        dfs(ans, root);
+        return ans;
+    }
+}
