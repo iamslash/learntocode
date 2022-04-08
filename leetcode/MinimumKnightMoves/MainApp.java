fd01b0690aee00283d0174631049b30c47cb7f81
David Sun <iamslash@gmail.com>
2022-04-09 00:25:47 +0900

    Update MinimumKnightMoves

--- leetcode/MinimumKnightMoves/MainApp.java
+++ leetcode/MinimumKnightMoves/MainApp.java
@@ -12,10 +12,10 @@ class Solution {
     public int minKnightMoves(int ex, int ey) {
         int step = 0;
         int[][] dirs = new int[][]{{1,2}, {2,1}, {2,-1}, {1,-1}, {-1,2}, {-2,1}, {-2,-1}, {-1,-2}};
-        Set<int[]> visit = new HashSet<>();
+        Set<String> visit = new HashSet<>();
         Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[]{0, 0});
-        visit.add(new int[]{0, 0});
+        visit.add("0,0");
         while (!q.isEmpty()) {
             int size = q.size();
             while (size-- > 0) {
@@ -27,10 +27,11 @@ class Solution {
                 for (int[] dir : dirs) {
                     int nx = x + dir[0], ny = y + dir[1];
                     int[] pos = new int[]{nx, ny};
-                    if (!visit.contains(pos) &&
+                    String posStr = String.format("%d,%d", nx, ny);
+                    if (!visit.contains(posStr) &&
                         ((ex * nx) >= 0 && (ey * ny) >= 0) || step < 3) {
                         q.add(pos);
-                        visit.add(pos);
+                        visit.add(posStr);
                     }
                 }
             }
@@ -42,5 +43,17 @@ class Solution {
 
 public class MainApp {
   public static void main(String[] args) {
+      {
+          Set<int[]> visit = new HashSet<>();
+          visit.add(new int[]{0, 1});
+          System.out.println(visit.contains(new int[]{0, 0}));
+          System.out.println(visit.contains(new int[]{0, 1}));
+      }
+      {
+          Set<String> visit = new HashSet<>();
+          visit.add("0,1");
+          System.out.println(visit.contains("0,0"));
+          System.out.println(visit.contains("0,1"));
+      }
   }
 }
