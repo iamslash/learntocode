f6f0a240f6efb80c7f9f021515fbca7fbb9dd112
David Sun <iamslash@gmail.com>
2022-04-13 09:38:38 +0900

    Accept CheckifaParenthesesStringCanBeValid

--- leetcode3/CheckifaParenthesesStringCanBeValid/MainApp.java
+++ leetcode3/CheckifaParenthesesStringCanBeValid/MainApp.java
@@ -6,16 +6,41 @@ import java.util.*;
 //      s: ))()))
 //         *)*)**
 
+// 21ms 71.40% 43.7MB 82.95%
 // greedy, back and forth
 // O(N) O(N)
 class Solution {
-    private boolean valid(String s, String locked, char op) {
-    }
     public boolean canBeValid(String s, String locked) {
         if (s.length() % 2 != 0) {
             return false;
         }
-        return valid(s, locked, '(') && valud(s, locked, ')');
+        int wild = 0, left = 0, right = 0, n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (locked.charAt(i) == '0') {
+                wild++;
+            } else if (s.charAt(i) == '(') {
+                left++;
+            } else if (s.charAt(i) == ')') {
+                right++;
+            }
+            if (wild + left - right < 0) {
+                return false;
+            }
+        }
+        wild = left = right = 0;
+        for (int i = n-1; i >= 0; --i) {
+            if (locked.charAt(i) == '0') {
+                wild++;
+            } else if (s.charAt(i) == '(') {
+                left++;
+            } else if (s.charAt(i) == ')') {
+                right++;
+            }
+            if (wild + right - left < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
@@ -23,3 +48,4 @@ public class MainApp {
   public static void main(String[] args) {
   }
 }
+
