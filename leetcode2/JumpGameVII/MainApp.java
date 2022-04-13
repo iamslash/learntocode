1ddc06af6cb4d9e37e2a4e60fe238bc4421904ef
David Sun <iamslash@gmail.com>
2022-04-14 07:08:33 +0900

    Accept JumpGameVII

--- leetcode2/JumpGameVII/MainApp.java
+++ leetcode2/JumpGameVII/MainApp.java
@@ -6,27 +6,24 @@
 // s: 011*1*
 //         j
 
-// Time Limit Exceeded
+// 9ms 93.21% 43.2MB 89.81%
 // two pointers
 // O(N) O(N)
 class Solution {
     public boolean canReach(String s, int minJump, int maxJump) {
         int n = s.length();
         char[] road = s.toCharArray();
-        for (int i = 0; i < n; ++i) {
-            // System.out.printf("i: %d\n", i);
+        for (int i = 0, j = 0; i < n; ++i) {
             if (i == 0 || road[i] == '*') {
-                int beg = i + minJump;
+                int beg = Math.max(j, i + minJump);
                 int end = Math.min(n, i + maxJump + 1);
-                // System.out.printf("i: %d, beg: %d, end: %d\n", i, beg, end);
-                for (int j = beg; j < end; ++j) {
+                for (j = beg; j < end; ++j) {
                     if (road[j] == '0') {
                         road[j] = '*';
                     }
                 }
             }
         }
-        // System.out.println(new String(road));
         return road[n-1] == '*';
     }
 }
