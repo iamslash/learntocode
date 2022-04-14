5829690ef9923789b9686fe7576baf7d38ff290f
David Sun <iamslash@gmail.com>
2022-04-15 07:58:17 +0900

    Add NumberofWaystoBuildSturdyBrickWall

---
+++ leetcode3/NumberofWaystoBuildSturdyBrickWall/MainApp.java
@@ -0,0 +1,14 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+class Solution {
+    public int buildWall(int height, int width, int[] bricks) {
+        
+    }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
