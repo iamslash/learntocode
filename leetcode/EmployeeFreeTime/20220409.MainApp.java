1d921200aa8e49311a453eb4a94c3293a0d6b806
David Sun <iamslash@gmail.com>
2022-04-16 20:04:09 +0900

    Update EmployeeFreeTime

--- leetcode/EmployeeFreeTime/20220409.MainApp.java
+++ leetcode/EmployeeFreeTime/20220409.MainApp.java
@@ -5,45 +5,45 @@ import java.util.*;
 class Interval {
     public int start;
     public int end;
+
     public Interval() {}
+
     public Interval(int _start, int _end) {
         start = _start;
         end = _end;
     }
 };
 
-// [1,2],[5,6] [1,3] [4,10]
-//
-//       4           10
-// 1   3
+// 1,2 5,6
+// 1,3 4,10
+
 // 1 2     5 6
+// 1   3 4     10
 
-// [1,3],[6,7] [2,4],[2,5],[9,12]
-// 1   3     6 7
-//   2   4
-//   2     5       9 12
-
+// 13ms 73.59% 55.5MB 14.28%
 // sort
-// O(N) O(N)
+// O(NlgN) O(N)
 class Solution {
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
         List<Interval> ans = new ArrayList<>();
-        List<Interval> evtList = new ArrayList<>();
-        for (List<Interval> l : schedule) {
-            evtList.addAll(l);
+        List<Interval> intervalList = new ArrayList<>();
+        for (List<Interval> scheduleList : schedule) {
+            intervalList.addAll(scheduleList);
         }
-        evtList.sort((a, b) -> {
+        intervalList.sort((a, b) -> {
                 if (a.start == b.start) {
                     return a.end - b.end;
                 }
                 return a.start - b.start;
             });
-        int prvEnd = -1;
-        for (Interval evt : evtList) {
-            if (prvEnd > 0 && prvEnd < evt.start) {
-                ans.add(new Interval(prvEnd, evt.start));
+        Interval prvEvt = intervalList.get(0);
+        for (Interval curEvt : intervalList) {
+            if (curEvt.start <= prvEvt.end) {
+                prvEvt.end = Math.max(prvEvt.end, curEvt.end);
+            } else {
+                ans.add(new Interval(prvEvt.end, curEvt.start));
+                prvEvt = curEvt;
             }
-            prvEnd = evt.end;
         }
         return ans;
     }
