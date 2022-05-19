b85b1d92620c5be1800c338142b4ca8fd66d9530
David Sun <iamslash@gmail.com>
2022-05-20 07:37:28 +0900

    Accept MinimumCosttoSetCookingTime

--- leetcode3/MinimumCosttoSetCookingTime/MainApp.java
+++ leetcode3/MinimumCosttoSetCookingTime/MainApp.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 // Idea: greedy
 //
-// There are 2 cases for minimum cost
+// startAt should be represented to 4 digits.  2 for min, 2 for sec.
+// 3 digits of sec can not be represented. Consider startAt to 2
+// digits 2 digits of sec. There are just 2 cases.
 // 0) min, sec
-// 1) min-1, sec+60
+// 1) min-1, sec+60 (sec+60 <= 99)
 // 
 // ex)
 //      startAt: 1
@@ -23,13 +25,37 @@ import java.util.*;
 //    1 min 16 sec    1 + 2 + 2 + 1 + 2 = 8
 //    0 min 76 sec    1 + 2 + 1 + 2 = 6
 
+// 11ms 13.33% 39.8MB 77.95%
+// greedy
+// O(N) O(1)
 class Solution {
-    public int minCostSetTime(int startAt, int moveCost, int pushCost, int targetSeconds) {
-        
+    private int getCost(int startAt, int moveCost, int pushCost, int min, int sec) {
+        // System.out.printf("min: %d, sec: %d\n", min, sec);
+        if (min < 0 || min >= 100 || sec >= 100) {
+            return Integer.MAX_VALUE;
+        }
+        int cost = 0, prevAt = startAt;
+        StringBuilder sb = new StringBuilder();
+        if (min > 0) {
+            sb.append(Integer.toString(min));
+            sb.append(String.format("%02d", sec));
+        } else {
+            sb.append(Integer.toString(sec));
+        }
+        // System.out.println(sb.toString());
+        for (char c : sb.toString().toCharArray()) {
+            if (prevAt != c-'0') {
+                cost += moveCost;
+            }
+            cost += pushCost;
+            prevAt = c-'0';
+        }
+        return cost; 
     }
-}
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
+    public int minCostSetTime(int startAt, int moveCost, int pushCost, int targetSeconds) {
+        int min = targetSeconds / 60;
+        int sec = targetSeconds % 60;
+        return Math.min(getCost(startAt, moveCost, pushCost, min, sec),
+                        getCost(startAt, moveCost, pushCost, min-1, sec+60));
+    }
 }
