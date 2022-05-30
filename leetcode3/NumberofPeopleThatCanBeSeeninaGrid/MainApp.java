4cc78bd06302320c6c71b06c5b8ae0d3579c26d9
David Sun <iamslash@gmail.com>
2022-05-31 07:22:33 +0900

    Accept NumberofPeopleThatCanBeSeeninaGrid

--- leetcode3/NumberofPeopleThatCanBeSeeninaGrid/MainApp.java
+++ leetcode3/NumberofPeopleThatCanBeSeeninaGrid/MainApp.java
@@ -2,28 +2,46 @@
 
 import java.util.*;
 
-//                  i
+//          
 // heights: 3 1 4 2 5
-//                    j
-//          2 1 2 1
+//        j
+//     deq: 4 5
+//     ans: 2 1 2 1 0
 
-// two pointers
-// O(HW) O(1)
+//          i
+// heights: 5 1 
+//            j  
+//          3 1
+//          4 1
+//     deq:
+
+// 30ms 91.78% 66MB 91.78%
+// mono stack
+// O(HW) O(HW)
 class Solution {
+    private void fill(int[][] heights, int[][] ans,
+                      int y, int x, int dy, int dx) {
+        Deque<Integer> deq = new ArrayDeque<>();
+        for (int i = y, j = x; i >= 0 && j >= 0; i += dy, j += dx) {
+            while (deq.size() > 0 && deq.peekFirst() < heights[i][j]) {
+                deq.removeFirst();
+                ans[i][j]++;
+            }
+            ans[i][j] += deq.size() > 0 ? 1 : 0;
+            if (deq.size() > 0 && deq.peekFirst() == heights[i][j]) {
+                continue;
+            }
+            deq.addFirst(heights[i][j]);
+        }
+    }
     public int[][] seePeople(int[][] heights) {
         int h = heights.length, w = heights[0].length;
         int[][] ans = new int[h][w];
-        int i = 0, j = 1;
         for (int y = 0; y < h; ++y) {
-            while (j < n) {
-                // move j
-                while (j < n && heights[y][i] < heights[y][j]) {
-                    j++;
-                }
-                // move i
-                while (i < j) {
-                }
-            }
+            fill(heights, ans, y, w-1, 0, -1);
+        }
+        for (int x = 0; x < w; ++x) {
+            fill(heights, ans, h-1, x, -1, 0);
         }
         return ans;
     }
