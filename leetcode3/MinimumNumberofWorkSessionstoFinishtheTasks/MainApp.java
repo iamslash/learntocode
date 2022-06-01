d9b3488d551fd0e192abaaaa37671aa178dddc53
David Sun <iamslash@gmail.com>
2022-06-02 08:05:43 +0900

    Add MinimumNumberofWorkSessionstoFinishtheTasks

---
+++ leetcode3/MinimumNumberofWorkSessionstoFinishtheTasks/MainApp.java
@@ -0,0 +1,33 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+// sessionTime: 8    
+//       tasks: 3 1 3 1 1
+//              1 1 1 3 3  
+//                1   1
+//              3   3   1
+//       tasks: 3 1 3 1
+//              1 1 3 3
+//                1   1
+//              3   3
+
+// sessionTime: 5
+//       tasks: 3 1 3 1 1
+//              1 1 1 3 3
+//                1   1
+//              3   3   1
+//                      i
+//      prvSum: 5
+//         cnt: 2
+
+// recursive dynamic programming
+class Solution {
+    public int minSessions(int[] tasks, int sessionTime) {
+    }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
