8d669fc201124a9f08cd1b319ee95376ce474c09
David Sun <iamslash@gmail.com>
2022-06-09 21:19:09 +0900

    Accept MaximumWhiteTilesCoveredbyaCarpet

--- leetcode3/MaximumWhiteTilesCoveredbyaCarpet/MainApp.java
+++ leetcode3/MaximumWhiteTilesCoveredbyaCarpet/MainApp.java
@@ -5,26 +5,36 @@ import java.util.*;
 //         b
 //  tiles: 1,5 10,11 12,18 20,25 30,32
 //           e
-// maxLen:   
+// maxLen:
 
-// sort
+// ========
+// ----- -----
+//     i     j
+
+// ========
+// ----- --
+//     i  j
+
+// 76ms 68.18% 75.2MB 84.20%
+// sort, sliding window
 // O(NlgN) O(1)
 class Solution {
     public int maximumWhiteTiles(int[][] tiles, int carpetLen) {
         Arrays.sort(tiles, (a, b) -> a[0] - b[0]);
-        int maxLen = 0, n = tiles.length, prvBeg = -1, prvEnd = -1;
-        for (int i = 0; i < n; ++i) {
-            if (i == 0 || prvEnd + 1 != tiles[i][0]) {
-                prvBeg = tiles[i][0];
+        int maxLen = 0, covered = 0, i = 0, j = 0, n = tiles.length;
+        while (maxLen < carpetLen && j < n) {
+            if (i == j ||
+                tiles[i][0] + carpetLen > tiles[j][1]) {
+                covered += Math.min(carpetLen, tiles[j][1] - tiles[j][0] + 1);
+                maxLen = Math.max(maxLen, covered);
+                j++;
+            } else {
+                int part = Math.max(0, tiles[i][0] + carpetLen - tiles[j][0]);
+                maxLen = Math.max(maxLen, covered + part);
+                covered -= (tiles[i][1] - tiles[i][0] + 1);
+                i++;
             }
-            prvEnd = tiles[i][1];
-            maxLen = Math.max(maxLen, Math.min(prvEnd - prvBeg + 1, carpetLen));
         }
         return maxLen;
     }
-}
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
 }
