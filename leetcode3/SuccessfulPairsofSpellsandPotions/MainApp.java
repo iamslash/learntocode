31eabd81ad0b0997c745451b754df50ffa098e72
David Sun <iamslash@gmail.com>
2022-06-14 08:13:19 +0900

    Add SuccessfulPairsofSpellsandPotions

---
+++ leetcode3/SuccessfulPairsofSpellsandPotions/MainApp.java
@@ -0,0 +1,10 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+// binary search
+// O(NlgM) O(N)
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+    }
+}
