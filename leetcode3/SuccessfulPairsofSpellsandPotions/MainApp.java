9dd3d3c8e6c3eb5079c1e6517371e3cc16240a97
David Sun <iamslash@gmail.com>
2022-06-18 10:26:41 +0900

    Accept SuccessfulPairsofSpellsandPotions

--- leetcode3/SuccessfulPairsofSpellsandPotions/MainApp.java
+++ leetcode3/SuccessfulPairsofSpellsandPotions/MainApp.java
@@ -2,6 +2,7 @@
 
 import java.util.*;
 
+// 282ms 5.08% 152.5MB 11.63%
 // binary search
 // O(NlgM) O(N)
 class Solution {
@@ -13,9 +14,14 @@ class Solution {
         for (int i = m-1; i >= 0; --i) {
             ptn2idxMap.put((long)potions[i], i);
         }
+        // for (int i = 0; i < m; ++i) {
+        //     ptn2idxMap.put((long)potions[i], i);
+        // }        
+        System.out.println(ptn2idxMap);
         for (int i = 0; i < n; ++i) {
             long need = (success + spells[i] - 1) / spells[i];
             spells[i] = m - ptn2idxMap.ceilingEntry(need).getValue();
+            // System.out.printf("need: %d, ceilingKey: %d, celingVal: %d\n", need, ptn2idxMap.ceilingEntry(need).getKey(), ptn2idxMap.ceilingEntry(need).getValue());
         }
         return spells;
     }
