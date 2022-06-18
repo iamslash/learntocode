5234aaf3075e7a1489244af3d9e38eb1a2a3a976
David Sun <iamslash@gmail.com>
2022-06-18 10:19:49 +0900

    Update SuccessfulPairsofSpellsandPotions

--- leetcode3/SuccessfulPairsofSpellsandPotions/MainApp.java
+++ leetcode3/SuccessfulPairsofSpellsandPotions/MainApp.java
@@ -6,5 +6,17 @@ import java.util.*;
 // O(NlgM) O(N)
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length, m = potions.length;
+        Arrays.sort(potions);
+        TreeMap<Long, Integer> ptn2idxMap = new TreeMap<>();
+        ptn2idxMap.put(Long.MAX_VALUE, m);
+        for (int i = m-1; i >= 0; --i) {
+            ptn2idxMap.put((long)potions[i], i);
+        }
+        for (int i = 0; i < n; ++i) {
+            long need = (success + spells[i] - 1) / spells[i];
+            spells[i] = m - ptn2idxMap.ceilingEntry(need).getValue();
+        }
+        return spells;
     }
 }
