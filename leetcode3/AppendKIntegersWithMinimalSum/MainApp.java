d1efcfe7914564b3a8c24f23b7093b101529ae7e
David Sun <iamslash@gmail.com>
2022-06-19 22:04:44 +0900

    Add AppendKIntegersWithMinimalSum

---
+++ leetcode3/AppendKIntegersWithMinimalSum/MainApp.java
@@ -0,0 +1,14 @@
+// Copyright (C) 2022 by iamslash
+
+import java.utill.*;
+
+class Solution {
+    public long minimalKSum(int[] nums, int k) {
+        Arrays.sort(nums);
+    }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
