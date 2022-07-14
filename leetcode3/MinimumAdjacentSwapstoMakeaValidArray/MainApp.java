f7d73f7ad5b8e2fc833817c63dc3278181efc3dd
David Sun <iamslash@gmail.com>
2022-07-15 07:54:30 +0900

    Add MinimumAdjacentSwapstoMakeaValidArray

---
+++ leetcode3/MinimumAdjacentSwapstoMakeaValidArray/MainApp.java
@@ -0,0 +1,20 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+// nums: 3 4 5 5 3 1
+//             3 1 5
+//           3 1 5 5
+//         3 1 4 5 5
+
+// sort
+// O(NlgN) O(N)
+class Solution {
+    public int minimumSwaps(int[] nums) {
+    }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
