de61547815038570c204d9c073be78ea6f6a7f41
David Sun <iamslash@gmail.com>
2022-08-19 07:58:13 +0900

    Add ConstructSmallestNumberFromDIString

---
+++ leetcode3/ConstructSmallestNumberFromDIString/MainApp.java
@@ -0,0 +1,17 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+// pattern: I I I D I D D D
+//                  .
+//          1 2 3 4  
+//                  5
+//                  
+
+class Solution {
+    public String smallestNumber(String pattern) {
+        StringBuilder ans = new StringBuilder();
+        StringBuilder buf = new StringBuilder();
+        return ans;
+    }
+}
