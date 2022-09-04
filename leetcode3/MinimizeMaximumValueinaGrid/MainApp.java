d8fa2d36b2438ff788daaf295da02ef572d1ce1a
David Sun <iamslash@gmail.com>
2022-09-04 13:27:06 +0900

    Update MinimizeMaximumValueinaGrid

---
+++ leetcode3/MinimizeMaximumValueinaGrid/MainApp.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+// grid:
+// 3 1
+// 2 5
+// 
+// pq: 1 2 3 5
+// maxRows:
+// maxCols:
+
+// priority queue
+// O(HW*lgHW) O(HW)
+class Solution {
+    public int[][] minScore(int[][] grid) {
+        int h = grid.length, w = grid[0].length;
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> grid[a[0]][a[1]] - grid[b[0]][b[1]]);
+        for (int i = 0; i < h; ++i) {
+            for (int j = 0; j < w; ++j) {
+                pq.offer(new int[]{i, j});
+            }
+        }
+        int[] maxRows = new int[h], maxCols = new int[w];
+        int[][] ans = new int[h][w];
+        while (pq.size() > 0) {
+            int[] item = pq.poll();
+            int row = item[0], col = item[1];
+            int maxVal = Math.max(maxRows[row], maxCols[col]) + 1;
+            ans[row][col] = maxVal;
+            maxRows[row] = maxVal;
+            maxCols[col] = maxVal;
+        }
+        return ans;
+    }
+}
---
+++ leetcode3/MinimizeMaximumValueinaGrid/README.md
@@ -0,0 +1,48 @@
+# Problem
+
+[Minimize Maximum Value in a Grid](https://leetcode.com/problems/minimize-maximum-value-in-a-grid/)
+
+2 차원 배열 `M[][]` 가 주어진다. `M[][]` 의 값들은 distinct 하다.
+인접한 값들의 크기 순서는 지키고 `max(M[][])` 가 최소가 되도록 값들을
+변경해 보자. 이때 변경된 `M[][]` 의 값들은 distinct 할 필요는 없다.
+그리고 변경된 `M[][]` 를 구하라.
+
+# Idea
+
+예를 들어 `[[3,1],[2,5]]` 의 경우를 살펴보자.
+
+```
+M:
+3 1    3 1    2 1    2 1 
+2 5    1 5    1 5    1 2
+```
+
+따라서 답은 다음과 같다. 
+
+```
+2 1
+1 2
+```
+
+다음과 같은 자료구조를 선언한다.
+
+* `PriorityQueue<Integer> pq` 를 선언하여 `M[][]` 를 삽입한다.  `M[][]`
+의 오름차순을 유지한다.
+* `int[] maxRows` 를 선언하여 행 마다 최대값을 저장한다. 
+* `int[] maxCols` 를 선언하여 열 마다 최대값을 저장한다.
+
+`max(M[][])` 의 값이 최소가 되려면 `1` 부터 하나씩 값을 늘려 `M[][]`
+에 배정하면 된다. 이때 `M[y][x]` 와 인접한 값들과 크기 순서를 원래대로
+유지해야 한다. 상하 좌우 서로 인접한 값들끼리의 순서가 중요하기 때문에
+`max(maxRows[y], maxCols[x]) + 1` 를 `M[y][x]` 에 저장한다. 그리고
+`maxRows[], maxCols[]` 를 업데이트 한다.
+
+# Implementation
+
+* [java11](MainApp.java)
+
+# Complexity
+
+```
+O(HW*lgHW) O(HW)
+```
