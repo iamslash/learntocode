e5672717b71e640c4d1fc8ee41500b29f8f5da69
David Sun <iamslash@gmail.com>
2022-09-06 08:20:19 +0900

    Add FindSubarraysWithEqualSum

---
+++ leetcode3/FindSubarraysWithEqualSum/a.cpp
@@ -0,0 +1,30 @@
+/* Copyright (C) 2022 by iamslash */
+
+#include <cstdio>
+
+using namespace std;
+
+#include <vector>
+#include <set>
+
+// sliding window, set
+// O(N) O(N)
+class Solution {
+public:
+  bool findSubarrays(vector<int>& nums) {
+    int n = nums.size();
+    set<int> sumSet;
+    for (int i = 1; i < n; ++i) {
+      int sum = nums[i-1] + nums[i];
+      if (sumSet.find(sum) != sumSet.end()) {
+        return true;
+      }
+      sumSet.insert(sum);
+    }
+    return true;
+  }
+};
+
+int main() {
+  return 0;
+}
