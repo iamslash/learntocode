8321244e3eebe6edf96100665c11607a69aedc03
David Sun <iamslash@gmail.com>
2022-10-03 15:59:24 +0900

    Accept NumberofCommonFactors

---
+++ leetcode3/NumberofCommonFactors/MainApp.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+// 2ms 25.00% 41.1MB 50.00%
+class Solution {
+    public int commonFactors(int a, int b) {
+        int ans = 1;
+        int bnd = Math.min(a, b);
+        for (int factor = 2; factor <= bnd; ++factor) {
+            if (a % factor == 0 && b % factor == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+
+// 0ms 100.00% 40.9MB 75.00%
+class Solution {
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+    public int commonFactors(int a, int b) {
+        int ans = 1, bnd = gcd(a, b);
+        for (int factor = 2; factor <= bnd; ++factor) {
+            if (a % factor == 0 && b % factor == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
