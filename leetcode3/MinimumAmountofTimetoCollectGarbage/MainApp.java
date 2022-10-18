21d62c1eb2e2c4dacc0fedad7a3a7add23d71f34
David Sun <iamslash@gmail.com>
2022-10-18 21:29:24 +0900

    Accept MinimumAmountofTimetoCollectGarbage

--- leetcode3/MinimumAmountofTimetoCollectGarbage/MainApp.java
+++ leetcode3/MinimumAmountofTimetoCollectGarbage/MainApp.java
@@ -2,7 +2,15 @@
 
 import java.util.*;
 
-// brute force
+// garbage: G P GP GG
+//  travel:   2  4  3
+//               i
+//   lasts: P M G
+//          2 0 3
+//    cost: 8
+
+// 32ms 85.14% 68.1MB 94.18%
+// hash map
 // O(N) O(1)
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
@@ -10,7 +18,6 @@ class Solution {
         int[] lasts = new int[3];
         for (int i = 0; i < garbage.length; ++i) {
             cost += garbage[i].length();
-            Arrays.fill(uniqCnts, 0);
             for (int j = 0; j < garbage[i].length(); ++j) {
                 char c = garbage[i].charAt(j);
                 if (c == 'P') {
@@ -24,7 +31,7 @@ class Solution {
         }
         for (int i = 0; i < travel.length; ++i) {
             for (int j = 0; j < lasts.length; ++j) {
-                if (lasts[j] - 1 <= i) {
+                if (lasts[j] - 1 >= i) {
                     cost += travel[i];
                 }
             }
