9d2b9315b1a0d9f618b2daee9ec4d8795d1723c3
David Sun <iamslash@gmail.com>
2022-10-21 09:09:22 +0900

    Accept NumberofWaystoSelectBuildings

--- leetcode3/NumberofWaystoSelectBuildings/MainApp.java
+++ leetcode3/NumberofWaystoSelectBuildings/MainApp.java
@@ -2,86 +2,39 @@
 
 import java.util.*;
 
-// s: 001101
-//    0 1 0
-//    0  10
-//     01 0
-//     0 10
-//      1 01
-//       101
+//            
+// s: 0 0 1 1 0 1
+//    0   1   0
+//    0     1 0
+//      0 1   0
+//      0   1 0
+//        1   0 1
+//          1 0 1
 
-// Time Limit Exceeded
-// brute force
-// O(N^3) O(1)
-class Solution {
-    public long numberOfWays(String s) {
-        int n = s.length(), cnt = 0;
-        for (int i = 0; i < n-2; ++i) {
-            for (int j = i+1; j < n-1; ++j) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    continue;
-                }
-                for (int k = j+1; k < n; ++k) {
-                    if (s.charAt(j) == s.charAt(k)) {
-                        continue;
-                    }
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-}
-
-// 51ms 68.98& 63.2MB 68.16%
-// math
-// O(N) O(1)
-class Solution {
-    public long numberOfWays(String s) {
-        long n = s.length(), cnt = 0;
-        long leftZeroCnt = s.charAt(0) == '0' ? 1 : 0, rightZeroCnt = 0;
-        long leftOneCnt = s.charAt(0) == '1' ? 1 : 0, rightOneCnt = 0;
-        for (int i = 1; i < n; ++i) {
-            char c = s.charAt(i);
-            rightZeroCnt += c == '0' ? 1 : 0;
-            rightOneCnt += c == '1' ? 1 : 0; 
-        }
-        // System.out.printf("leftZeroCnt: %d, rightZeroCnt: %d\n", leftZeroCnt, rightZeroCnt);
-        // System.out.printf("leftOneCnt: %d, rightOneCnt: %d\n", leftOneCnt, rightOneCnt);
-        for (int i = 1; i < n-1; ++i) {
-            if (s.charAt(i) == '0') {
-                cnt += leftOneCnt * rightOneCnt;
-                leftZeroCnt++;
-                rightZeroCnt--;
-            } else {
-                cnt += leftZeroCnt * rightZeroCnt;
-                leftOneCnt++;
-                rightOneCnt--;
-            }
-        }        
-        return cnt;
-    }
-}
-
+// Idea: iterative dynamic programming
 //
-// s: 001101
-//    0 1 0
-//    0  10
-//     01 0
-//     0 10
-//      1 01
-//       101
+//  _1: count ends with 1
+// _01: count ends with 01
+//  _0: count ends with 0
+// _10: count ends with 10
+//
+//   current character is 0
+//      total count: total count + _01
+//       _0 += 1
+//      _10 += _1
+//   current character is 1
+//      total count: total count + _10
+//       _1 += 1
+//      _01 += _0
 
-// 22ms 98.30% 68.4MB 59.62%
+// 34ms 85.67% 69MB 35.54%
 // iterative dynamic programming
 // O(N) O(1)
 class Solution {
     public long numberOfWays(String s) {
-        long _01 = 0;
-        long _10 = 0;
-        long _0 = 0;
-        long _1 = 0;
         long cnt = 0;
+        // count ends with 1, 01, 0, 10
+        long _1 = 0, _01 = 0, _0 = 0, _10 = 0;
         for (char c : s.toCharArray()) {
             if (c == '0') {
                 cnt += _01;
@@ -95,9 +48,4 @@ class Solution {
         }
         return cnt;
     }
-}
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
 }
