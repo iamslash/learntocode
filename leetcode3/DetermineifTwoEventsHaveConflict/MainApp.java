5b8008d4452162ef9e620db02a530747677e6c22
David Sun <iamslash@gmail.com>
2022-10-24 08:30:21 +0900

    Accept DetermineifTwoEventsHaveConflict

--- leetcode3/DetermineifTwoEventsHaveConflict/MainApp.java
+++ leetcode3/DetermineifTwoEventsHaveConflict/MainApp.java
@@ -2,9 +2,13 @@
 
 import java.util.*;
 
+// 1ms 90.00% 41.6MB 40.00%
+// geometry
+// O(1) O(1)
 class Solution {
-    public boolean haveConflict(String[] event1, String[] event2) {
-        
+    public boolean haveConflict(String[] evt1, String[] evt2) {
+        return evt1[0].compareTo(evt2[1]) <= 0 &&
+            evt2[0].compareTo(evt1[1]) <= 0;
     }
 }
 
