56e21def3469b6f2ea8bf503f00425cd5553c801
David Sun <iamslash@gmail.com>
2022-10-25 21:55:30 +0900

    Add BitwiseXORofAllPairings

---
+++ leetcode3/BitwiseXORofAllPairings/MainApp.java
@@ -0,0 +1,19 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+// nums1: a b
+// nums2: c d e
+//        a ^ c ^ a ^ d ^ a ^ e
+//        b   c   b   d   b   e
+
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        
+    }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
+}
