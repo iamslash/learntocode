534e26ddd255f4ed69c2cdbcd82ed072990b4e05
David Sun <iamslash@gmail.com>
2022-10-25 09:30:20 +0900

    Accept ConstructSmallestNumberFromDIString

--- leetcode3/ConstructSmallestNumberFromDIString/MainApp.java
+++ leetcode3/ConstructSmallestNumberFromDIString/MainApp.java
@@ -2,16 +2,26 @@
 
 import java.util.*;
 
-// pattern: I I I D I D D D
-//                  .
-//          1 2 3 4  
-//                  5
-//                  
+//                      i   
+//            I I D D D I
+//     buf:       3 4 5 6
+//     ans:   1 2 6 5 4 3
 
+// 1ms 92.17% 42MB 55.84%
+// brute force
+// O(N) O(N)
 class Solution {
-    public String smallestNumber(String pattern) {
+    public String smallestNumber(String s) {
         StringBuilder ans = new StringBuilder();
         StringBuilder buf = new StringBuilder();
-        return ans;
+        int n = s.length();
+        for (int i = 0; i <= n; ++i) {
+            buf.append((char)('1' + i));
+            if (i == n || s.charAt(i) == 'I') {
+                ans.append(buf.reverse());
+                buf = new StringBuilder();
+            }
+        }
+        return ans.toString();
     }
 }
