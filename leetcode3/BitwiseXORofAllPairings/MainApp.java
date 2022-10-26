5a36fc00c81aede65039732f49a26b260f0c8cbc
David Sun <iamslash@gmail.com>
2022-10-26 22:27:10 +0900

    Accept BitwiseXORofAllPairings

--- leetcode3/BitwiseXORofAllPairings/MainApp.java
+++ leetcode3/BitwiseXORofAllPairings/MainApp.java
@@ -7,9 +7,23 @@ import java.util.*;
 //        a ^ c ^ a ^ d ^ a ^ e
 //        b   c   b   d   b   e
 
+// 6ms 31.92% 85.7MB 37.01%
+// bit manipulation
+// O(N) O(1)
 class Solution {
     public int xorAllNums(int[] nums1, int[] nums2) {
-        
+        int n = nums1.length, m = nums2.length, ans = 0;
+        if (n % 2 > 0) {
+            for (int num : nums2) {
+                ans ^= num;
+            }
+        }
+        if (m % 2 > 0) {
+            for (int num : nums1) {
+                ans ^= num;
+            }
+        }
+        return ans;
     }
 }
 
