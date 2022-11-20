a85effe6e61bae5d34b3b43d890971848628bb64
David Sun <iamslash@gmail.com>
2022-11-20 13:15:56 +0900

    Accept MinimumNumberofMovestoMakePalindrome

--- leetcode3/MinimumNumberofMovestoMakePalindrome/20221113.MainApp.java
+++ leetcode3/MinimumNumberofMovestoMakePalindrome/20221113.MainApp.java
@@ -2,32 +2,39 @@
 
 import java.util.*;
 
+//        i 
 // s: a a b b
-//    a b b a
+//          j
+
+//    i
+// s: l e t e l t 
+//              j
 
 //        i
-// s: l e t e l t
-//              j
-//    l e e l
+// s: aaaac
+//        j
 
-// 18ms 65.58% 42.5MB 71.70%
+// 54ms 18.18% 76.1MB 8.88%
 // greedy algorithm
 // O(N) O(N)
 class Solution {
     public int minMovesToMakePalindrome(String s) {
         int ans = 0;
-        while (!s.isEmpty()) {
+        while (s.length() > 0) {
             int i = s.indexOf(s.charAt(s.length() - 1));
             if (i == s.length() - 1) {
-                ans += (i / 2);
+                ans += i / 2;
             } else {
                 ans += i;
-                // Remove i-th character
                 s = s.substring(0, i) + s.substring(i + 1);
             }
-            // Remove j-th character
-            s = s.substring(0, s.length() - 1);            
+            s = s.substring(0, s.length() - 1);
         }
         return ans;
     }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
 }
