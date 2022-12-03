5eec08870f9b0b6923a6e5be6b7a3dbf21199c05
David Sun <iamslash@gmail.com>
2022-12-03 21:42:25 +0900

    Add MinimumPenaltyforaShop

---
+++ leetcode3/MinimumPenaltyforaShop/MainApp.java
@@ -0,0 +1,14 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+// customers: Y Y N Y
+//    psOpen: 0 0 1 1
+//   psClose: 1 2 2 3
+//   penalty: 3 2 1         
+
+class Solution {
+    public int bestClosingTime(String customers) {
+        
+    }
+}
