94a8cd61d102849c5c78a5ed34e19cf937865886
David Sun <iamslash@gmail.com>
2022-12-11 14:51:41 +0900

    Accept CheckifWordCanBePlacedInCrossword

--- leetcode3/CheckifWordCanBePlacedInCrossword/20211014.MainApp.java
+++ leetcode3/CheckifWordCanBePlacedInCrossword/20211014.MainApp.java
@@ -1,46 +1,44 @@
-// Copyright (C) 2022 by iamslash
+// Copyright (C) 2021 by iamslash
 
 import java.util.*;
 
-// 29ms 41.59% 94.5MB 73.80%
+// 5ms 83.98% 68.5MB 60.62%
 // brute force
-// O(HW) O(1)
+// O(HWS) O(1)
 class Solution {
     private boolean inside(char[][] board, int y, int x) {
-        int h = board.length, w = board[0].length;
-        return y >= 0 && y < h && x >= 0 && x < w;
+        return y >= 0 && y < board.length && x >= 0 && x < board[0].length;
     }
     public boolean placeWordInCrossword(char[][] board, String word) {
-        int h = board.length, w = board[0].length, n = word.length();
-        int dirs[] = {-1, 0, 1, 0, -1};
+        int h = board.length, w = board[0].length;
+        int dir[] = {-1, 0, 1, 0, -1};
         for (int y = 0; y < h; ++y) {
             for (int x = 0; x < w; ++x) {
+                // Check starting point
                 if (board[y][x] != ' ' && board[y][x] != word.charAt(0)) {
                     continue;
                 }
+                // Check each direction
                 for (int i = 0; i < 4; ++i) {
-                    int dy = dirs[i], dx = dirs[i+1];
+                    int dy = dir[i], dx = dir[i+1];
                     int ny = y, nx = x, idx = 0;
-                    int ey = y + dy * n, ex = x + dx * n;
                     // Check opposite position from the starting position
                     if (inside(board, ny - dy, nx - dx) &&
                         board[ny - dy][nx - dx] != '#') {
                         continue;
                     }
-                    while (idx < word.length() &&
-                           inside(board, ny, nx)) {
+                    while (idx < word.length() && inside(board, ny, nx)) {
                         if (board[ny][nx] == '#' ||
-                            (board[ny][nx] != ' ' &&
-                             board[ny][nx] != word.charAt(idx))) {
+                            (board[ny][nx] != ' ' && board[ny][nx] != word.charAt(idx))) {
                             break;
                         }
-                        idx++;
+                        ++idx;
                         ny += dy;
                         nx += dx;
                     }
+                    // Check end position
                     if (idx == word.length() &&
-                        (!inside(board, ny, nx) ||
-                         board[ny][nx] == '#')) {
+                        (!inside(board, ny, nx) || board[ny][nx] == '#')) {
                         return true;
                     }
                 }
@@ -48,4 +46,9 @@ class Solution {
         }
         return false;
     }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
 }
