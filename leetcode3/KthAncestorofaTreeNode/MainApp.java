e54f88b7b3cbe6f10571cc85a6f6bcb577f1c32a
David Sun <iamslash@gmail.com>
2022-12-11 14:28:33 +0900

    Accept KthAncestorofaTreeNode

---
+++ leetcode3/KthAncestorofaTreeNode/MainApp.java
@@ -0,0 +1,76 @@
+// Copyright (C) 2022 by iamslash
+
+import java.util.*;
+
+// 142ms 70.22% 105.0MB 64.04%
+// binary lifting
+// O(lgN) O(N)
+class TreeAncestor {
+    Map<Integer, List<Integer>> graph = new HashMap<>();
+    Integer[][] C;
+
+    public TreeAncestor(int n, int[] parents) {
+        this.C = new Integer[n][30];
+        for (int u = 0; u < n; ++u) {
+            int p = parents[u];
+            graph.computeIfAbsent(p, v -> new ArrayList<>()).add(u);
+            if (u > 0) {
+                C[u][0] = parents[u];
+            }
+        }
+        dfs(0);
+    }
+    
+    private void dfs(int u) {
+        for (int i = 1; this.C[u][i - 1] != null; ++i) {
+            int p = this.C[u][i - 1];
+            C[u][i] = C[p][i - 1];
+        }
+        for (int v : graph.getOrDefault(u, new ArrayList<>())) {
+            dfs(v);
+        }
+    }
+    
+    public int getKthAncestor(int node, int k) {
+        for (int i = 0; k > 0; ++i) {
+            if (k % 2 == 1) {
+                if (C[node][i] == null) {
+                    return -1;
+                }
+                node = C[node][i];
+            }
+            k /= 2;
+        }
+        return node;
+    }
+}
+
+// binary lifting
+// 174ms 42.70% 98.1MB 82.02%
+class TreeAncestor {
+    int[][] C;
+    int maxPow;
+    public TreeAncestor(int n, int[] parents) {
+        maxPow = (int) (Math.log(n) / Math.log(2)) + 1;
+        C = new int[maxPow][n];
+        C[0] = parents;
+        for (int p = 1; p < maxPow; ++p) {
+            for (int j = 0; j < n; ++j) {
+                int pre = C[p - 1][j];
+                C[p][j] = pre == -1 ? -1 : C[p - 1][pre];
+            }
+        }
+    }
+    public int getKthAncestor(int node, int k) {
+        int maxPow = this.maxPow;
+        while (k > 0 && node > -1) {
+            if (k >= (1 << maxPow)) {
+                node = C[maxPow][node];
+                k -= (1 << maxPow);
+            } else {
+                maxPow--;
+            }
+        }
+        return node;
+    }
+}
--- leetcode3/MinimumSumofSquaredDifference/MainApp.java
+++ leetcode3/MinimumSumofSquaredDifference/MainApp.java
@@ -5,10 +5,13 @@ import java.util.*;
 // nums1: 1 2 3 4
 // nums2: 2 10 20 19
 //    pq: 1 8 15
-//   prv: 17
-//   cnt:  1
 //     k:  0
 //     x:  2
+
+// nums1: 10 10 10 11 5
+// nums2:  1  0  6  6 1
+//    pq:
+//     k: 38
 
 // heap
 // O(NlgN) O(N)
@@ -27,23 +30,14 @@ class Solution {
         int prv = 0, cnt = 0;
         while (k > 0 && !pq.isEmpty()) {
             int top = pq.poll();
-            long x = (prv - top) * cnt;
-            if (x <= k) {
-                prv = top;
-                cnt++;
-                k -= x;
+            if (pq.isEmpty()) {
+                if (top > k) {
+                    pq.offer(top - k);
+                }
             } else {
-                int a = k / cnt;
-                int b = k % cnt;
-                int c = prv - a;
-                for (int i = 0; i < cnt - b; ++i) {
-                    pq.add(c);
-                }
-                for (int i = 0; i < b; ++i) {
-                    pq.add(c - 1);
-                }
-                pq.add(top);
-                k = 0;
+                int gap = Math.min(top - pq.peek() + 1, k);
+                pq.offer(top - gap);
+                k -= gap;
             }
         }
         long ans = 0;
@@ -52,9 +46,4 @@ class Solution {
         }
         return ans;
     }
-}
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
 }
