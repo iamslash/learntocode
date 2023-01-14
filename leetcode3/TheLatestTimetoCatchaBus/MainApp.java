230e699a038702c4c7089e850614be5ac0a12857
David Sun <iamslash@gmail.com>
2023-01-14 18:13:51 +0900

    Accept TheLatestTimetoCatchaBus

--- leetcode3/TheLatestTimetoCatchaBus/MainApp.java
+++ leetcode3/TheLatestTimetoCatchaBus/MainApp.java
@@ -8,34 +8,42 @@ import java.util.*;
 //    pass: 2 17 18 19
 //          j
 //     cnt: 1
-//   tsSet:
+//  pasSet:
 //     ans: 1
 
+// Idea: greedy
+//
+// i for buses
+// j for passengers
+// passengerSet for passengers who have gone
+// The ans should be the last one possible.
+// Consider passenger[j-1] as candidate on loops
+// Consider buses[i] as candidate at last
+
+// 50ms 41.96% 64.8MB 58.74%
+// greedy, sort, two pointers
+// O(N) O(N)
 class Solution {
     public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
         Arrays.sort(buses);
         Arrays.sort(passengers);
-        Set<Integer> tsSet = new HashSet<>();
+        Set<Integer> passengerSet = new HashSet<>();
         int ans = 0, n = buses.length, m = passengers.length;
+        // i for bus, j for passenger
         for (int i = 0, j = 0; i < n; ++i) {
             int cnt = 0;
             while (j < m && cnt < capacity && buses[i] >= passengers[j]) {
-                if (!tsSet.contains(passengers[j] - 1)) {
+                if (!passengerSet.contains(passengers[j] - 1)) {
                     ans = passengers[j] - 1;
                 }
-                tsSet.add(passengers[j]);
+                passengerSet.add(passengers[j]);
                 cnt++;
                 j++;
             }
-            if (cnt < capacity && !tsSet.contains(buses[i])) {
+            if (cnt < capacity && !passengerSet.contains(buses[i])) {
                 ans = buses[i];
             }
         }
         return ans;
     }
-}
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
 }
