c14a51714addfd2170908f68387437eccf169f18
David Sun <iamslash@gmail.com>
2023-02-04 20:26:43 +0900

    Accept AppendCharacterstoStringtoMakeSubsequence

--- leetcode3/AppendCharacterstoStringtoMakeSubsequence/20221221.MainApp.java
+++ leetcode3/AppendCharacterstoStringtoMakeSubsequence/20221221.MainApp.java
@@ -1,20 +1,14 @@
-// Copyright (C) 2023 by iamslash
+// Copyright (C) 2022 by iamslash
 
 import java.util.*;
 
-
-//        i
-// s: coaching
-// t: coc
-//       j
-
-// 5ms 82.86% 42.9MB 93.43%
+// 4ms 98.33% 42.9MB 98.41%
 // two pointers
 // O(N) O(1)
 class Solution {
     public int appendCharacters(String s, String t) {
-        int j = 0, n = s.length(), m = t.length();
-        for (int i = 0; i < n && j < m; ++i) {
+        int i = 0, j = 0, n = s.length(), m = t.length();
+        for (i = 0; i < n && j < m; ++i) {
             if (s.charAt(i) == t.charAt(j)) {
                 j++;
             }
