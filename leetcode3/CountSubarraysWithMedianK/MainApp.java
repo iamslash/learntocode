7838e04452012a686f2c2fc0ab7ebda4fd4f113e
David Sun <iamslash@gmail.com>
2023-02-11 16:03:38 +0900

    Accept CountSubarraysWithMedianK

--- leetcode3/CountSubarraysWithMedianK/MainApp.java
+++ leetcode3/CountSubarraysWithMedianK/MainApp.java
@@ -2,21 +2,7 @@
 
 import java.util.*;
 
-//      k: 1
-//   nums: 2 5 1 4 3 6
-//             1
-//             1 4
-//           5 1
-
-//      k: 1
-//   nums: 2 5 1 4 3 6
-//         i
-//    sum: 2 1 0 1 2 3
-// sumMap:     0 1 2 3
-//             1 1 1 1
-//         
-
-// 17ms 91.70% 56.2MB 81.26%
+// 17ms 81.78% 58.3MB 42.38%
 // hash map
 // O(N) O(N)
 class Solution {
@@ -29,31 +15,20 @@ class Solution {
             }
         }
         long ans = 0;
-        Map<Integer, Integer> sumMap = new HashMap<>();
+        Map<Integer, Integer> sumFreqMap = new HashMap<>();
         int sum = 0;
-        // right side of pos
+        // Right side of pos
         for (int i = pos; i < n; ++i) {
-            if (nums[i] > k) {
-                sum += 1;
-            } else if (nums[i] < k) {
-                sum -= 1;
-            }
-            sumMap.put(sum, sumMap.getOrDefault(sum, 0) + 1);
+            sum += nums[i] > k ? 1 : (nums[i] < k ? -1 : 0);
+            sumFreqMap.put(sum, sumFreqMap.getOrDefault(sum, 0) + 1);
         }
-        System.out.println(sumMap);
-        // left side of pos
+        // Left side of pos
         sum = 0;
         for (int i = pos; i >= 0; --i) {
-            if (nums[i] > k) {
-                sum += 1;
-            } else if (nums[i] < k) {
-                sum -= 1;
-            }
-            ans += sumMap.getOrDefault(0 - sum, 0);  // median of odd
-            ans += sumMap.getOrDefault(1 - sum, 0);  // median of even
-            System.out.printf("i: %d, sum: %d, ans: %d\n", i, sum, ans);
+            sum += nums[i] > k ? 1 : (nums[i] < k ? -1 : 0);
+            ans += sumFreqMap.getOrDefault(0 - sum, 0);
+            ans += sumFreqMap.getOrDefault(1 - sum, 0);
         }
-        
         return (int)ans;
     }
 }
