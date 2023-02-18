36a622bf88aded14089161e09175b63ed210f7c1
David Sun <iamslash@gmail.com>
2023-02-18 12:25:50 +0900

    Accept CounttheNumberofGoodSubsequences

--- leetcode3/CounttheNumberofGoodSubsequences/MainApp.java
+++ leetcode3/CounttheNumberofGoodSubsequences/MainApp.java
@@ -2,6 +2,15 @@
 
 import java.util.*;
 
+// Idea: combination
+//
+// (1+C(n1,k)) * (1+C(n2,k)) * ...... * (1+C(n26,k)) - 1
+// C(n1, k) means combinations of character 'a' and frequency is k.
+// 1+ means not choosing any
+// -1 means empty sequences
+// loops k from 1 to max frequency of [a..z]
+
+// 139ms 34.43% 41.9MB 93.44%
 // binomial coefficient
 // O(N) O(N)
 // difficult
@@ -28,6 +37,58 @@ class Solution {
                 val = val * (1 + coefs[i]) % mod;
             }
             ans = (ans + val - 1) % mod;
+        }
+        return (int)ans;
+    }
+}
+
+// 30ms 70.49% 42.3MB 62.30%
+// binomial coefficient
+// O(N) O(1)
+class Solution {
+    private static long[] fact = new long[10001];
+    private static long[] inv = new long[10001];
+    private static int mod = 1_000_000_007;
+    static {
+        fact[0] = 1;
+        inv[0] = 1;
+        for (int a = 1; a <= 10_000; ++a) {
+            fact[a] = (fact[a-1] * a) % mod;
+            inv[a] = powmod(fact[a], mod-2, mod);
+        }
+    }
+    private static long powmod(long a, long b, int p) {
+        long ans = 1, base = a;
+        while (b > 0) {
+            if (b % 2 == 1) {
+                ans = (ans * base) % p;
+            }
+            base = (base * base) % p;
+            b >>= 1;
+        }
+        return ans;
+    }
+    private long combi(long n, long k, int p) {
+        return (((fact[(int)n] * inv[(int)k]) % p) * inv[(int)(n-k)]) % p;
+    }
+    public int countGoodSubsequences(String s) {
+        long ans = 0, maxFreq = 0, n = s.length();
+        int[] freqs = new int[26];
+        for (int i = 0; i < n; ++i) {
+            freqs[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; ++i) {
+            maxFreq = Math.max(maxFreq, freqs[i]);
+        }
+        for (int k = 1; k <= maxFreq; ++k) {
+            long cnt = 1;
+            for (int i = 0; i < 26; ++i) {
+                if (freqs[i] >= k) {
+                    cnt = (cnt * (1 + combi(freqs[i], k, mod))) % mod;
+                }
+            }
+            cnt--;
+            ans = (ans + cnt) % mod;
         }
         return (int)ans;
     }
