ecab5c5674a33f595c8ea6d3cd1b413d7e98c1cc
David Sun <iamslash@gmail.com>
2023-02-18 20:45:46 +0900

    Accept TheLatestTimetoCatchaBus

--- leetcode3/TheLatestTimetoCatchaBus/MainApp.java
+++ leetcode3/TheLatestTimetoCatchaBus/MainApp.java
@@ -1,46 +1,44 @@
-// Copyright (C) 2022 by iamslash
+// Copyright (C) 2023 by iamslash
 
 import java.util.*;
 
-//     cap: 2
-//           i
-//   buses: 10 20
-//    pass: 2 17 18 19
-//          j
-//     cnt: 1
-//  pasSet:
-//     ans: 1
+//   capacity: 2
+//      buses: 10 20 30
+// passengers: 4 11 13 19 21 25 26
 
-// Idea: greedy
+// Idea: greedy, sort, two pointers
 //
-// i for buses
-// j for passengers
-// passengerSet for passengers who have gone
+//         i: buses index
+//         j: passengers index
+// personSet: passengers who has depart
 // The ans should be the last one possible.
-// Consider passenger[j-1] as candidate on loops
-// Consider buses[i] as candidate at last
+// Consider passengers[j-1] as candidate on loop
+// Consider buses[i] as candidate on loop
 
-// 50ms 41.96% 64.8MB 58.74%
+// 49ms 37.25% 64.9MB 34.18%
 // greedy, sort, two pointers
 // O(N) O(N)
 class Solution {
-    public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
+    public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capa) {
         Arrays.sort(buses);
         Arrays.sort(passengers);
-        Set<Integer> passengerSet = new HashSet<>();
+        Set<Integer> personSet = new HashSet<>();
         int ans = 0, n = buses.length, m = passengers.length;
-        // i for bus, j for passenger
+        // i for bus, j for person
         for (int i = 0, j = 0; i < n; ++i) {
             int cnt = 0;
-            while (j < m && cnt < capacity && buses[i] >= passengers[j]) {
-                if (!passengerSet.contains(passengers[j] - 1)) {
+            // Update ans with passengers[j-1]
+            while (j < m && cnt < capa &&
+                   passengers[j] <= buses[i]) {
+                if (!personSet.contains(passengers[j] - 1)) {
                     ans = passengers[j] - 1;
                 }
-                passengerSet.add(passengers[j]);
+                personSet.add(passengers[j]);
                 cnt++;
                 j++;
             }
-            if (cnt < capacity && !passengerSet.contains(buses[i])) {
+            // Update ans with buses[i]
+            if (cnt < capa && !personSet.contains(buses[i])) {
                 ans = buses[i];
             }
         }
