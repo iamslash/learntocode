6d018a0277eb4036694a3d84cefc8fdacf6030be
David Sun <iamslash@gmail.com>
2023-02-18 20:29:58 +0900

    Accept StockPriceFluctuation

--- leetcode3/StockPriceFluctuation/20220316.MainApp.java
+++ leetcode3/StockPriceFluctuation/20220316.MainApp.java
@@ -1,20 +1,23 @@
-// Copyright (C) 2023 by iamslash
+// Copyright (C) 2022 by iamslash
 
-// 163ms 39.47% 95.9MB 38.80%
-// hash map
-// update: O(NlgN)
-// current: O(1)
-// maximum: O(1)
-// minimum: O(1)
 import java.util.*;
-class StockPrice {
-    int lastTs = -1;
-    Map<Integer, Integer> tsPriceMap = new HashMap<>();
-    SortedMap<Integer, Set<Integer>> priceTssMap = new TreeMap<>((a, b) -> a - b);
 
+// 217ms 44.48% 173.8MB 20.83%
+// hash map
+//  update: O(NlgN) O(N)
+// current: O(1) O(N)
+// maximum: O(1) O(N)
+// minimum: O(1) O(N)
+class StockPrice {
+    Map<Integer, Integer> tsPriceMap = new HashMap<>();
+    int lastTs = -1;
+    SortedMap<Integer, Set<Integer>> priceTssMap = new TreeMap<>((a, b) -> a - b);
+    public StockPrice() {
+        
+    }
     public void update(int ts, int price) {
         lastTs = Math.max(lastTs, ts);
-        // Hit
+        // hit
         if (tsPriceMap.containsKey(ts)) {
             int prevPrice = tsPriceMap.get(ts);
             priceTssMap.get(prevPrice).remove(ts);
@@ -22,7 +25,7 @@ class StockPrice {
                 priceTssMap.remove(prevPrice);
             }
         }
-        // Save
+        // save
         tsPriceMap.put(ts, price);
         priceTssMap.putIfAbsent(price, new HashSet<>());
         priceTssMap.get(price).add(ts);
@@ -39,4 +42,9 @@ class StockPrice {
     public int minimum() {
         return priceTssMap.firstKey();
     }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
 }
