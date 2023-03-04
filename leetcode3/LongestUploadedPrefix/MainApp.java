0702e5cb5e7570812f7778466ec91edf6e15205f
David Sun <iamslash@gmail.com>
2023-03-04 10:01:52 +0900

    Accept LongestUploadedPrefix

--- leetcode3/LongestUploadedPrefix/MainApp.java
+++ leetcode3/LongestUploadedPrefix/MainApp.java
@@ -1,13 +1,39 @@
-// Copyright (C) 2022 by iamslash
+// Copyright (C) 2023 by iamslash
 
 import java.util.*;
 
-// 252ms 8.34% 153.6MB 54.26%
+// Idea: sorted set
+//
+// Examples of uploaded video and longest index
+//       [1] : 1
+//    [1, 2] : 2
+// [1, 2, 3] : 3
+//       [2] : 0
+//       [3] : 0
+//    [2, 3] : 0
+//
+// Let's make a sorted set.
+// 0 1 2 3 4
+// upload 2, remove that, ans is 0.
+// 0 1 3 4
+// upload 1, remove that, ans is 1.
+// 0 3 4
+// upload 0, remove that, ans is 3.
+// 3 4
+
+// Idea: brute force
+//
+// Let's make an array boolean[] uploaded, int longest.
+// uploaded[i] == true means i + 1 video was uploaded.
+// longest means longest index of video.
+
+// 134ms 13.57% 122.MB 50.23%
 // sorted set
-// O(NlgN) O(N)
+//  upload: O(lgN) O(N)
+// longest: O(lgN) O(N)
 class LUPrefix {
-    private SortedSet<Integer> prefixSet = new TreeSet<>();
-    private int size = 0;
+    SortedSet<Integer> prefixSet = new TreeSet<>();
+    int size = 0;
 
     public LUPrefix(int n) {
         for (int i = 0; i < n; ++i) {
@@ -28,8 +54,34 @@ class LUPrefix {
     }
 }
 
+// 36ms 91.86% 108.8MB 64.25%
+// brute force
+//  upload: O(N) O(N)
+// longest: O(1) O(N)
+class LUPrefix {
+    private boolean[] uploaded;
+    private int longest;
 
-public class MainApp {
-  public static void main(String[] args) {
-  }
+    public LUPrefix(int n) {
+        uploaded = new boolean[n + 1];
+        longest = 0;
+        uploaded[0] = true;
+    }
+    
+    public void upload(int video) {
+        uploaded[video] = true;
+        if (video == longest + 1) {
+            for (int i = longest + 1; i < uploaded.length; ++i) {
+                if (uploaded[i]) {
+                    longest = i;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    
+    public int longest() {
+        return longest;
+    }
 }
