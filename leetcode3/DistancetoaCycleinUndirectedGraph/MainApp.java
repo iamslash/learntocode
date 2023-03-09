aa6c62070f86c19099e80a7ab4f8b28866cf17bf
David Sun <iamslash@gmail.com>
2023-03-10 07:50:01 +0900

    Accept DistancetoaCycleinUndirectedGraph

--- leetcode3/DistancetoaCycleinUndirectedGraph/MainApp.java
+++ leetcode3/DistancetoaCycleinUndirectedGraph/MainApp.java
@@ -2,9 +2,32 @@
 
 import java.util.*;
 
+
+// Idea: BFS
+//
+// List<Integer>[] graph: graph.
+// int[] ans: distance from the cycle nodes.
+// int[] cnts: count of neighbors.
+// BFS not cycle nodes. out to in.
+//   cnts[u] == Integer.MAX_VALUE.
+// BFS cycle nodes. in to out.
+//   Update distances of not cycle nodes.
+
+// 23ms 100.00% 95.7MB 88.89%
+// bfs
+// O(N) O(N)
 class Solution {
+    private void dump(int[] A) {
+        for (int a : A) {
+            System.out.printf("%d ", a);
+        }
+        System.out.println();
+    }
     public int[] distanceToCycle(int n, int[][] edges) {
         List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; ++i) {
+            graph[i] = new ArrayList<>();
+        }
         int[] ans = new int[n];
         int[] cnts = new int[n];
         Queue<Integer> q = new LinkedList<>();
@@ -21,7 +44,10 @@ class Solution {
                 q.offer(u);
             }
         }
-        // BFS and add next leaf nodes to q
+        //    q: 0 6
+        // cnts: 1 3 3 2 2 2 1 
+
+        // BFS not cycle nodes.
         while (!q.isEmpty()) {
             int u = q.poll();
             ans[u] = Integer.MAX_VALUE;
@@ -31,13 +57,19 @@ class Solution {
                 }
             }
         }
-        // Add other nodes to q
+        //    q: []
+        // cnts: 1 2 2 2 2 1 1 
+        //  ans: M 0 0 0 0 M M
+
+        // Add loop nodes.
         for (int u = 0; u < n; ++u) {
             if (cnts[u] > 1) {
                 q.offer(u);
             }
         }
-        // BFS loop nodes
+        //   q: [1, 2, 3, 4]
+        
+        // BFS cycle nodes
         while (!q.isEmpty()) {
             int u = q.poll();
             for (int v : graph[u]) {
@@ -50,3 +82,7 @@ class Solution {
         return ans;
     }
 }
+
+//    q: 5
+// cnts: 1 2 2 2 2 1 1
+//  ans: 1 0 0 0 0 1 2
