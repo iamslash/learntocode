79f882261413cb2f8c413f3ca7daf6402a6c852e
David Sun <iamslash@gmail.com>
2023-04-13 21:15:08 +0900

    Accept PromiseTimeLimit

--- leetcode3/PromiseTimeLimit/a.ts
+++ leetcode3/PromiseTimeLimit/a.ts
@@ -1,9 +1,14 @@
 // Copyright (C) 2023 by iamslash
 
+// 63ms 100.00% 43.5MB 100.00%
+// O(1) O(1)
 type Fn = (...params: any[]) => Promise<any>;
-
 function timeLimit(fn: Fn, t: number): Fn {
     return async function(...args) {
-        
+        const orgPromise = fn(...args);
+        const timedPromise = new Promise((_, reject) => {
+            setTimeout(() => { reject("Time Limit Exceeded") }, t);
+        });
+        return Promise.race([orgPromise, timedPromise]);
     }
 };
