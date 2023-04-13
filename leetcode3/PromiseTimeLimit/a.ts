d77cc062f3d60bd4f15b882f35d9f52ea5a688ee
David Sun <iamslash@gmail.com>
2023-04-13 09:53:19 +0900

    Add PromiseTimeLimit

---
+++ leetcode3/PromiseTimeLimit/a.ts
@@ -0,0 +1,9 @@
+// Copyright (C) 2023 by iamslash
+
+type Fn = (...params: any[]) => Promise<any>;
+
+function timeLimit(fn: Fn, t: number): Fn {
+    return async function(...args) {
+        
+    }
+};
