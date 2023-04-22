3fe9e1f526ee9fb1a935a8a28c70364132a6eac4
David Sun <iamslash@gmail.com>
2023-04-23 08:41:56 +0900

    Accept NumberofBeautifulPartitions

--- leetcode3/NumberofBeautifulPartitions/20230305.MainApp.java
+++ leetcode3/NumberofBeautifulPartitions/20230305.MainApp.java
@@ -2,44 +2,42 @@
 
 import java.util.*;
 
+// primes: 2 3 5 7
+
+//               i
+//      s: 23542185131
+//
 
 // Idea: recursive dynamic programming
 //
-// Declare cache C[idx][remained k]
+// Declare cache C[index][remained k]
 // Design int dfs(int i, int k)
-// Loop inside dfs
-//   i: start index
-//   j: end index
+// i stands for start index
+// j stands for end index
 //
-// ex)
-// minLen: 2
-//      k: 3
-//                           i
-//                 .         .
-//      s: 2 3 5 4 2 1 8 5 1 3 1
-//                             j              
-//                             o
-//
-// dfs(0, 3) 
-//   dfs(2, 2) 
-//   dfs(3, 2): 0 
-//   dfs(4, 2): 1
-//     dfs(6, 1): 0
-//     dfs(7, 1): 0
-//     dfs(8, 1): 0
-//     dfs(9, 1): 1
-//   dfs(5, 2) 
-//   dfs(6, 2) 
-//   dfs(7, 2) 
+// minLen : 2
+//       k: 3
+//                  i          
+// s: 2 3 5 4 2 1 8 5 1 3 1
+//                      j 
+// dfs(3, 0) : 3
+//   dfs(2, 4) : 2
+//     dfs(1, 7) : 1
+//       dfs(0, 9) : 0
+//       dfs(0, 11) : 1 
+//     dfs(1, 9) : 1
+//       dfs(0, 11) : 1 hit
+//   dfs(2, 7) : 1
+//     dfs(1, 9) : 1 hit
 
-// 693ms 45.31% 47.8MB 51.56%
+// 649ms 39.38% 47.7MB 58.13%
 // recursive dynamic programming
 // O(N^2K) O(NK)
 class Solution {
     private boolean isPrime(char c) {
         return c == '2' || c == '3' || c == '5' || c == '7';
     }
-    private int dfs(String s, int minLen, int[][] C, int i, int k) {
+    private int dfs(int[][] C, String s, int minLen, int i, int k) {
         int mod = 1_000_000_007, n = s.length();
         // base
         if (i == n && k == 0) {
@@ -56,18 +54,19 @@ class Solution {
         int cnt = 0;
         if (isPrime(s.charAt(i))) {
             // i
-            // aabbcc
-            //  j
+            // 00xxxxaa
+            //   j
+            // j - i + 1 >= minLen
             for (int j = i + minLen - 1; j < n - minLen * (k - 1); ++j) {
                 if (isPrime(s.charAt(j))) {
                     continue;
                 }
-                cnt = (cnt + dfs(s, minLen, C, j + 1, k - 1)) % mod;
+                cnt = (cnt + dfs(C, s, minLen, j + 1, k - 1)) % mod;
             }
         }
         return C[i][k] = cnt;
     }
-    public int beautifulPartitions(String s, int k, int minLength) {
+    public int beautifulPartitions(String s, int k, int minLen) {
         int n = s.length();
         if (!isPrime(s.charAt(0)) || isPrime(s.charAt(n-1))) {
             return 0;
@@ -76,33 +75,34 @@ class Solution {
         for (int i = 0; i < n; ++i) {
             Arrays.fill(C[i], -1);
         }
-        return dfs(s, minLength, C, 0, k);
+        return dfs(C, s, minLen, 0, k);
     }
 }
 
-// Idea: iterative dyanmic programming
+// Idea: iterative dynamic programming
 //
-// Declare cache C[idx][remained k]
+// Declare cache C[index][remained k]
 // Init C[0][0] = 1
-// Set value a which means remained k
-// Set idx i which means start index of current partition
-// Set idx u which means start index of previous partition
+// Set value a wihch means remained k.
+// Set index i which means start index current partition.
+// Set index u which means start index previous partition.
 //
 // minLen: 3
-//         u
-//      s: aaabbbccc
+//         u  
+//      s: ooopppxxx
 //            i
 
-// 39ms 89.06% 47MB 81.25%
+// 39ms 80.63% 47.7MB 58.13%
 // iterative dynamic programming
 // O(NK) O(NK)
+// difficult
 class Solution {
     private boolean isPrime(char c) {
         return c == '2' || c == '3' || c == '5' || c == '7';
     }
     public int beautifulPartitions(String s, int k, int minLen) {
         int n = s.length(), mod = 1_000_000_007;
-        if (!isPrime(s.charAt(0)) || isPrime(s.charAt(n - 1))) {
+        if (!isPrime(s.charAt(0)) || isPrime(s.charAt(n-1))) {
             return 0;
         }
         int[][] C = new int[n + 1][k + 1];
