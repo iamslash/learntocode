203c40bdd723e7ef55b05f5e867f1e5196958d63
David Sun <iamslash@gmail.com>
2023-05-12 07:39:24 +0900

    Accept ColortheTriangleRed

--- leetcode3/ColortheTriangleRed/MainApp.java
+++ leetcode3/ColortheTriangleRed/MainApp.java
@@ -2,20 +2,36 @@
 
 import java.util.*;
 
-// Wrong Answer for
-// input: 4
+// 9ms 100.00% 62.3MB 55.81%
+// pattern
+// O(N^2) O(N)
 class Solution {
     public int[][] colorRed(int n) {
-        int h = 2 * n - 1;
-        int[][] ans = new int[h][2];
-        ans[0][0] = 1;
-        ans[0][1] = 1;
-        for (int i = 1; i < n; ++i) {
-            ans[i*2 - 1][0] = i + 1;
-            ans[i*2 - 1][1] = 1;
-            ans[i*2][0] = i + 1;
-            ans[i*2][1] = i*2 + 1;
+        List<int[]> ans = new ArrayList<>();
+        // Build 4 rows pattern from the bottom
+        for (int i = n; i - 4 >= 0; i -= 4) {
+            for (int j = 1; j <= 2 * i - 1; j += 2) {
+                ans.add(new int[]{i, j});                
+            }
+            ans.add(new int[]{i - 1, 2});
+            for (int j = 2 * (i - 2) - 1; j > 2; j -= 2) {
+                ans.add(new int[]{i - 2, j});
+            }
+            ans.add(new int[]{i - 3, 1});
         }
-        return ans;
+        // Build rest pattern
+        int m = n % 4;
+        if (m >= 1) {
+            ans.add(new int[]{1, 1});
+        }
+        if (m >= 2) {
+            ans.add(new int[]{2, 1});
+            ans.add(new int[]{2, 3});
+        }
+        if (m >= 3) {
+            ans.add(new int[]{3, 1});
+            ans.add(new int[]{3, 5});            
+        }
+        return ans.toArray(new int[0][]);
     }
 }
