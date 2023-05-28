16fb7700a80783c5c3ff29c2c2be651bbcc63752
David Sun <iamslash@gmail.com>
2023-05-28 09:35:18 +0900

    Accept MaximumNumberofFishinaGrid

--- leetcode3/HeightofBinaryTreeAfterSubtreeRemovalQueries/MainApp.java
+++ leetcode3/HeightofBinaryTreeAfterSubtreeRemovalQueries/MainApp.java
@@ -15,7 +15,7 @@ class TreeNode {
     }
 }
 
-//          5
+//          5  
 //        /   \
 //      8       9
 //    /   \   /   \
@@ -25,44 +25,57 @@ class TreeNode {
 // 
 // queries: 3 2 4 8
 
+// Idea: pre-order
+//
+// Traverse node a, left child l, right child r
+// When we visit left child l,
+//   we know max lv of sub-tree including a, r
+// Traverse node a, right child r, left child l
+// When we visit right child r,
+//   we know max lv of sub-tree including a, l
+// Therefore, we get max lv when we remove node a
+//   from max(max lv of left sub-tree including a and r,
+//            max lv of right sub-tree including a and l)
+
 // 16ms 92.21% 74.3MB 60.78%
 // pre-order, post-order
 // O(N) O(lgN)
 class Solution {
-    private int[] preHeights = new int[100001];
-    private int[] postHeights = new int[100001];
-    int maxHeight;
-    private void preOrder(TreeNode u, int height) {
+    private int[] leftMaxLv = new int[100001];
+    private int[] rightMaxLv = new int[100001];
+    int maxLv;
+    private void dfsLeft(TreeNode u, int height) {
         // base
         if (u == null) {
             return;
         }
         // recursion
-        preHeights[u.val] = maxHeight;
-        maxHeight = Math.max(maxHeight, height);
-        preOrder(u.left, height + 1);
-        preOrder(u.right, height + 1);
+        leftMaxLv[u.val] = maxLv;
+        maxLv = Math.max(maxLv, height);
+        dfsLeft(u.left, height + 1);
+        dfsLeft(u.right, height + 1);
     }
-    private void postOrder(TreeNode u, int height) {
+    private void dfsRight(TreeNode u, int height) {
         // base
         if (u == null) {
             return;
         }
         // recursion
-        postHeights[u.val] = maxHeight;
-        maxHeight = Math.max(maxHeight, height);
-        postOrder(u.right, height + 1);
-        postOrder(u.left, height + 1);
+        rightMaxLv[u.val] = maxLv;
+        maxLv = Math.max(maxLv, height);
+        dfsRight(u.right, height + 1);
+        dfsRight(u.left, height + 1);
     }
     public int[] treeQueries(TreeNode root, int[] queries) {
         int n = queries.length;
-        maxHeight = 0;
-        preOrder(root, 0);
-        maxHeight = 0;
-        postOrder(root, 0);
+        maxLv = 0;
+        dfsLeft(root, 0);
+        maxLv = 0;
+        dfsRight(root, 0);
         int[] ans = new int[n];
         for (int i = 0; i < n; ++i) {
-            ans[i] = Math.max(preHeights[queries[i]], postHeights[queries[i]]);
+            ans[i] = Math.max(leftMaxLv[queries[i]],
+                              rightMaxLv[queries[i]]);
         }
         return ans;
     }
--- leetcode3/MaximumNumberofFishinaGrid/20230501.MainApp.java
+++ leetcode3/MaximumNumberofFishinaGrid/20230501.MainApp.java
@@ -2,9 +2,9 @@
 
 import java.util.*;
 
-// 5ms 99.16% 43.7MB 5.11%
+// 5ms 71.43% 42.9MB 42.86%
 // dfs
-// O(HW) O(lgN)
+// O(N) O(lg4N)
 class Solution {
     private int dfs(int[][] grid, int y, int x) {
         int h = grid.length, w = grid[0].length;
@@ -12,7 +12,6 @@ class Solution {
         if (y < 0 || y >= h || x < 0 || x >= w || grid[y][x] == 0) {
             return 0;
         }
-        
         int fish = grid[y][x];
         grid[y][x] = 0;
         // recursion
@@ -23,8 +22,7 @@ class Solution {
             dfs(grid, y, x - 1);
     }
     public int findMaxFish(int[][] grid) {
-        int h = grid.length, w = grid[0].length;
-        int maxFish = 0;
+        int h = grid.length, w = grid[0].length, maxFish = 0;
         for (int y = 0; y < h; ++y) {
             for (int x = 0; x < w; ++x) {
                 if (grid[y][x] > 0) {
