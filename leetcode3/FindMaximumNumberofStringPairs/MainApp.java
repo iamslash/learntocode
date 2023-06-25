4a8372137b6054b96a78be072fe45d1edf19e590
David Sun <iamslash@gmail.com>
2023-06-25 23:29:01 +0900

    Accept FindMaximumNumberofStringPairs

--- leetcode3/FindMaximumNumberofStringPairs/MainApp.java
+++ leetcode3/FindMaximumNumberofStringPairs/MainApp.java
@@ -19,3 +19,22 @@ class Solution {
         return cnt;
     }
 }
+
+
+// 2ms 66.67% 41MB 100.00%
+// brute force
+// O(N^2) O(1)
+class Solution {
+    public int maximumNumberOfStringPairs(String[] words) {
+        int cnt = 0, n = words.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (words[i].charAt(0) == words[j].charAt(1) &&
+                    words[i].charAt(1) == words[j].charAt(0)) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
