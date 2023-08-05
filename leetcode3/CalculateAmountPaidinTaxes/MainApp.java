fc4a7dc22659e7999982d7c7e0bb59dc4c73ba2b
David Sun <iamslash@gmail.com>
2023-08-05 16:41:56 +0900

    Accept KRadiusSubarrayAverages, CalculateAmountPaidinTaxes

--- leetcode3/CalculateAmountPaidinTaxes/MainApp.java
+++ leetcode3/CalculateAmountPaidinTaxes/MainApp.java
@@ -1,26 +1,23 @@
-// Copyright (C) 2022 by iamslash
+// Copyright (C) 2023 by iamslash
 
 import java.util.*;
 
-
-//   income: 10
-// brackets: 3,50 7,10 12,25
-//              i
-
-// 1ms 88.89% 46.4MB 11.11%
-// math
+// 0ms 100.00% 43.5MB 53.77%
+// brute force
 // O(N) O(1)
 class Solution {
     public double calculateTax(int[][] brackets, int income) {
-        double sum = 0.0;
-        for (int i = 0; i < brackets.length; ++i) {
-            double curUpper = Math.min(brackets[i][0], income);
-            double prvUpper = i == 0 ? 0.0 : brackets[i-1][0];
-            sum += (curUpper - prvUpper) * brackets[i][1];
-            if (brackets[i][0] >= income) {
+        double ans = 0.0;
+        int prv = 0;
+        for (int[] bracket : brackets) {
+            int profit = Math.min(bracket[0], income) - prv;
+            prv = bracket[0];
+            double ratio = bracket[1] * 0.01;
+            ans += profit * ratio;
+            if (bracket[0] > income) {
                 break;
             }
         }
-        return sum * 0.01;
+        return ans;
     }
 }
--- leetcode3/KRadiusSubarrayAverages/20220411.MainApp.java
+++ leetcode3/KRadiusSubarrayAverages/20220411.MainApp.java
@@ -1,32 +1,37 @@
-// Copyright (C) 2023 by iamslash
+// Copyright (C) 2022 by iamslash
 
 import java.util.*;
 
-//             i
-// nums: 7 4 3 9 1 8 5 2 6
-
-// 6ms 99.10% 58.3MB 98.76%
+// 9ms 96.83% 62.2MB 89.97%
 // sliding window
 // O(N) O(N)
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        if (k == 0) {
-            return nums;
+        if (k <= 0) {
+            return nums;           
         }
+        long ps = 0;
         int n = nums.length;
         int[] ans = new int[n];
         Arrays.fill(ans, -1);
         int wndLen = k * 2 + 1;
-        long wndSum = 0;
         for (int i = 0; i < n; ++i) {
-            wndSum += nums[i];
+            // Add new one
+            ps += nums[i];
+            // Sub old one
             if (i - wndLen >= 0) {
-                wndSum -= nums[i - wndLen];
+                ps -= nums[i - wndLen];
             }
+            // Save avg
             if (i >= wndLen - 1) {
-                ans[i - k] = (int)(wndSum / wndLen);
+                ans[i-k] = (int)(ps / wndLen);
             }
         }
         return ans;
     }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
 }
