db867d77b28fce0039b0980c727652deec1b2597
David Sun <iamslash@gmail.com>
2023-08-14 07:44:12 +0900

    Accept CustomInterval

--- leetcode3/CustomInterval/a.js
+++ leetcode3/CustomInterval/a.js
@@ -1,28 +1,65 @@
 // Copyright (C) 2023 by iamslash
 
-// Time LImit Exceeded
-/**
- * @param {Function} fn
- * @param {number} delay
- * @param {number} period
- * @return {number} id
- */
-let ids = []
+// // 55ms 67.21% 42.1MB 32.79%
+// /**
+//  * @param {Function} fn
+//  * @param {number} delay
+//  * @param {number} period
+//  * @return {number} id
+//  */
+// let ids = [];
+// let idx = 0;
+// function customInterval(fn, delay, period) {
+//     idx += 1;
+//     function executeFn(passCnt) {
+//         ids[idx] = setTimeout(() => {
+//             fn();
+//             executeFn(passCnt + 1);
+//         }, delay + period * passCnt);    
+//     }
+//     executeFn(0);
+//     return idx;
+// }
+
+// /**
+//  * @param {number} id
+//  */
+// function customClearInterval(id) {
+//     clearTimeout(ids[id]);   
+// }
+
+// 64ms 14.75% 42.1MB 32.79%
+const idsMap = {};
+let cnt = 0;
 function customInterval(fn, delay, period) {
-    let len = ids.length;
-    let idx = 0;
-    function executeFn() {
-        fn();
-        idx += 1;
-        ids[len] = setTimeout(executeFn, delay + idx * period);
-    }
-    idx[0] = setTimeout(executeFn, delay);
-    return 0;
+    cnt++;
+    function executeFn(passCnt) {
+        idsMap[cnt] = setTimeout(() => {
+            fn();
+            executeFn(passCnt + 1);
+        }, delay + period * passCnt);    
+    };
+    executeFn(0);
+    return cnt;
+}
+function customClearInterval(id) {
+    clearTimeout(idsMap[id]);   
 }
 
-/**
- * @param {number} id
- */
+
+// Why this is right solution???
+// 53ms 83.61% 42.2MB 27.87%
+const idsMap = {};
+function customInterval(fn, delay, period) {
+    function executeFn(passCnt) {
+        idsMap[1] = setTimeout(() => {
+            fn();
+            executeFn(passCnt + 1);
+        }, delay + period * passCnt);    
+    };
+    executeFn(0);
+    return 1;
+}
 function customClearInterval(id) {
-    clearTimeout(ids[id]);   
+    clearTimeout(idsMap[id]);   
 }
