048a813ac7d4d224d485ca9980ce7c1e9d4ac907
David Sun <iamslash@gmail.com>
2023-08-27 10:33:54 +0900

    Accept TheNumberofWeakCharactersintheGame

--- leetcode2/TheNumberofWeakCharactersintheGame/MainApp.java
+++ leetcode2/TheNumberofWeakCharactersintheGame/MainApp.java
@@ -9,18 +9,66 @@ import java.util.*;
 //              i
 //        6,3 5,5 3,6
 
-// 176ms 6.75% 100.9MB 71.57%
-// sort
-// O(NlgN) O(1)
+// // 176ms 6.75% 100.9MB 71.57%
+// // sort
+// // O(NlgN) O(1)
+// class Solution {
+//     public int numberOfWeakCharacters(int[][] props) {
+//         Arrays.sort(props, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+//         int n = props.length, cnt = 0, maxDefence = 0;
+//         for (int i = 0; i < n; ++i) {
+//             if (props[i][1] < maxDefence) {
+//                 cnt++;
+//             }
+//             maxDefence = Math.max(maxDefence, props[i][1]);
+//         }
+//         return cnt;
+//     }
+// }
+
+
+// Idea: hash map
+//
+// Let's think about 2 dimension graph.
+//
+// |....    
+// |    .
+// |     .
+//  ------
+//
+
+
+// props: 3,6 5,5 6,3
+
+// props: 1,1 1,2 2,1 2,2
+//
+// |
+// |
+// |..
+// |..
+//  --
+
+// 9ms 89.32% 106.3MB 5.10%
+// hash map
+// O(N) O(1)
 class Solution {
     public int numberOfWeakCharacters(int[][] props) {
-        Arrays.sort(props, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
-        int n = props.length, cnt = 0, maxDefence = 0;
-        for (int i = 0; i < n; ++i) {
-            if (props[i][1] < maxDefence) {
+        int[] maxY = new int[100_002];
+        int cnt = 0;
+        for (int[] prop : props) {
+            int x = prop[0], y = prop[1];
+            maxY[x] = Math.max(maxY[x], y);
+        }
+        for (int x = 100_000; x >= 0; --x) {
+            maxY[x] = Math.max(maxY[x], maxY[x + 1]);
+        }
+        for (int[] prop : props) {
+            int x = prop[0], y = prop[1];
+            // maxY[x + 1] for same x values.
+            // ex) 1,1 1,2
+            if (y < maxY[x + 1]) {
                 cnt++;
             }
-            maxDefence = Math.max(maxDefence, props[i][1]);
         }
         return cnt;
     }
