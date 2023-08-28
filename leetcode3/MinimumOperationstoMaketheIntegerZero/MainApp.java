d3c634a36c8de2915f0ccb64ec4a230d161c6df0
David Sun <iamslash@gmail.com>
2023-08-28 22:39:07 +0900

    Accept MinimumOperationstoMaketheIntegerZero

--- leetcode3/MinimumOperationstoMaketheIntegerZero/MainApp.java
+++ leetcode3/MinimumOperationstoMaketheIntegerZero/MainApp.java
@@ -6,10 +6,19 @@ import java.util.*;
 //
 // r = num1 - k * num2
 // 0 <= k <= 60
-// r sholud be sum of 2^m (0 <= m <= 60)
+// Find minimum k (bitcount(r) <= k <= r)
 
+// 0ms 100.00% 39.1MB 98.57%
+// math
+// O(1) O(1)
 class Solution {
-    public int makeTheIntegerZero(int num1, int num2) {
-        
+    public int makeTheIntegerZero(long num1, long num2) {
+        for (int k = 1; k <= 60; ++k) {
+            long r = num1 - k * num2;
+            if (Long.bitCount(r) <= k && k <= r) {
+                return k;
+            }
+        }
+        return -1;
     }
 }
