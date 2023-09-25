8a73a6e1ccfc25c05d530813ba7c1624a66dee9b
David Sun <iamslash@gmail.com>
2023-09-25 20:54:35 +0900

    Accept MaximumLengthofSemiDecreasingSubarrays

--- leetcode3/MaximumLengthofSemiDecreasingSubarrays/MainApp.java
+++ leetcode3/MaximumLengthofSemiDecreasingSubarrays/MainApp.java
@@ -1,11 +1,32 @@
 // Copyright (C) 2023 by iamslash
 
-impport java.util.*;
+import java.util.*;
 
+// 13ms 61.70% 57.9MB 17.02%
 // mono stack
 // O(N) O(N)
 class Solution {
     public int maxSubarrayLength(int[] nums) {
-        
+        Stack<Integer> stck = new Stack<>();
+        int n = nums.length;
+        for (int i = n - 1; i >= 0; --i) {
+            if (stck.isEmpty() || nums[i] < nums[stck.peek()]) {
+                stck.push(i);
+            }
+        }
+        int ans = 0, maxNum = Integer.MIN_VALUE;
+        for (int i = 0; i < n; ++i) {
+            while (!stck.isEmpty() && stck.peek() <= i) {
+                stck.pop();
+            }
+            if (nums[i] > maxNum) {
+                maxNum = nums[i];
+                while (!stck.isEmpty() && nums[stck.peek()] < maxNum) {
+                    ans = Math.max(ans, stck.peek() - i + 1);
+                    stck.pop();
+                }
+            }
+        }
+        return ans;
     }
 }
