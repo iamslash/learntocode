40cf81cbe9a355c5433f7c431432649bc7d90913
David Sun <iamslash@gmail.com>
2024-02-10 18:36:38 +0900

    Accept ImageSmoother

--- leetcode/ImageSmoother/20230917.MainApp.java
+++ leetcode/ImageSmoother/20230917.MainApp.java
@@ -1,19 +1,19 @@
-// Copyright (C) 2024 by iamslash
+// Copyright (C) 2023 by iamslash
 
 import java.util.*;
 
-// 5ms 75.11% 45.8MB 5.49%
-// brute force
+// 4ms 95.88% 44.5MB 64.42%
+// brute foce
 // O(HW) O(HW)
 class Solution {
-    private int avg(int[][] img, int row, int col) {
-        int h = img.length, w = img[0].length;
-        int sum = 0, cnt = 0;
-        for (int y = row - 1; y < row + 2; ++y) {
-            for (int x = col - 1; x < col + 2; ++x) {
-                if (y >= 0 && y < h && x >= 0 && x < w) {
-                    sum += img[y][x];
+    private int avg(int[][] grid, int y, int x) {
+        int h = grid.length, w = grid[0].length;
+        int cnt = 0, sum = 0;
+        for (int i = y - 1; i <= y + 1; ++i) {
+            for (int j = x - 1; j <= x + 1; ++j) {
+                if (i >= 0 && i < h && j >= 0 && j < w) {
                     cnt++;
+                    sum += grid[i][j];
                 }
             }
         }
