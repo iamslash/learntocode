8aae2ce051c0bc8ed366c21041b83317779e73da
David Sun <iamslash@gmail.com>
2024-02-11 10:17:21 +0900

    Accept 4Sum

--- leetcode/4Sum/MainApp.java
+++ leetcode/4Sum/MainApp.java
@@ -31,7 +31,7 @@ import java.util.*;
 // It's ok same numbers.
 // The quadruplets are distinct.
 
-// 31ms 26.55% 44.5MB 37.98%
+// 19ms 45.70% 45MB 27.50%
 // two pointers, sort
 // O(N^3) O(1)
 class Solution {
@@ -42,13 +42,15 @@ class Solution {
 
         for (int i = 0; i < n - 3; ++i) {
             for (int j = i + 1; j < n - 2; ++j) {
+                // Skip same nums[j]
+                if (i + 1 < j && nums[j - 1] == nums[j]) {
+                    continue;
+                }
                 int l = j + 1, r = n - 1;
                 long sum = 0;
                 while (l < r) {
-                    sum = nums[i];
-                    sum += nums[j];
-                    sum += nums[l];
-                    sum += nums[r];
+                    sum = nums[i]; sum += nums[j];
+                    sum += nums[l]; sum += nums[r];
                     if (sum == target) {
                         ans.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
                         l++;
--- leetcode/4Sum/a.cpp
+++ leetcode/4Sum/a.cpp
@@ -1,39 +1,43 @@
-/* Copyright (C) 2018 by iamslash */
-// https://leetcode.com/problems/4sum/description/
+/* Copyright (C) 2024 by iamslash */
 
 #include <cstdio>
 #include <vector>
-#include <algorithm>
 #include <set>
 
+using namespace std;
 
+// 27ms 72.20% 16.2MB 27.29%
+// two pointers
+// O(N^3) O(N)
 class Solution {
  public:
-  std::vector<std::vector<int>> fourSum(std::vector<int>& v, int t) {
-    if (v.size() < 4)
-      return std::vector<std::vector<int>>();
-    std::set<std::vector<int>> rslt;
-    int l, r, sum;
-    std::sort(v.begin(), v.end());
-    for (int i = 0; i < v.size()-3; ++i) {
-      for (int j = i+1; j < v.size()-2; ++j) {
-        // check skip
-        if (j > i + 1 && v[j] == v[j-1])
+  vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    int n = nums.size();
+    set<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < n - 3; ++i) {
+      for (int j = i+1; j < n - 2; ++j) {
+        // Skip same nums[j]
+        if (j > i + 1 && nums[j] == nums[j-1]) {
           continue;
-        l = j + 1;
-        r = v.size() - 1;
+        }
+        int l = j + 1, r = n - 1;
         while (l < r) {
-          sum = v[i] + v[j] + v[l] + v[r];
-          if (sum == t) {
-            std::vector<int> rr = {v[i], v[j], v[l], v[r]};
-            rslt.insert(rr);
-            while (l < r && v[l] == v[l+1])
-              ++l;
-            while (l < r && v[r] == v[r-1])
-              --r;
+          long sum = nums[i];
+          sum += nums[j];
+          sum += nums[l]; 
+          sum += nums[r];
+          if (sum == target) {
+            ans.insert({nums[i], nums[j], nums[l], nums[r]});
             ++l;
+            while (l < r && nums[l - 1] == nums[l]) {
+              ++l;
+            }
             --r;
-          } else if (sum < t) {
+            while (l < r && nums[r] == nums[r + 1]) {
+              --r;
+            }
+          } else if (sum < target) {
             ++l;
           } else {
             --r;
@@ -41,27 +45,10 @@ class Solution {
         }
       }
     }
-    return std::vector<std::vector<int>>(rslt.begin(), rslt.end());
+    return vector<vector<int>>(ans.begin(), ans.end());
   }
 };
 
 int main() {
-  // std::vector<int> v = {1, 0, -1, 0, -2, 2};
-  // int t = 0;
-  // std::vector<int> v = {-3, -2, -1, 0, 0, 1, 2, 3};
-  // int t = 0;
-  // std::vector<int> v = {};
-  // int t = 0;
-  std::vector<int> v = {1, 0, -1, 0, -2, 2};
-  int t = 0;
-
-  Solution s;
-  auto r = s.fourSum(v, t);
-  for (const auto& rr : r) {
-    for (int rrr : rr) {
-      printf("%2d ", rrr);
-    }
-    printf("\n");
-  }
   return 0;
 }
