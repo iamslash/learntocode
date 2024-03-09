6739133677a22426ec998b87ede35e2614188411
David Sun <iamslash@gmail.com>
2024-03-09 22:59:49 +0900

    Accept CopyListwithRandomPointer

--- leetcode/CopyListwithRandomPointer/MainApp.java
+++ leetcode/CopyListwithRandomPointer/MainApp.java
@@ -1,38 +1,44 @@
-// Copyright (C) 2022 by iamslash
+// Copyright (C) 2024 by iamslash
 
 import java.util.*;
 
-// Definition for a Node.
 class Node {
-  int val;
-  Node next;
-  Node random;
+    int val;
+    Node next;
+    Node random;
 
-  public Node(int val) {
-    this.val = val;
-    this.next = null;
-    this.random = null;
-  }
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
 }
 
-// 0ms 100.00% 42.5MB 16.54%
+// head: 7 13 11 10 1
+//                    p
+// clon: 7 13 11 10 1
+//                  b q
+
+// 0ms 100.00% 44.2MB 71.59%
 // hash map
 // O(N) O(N)
 class Solution {
-  public Node copyRandomList(Node head) {
-    Map<Node, Node> nodeMap = new HashMap<>();
-    Node p = head;
-    while (p != null) {
-      nodeMap.put(p, new Node(p.val));
-      p = p.next;
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> nodeMap = new HashMap<>();
+        Node p = head;
+        while (p != null) {
+            nodeMap.put(p, new Node(p.val));
+            p = p.next;
+        }
+
+        p = head;
+        while (p != null) {
+            Node newNode = nodeMap.get(p);
+            newNode.next = nodeMap.get(p.next);
+            newNode.random = nodeMap.get(p.random);
+            p = p.next;
+        }
+
+        return nodeMap.get(head);
     }
-    p= head;
-    while (p != null) {
-      Node v = nodeMap.get(p);
-      v.next = nodeMap.get(p.next);
-      v.random = nodeMap.get(p.random);
-      p = p.next;
-    }
-    return nodeMap.get(head);
-  }
 }
