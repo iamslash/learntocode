62b4c1647920dc762bd201bccdb6ecc55908b630
David Sun <iamslash@gmail.com>
2024-03-30 18:47:11 +0900

    Accept NumberOfIslands

--- leetcode/NumberOfIslands/20211015.MainApp.java
+++ leetcode/NumberOfIslands/20211015.MainApp.java
@@ -2,27 +2,26 @@
 
 import java.util.*;
 
-// 2ms 99.98% 47.1MB 85.86%
+// 3ms 61.99% 47.2MB 63.35%
 // DFS
 // O(N) O(N)
 class Solution {
-    private int[] dirs = new int[]{-1, 0, 1, 0, -1};
-    private void dfs(char[][] grid, int y, int x) {
-        int h = grid.length, w = grid[0].length;
-        // visit
+    private int[] dir = new int[]{-1, 0, 1, 0, -1};
+    private int dfs(char[][] grid, int y, int x) {
+        // base
         grid[y][x] = '0';
+        int h = grid.length, w = grid[0].length;
+        // recursion
         for (int i = 0; i < 4; ++i) {
-            int ny = y + dirs[i], nx = x + dirs[i+1];
-            if (ny < 0 || ny >= h || nx < 0 || nx >= w ||
-                grid[ny][nx] != '1') {
+            int ny = y + dir[i], nx = x + dir[i+1];
+            if (ny < 0 || ny >= h || nx < 0 || nx >= w || grid[ny][nx] == '0') {
                 continue;
             }
             dfs(grid, ny, nx);
         }
     }
     public int numIslands(char[][] grid) {
-        int h = grid.length, w = grid[0].length;
-        int cnt = 0;
+        int h = grid.length, w = grid[0].length, cnt = 0;
         for (int y = 0; y < h; ++y) {
             for (int x = 0; x < w; ++x) {
                 if (grid[y][x] == '1') {
--- leetcode/NumberOfIslands/MainApp.java
+++ leetcode/NumberOfIslands/MainApp.java
@@ -1,28 +1,28 @@
-// Copyright (C) 2021 by iamslash
+// Copyright (C) 2024 by iamslash
 
 import java.util.*;
 
-// 2ms 99.98% 47.1MB 85.86%
-// DFS
+// 3ms 86.52% 49.5MB 49.88%
+// dfs
 // O(N) O(N)
 class Solution {
-    private int[] dirs = new int[]{-1, 0, 1, 0, -1};
+    int[] dirs = {-1, 0, 1, 0, -1};
     private void dfs(char[][] grid, int y, int x) {
         int h = grid.length, w = grid[0].length;
-        // visit
         grid[y][x] = '0';
+        
+        // recursion
         for (int i = 0; i < 4; ++i) {
-            int ny = y + dirs[i], nx = x + dirs[i+1];
-            if (ny < 0 || ny >= h || nx < 0 || nx >= w ||
-                grid[ny][nx] != '1') {
-                continue;
+            int ny = y + dirs[i], nx = x + dirs[i + 1];
+            if (0 <= ny && ny < h && 0 <= nx && nx < w && grid[ny][nx] == '1') {
+                dfs(grid, ny, nx);
             }
-            dfs(grid, ny, nx);
         }
     }
+    
     public int numIslands(char[][] grid) {
-        int h = grid.length, w = grid[0].length;
-        int cnt = 0;
+        int cnt = 0, h = grid.length, w = grid[0].length;
+
         for (int y = 0; y < h; ++y) {
             for (int x = 0; x < w; ++x) {
                 if (grid[y][x] == '1') {
@@ -31,11 +31,7 @@ class Solution {
                 }
             }
         }
+
         return cnt;
     }
-}
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
 }
