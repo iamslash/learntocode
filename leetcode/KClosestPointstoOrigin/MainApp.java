fe6ff90c4dbc46bbe4c18630663f3bcc7480b274
David Sun <iamslash@gmail.com>
2024-04-07 09:07:04 +0900

    Accept KClosestPointstoOrigin

--- leetcode/KClosestPointstoOrigin/MainApp.java
+++ leetcode/KClosestPointstoOrigin/MainApp.java
@@ -1,27 +1,30 @@
-// Copyright (C) 2021 by iamslash
+// Copyright (C) 2024 by iamslash
 
 import java.util.*;
 
-// 43ms 21.19% 115.4MB 8.30%
-// math
+// 28ms 64.48% 49MB 89.27%
+// sort
 // O(NlgN) O(N)
 class Solution {
+
+    private int dist(int[] pt) {
+        return pt[0] * pt[0] + pt[1] * pt[1];
+    }
+    
     public int[][] kClosest(int[][] points, int k) {
-        int[][] dists = new int[points.length][];
+        Integer[] idxs = new Integer[points.length]; // Use Integer instead of int
         for (int i = 0; i < points.length; ++i) {
-            int[] pt = points[i];
-            dists[i] = new int[]{pt[0], pt[1], pt[0]*pt[0] + pt[1]*pt[1]};
+            idxs[i] = i;
         }
-        Arrays.sort(dists, (a, b) -> a[2] - b[2]);
+
+        // Now, Arrays.sort can accept a custom Comparator for Integer[]
+        Arrays.sort(idxs, (i, j) -> dist(points[i]) - dist(points[j]));
+
         int[][] ans = new int[k][];
         for (int i = 0; i < k; ++i) {
-            ans[i] = new int[]{dists[i][0], dists[i][1]};
+            ans[i] = points[idxs[i]];
         }
+        
         return ans;
     }
-}
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
 }
