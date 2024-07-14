c5b29d16a942089ed32aa45a642095e39e397128
David Sun <iamslash@gmail.com>
2024-07-14 13:31:40 +0900

    Accept ConcatenatedWords

--- leetcode2/ConcatenatedWords/20220205.MainApp.java
+++ leetcode2/ConcatenatedWords/20220205.MainApp.java
@@ -1,99 +1,31 @@
-// Copyright (C) 2024 by iamslash
+// Copyright (C) 2022 by iamslash
 
 import java.util.*;
 
-//  minLen: 3
-//            i
+//                           i
 //   words: cat dog rat cats dogcatsdog catsdogcats ratcatdogcat hippopotamuses
 // wordSet: 
-//     ans: 
+//     ans: cat dog rat cats
 //
 
 // Time Limit Exceeded
-// sort
-// O(NlgN) + O(N * 2^N)
-// O(NK) + O(L)
-// N: the number of words.
-// L: the length of the word.
-// K: the average length of the words.
+// DFS
+// O(??) O(??)
 class Solution {
-    private boolean concatenated(Set<String> wordSet, int minLen, String word) {
+    private boolean concatenated(Set<String> wordSet, int minLen, String s) {
         // base
-        if (wordSet.contains(word)) {
+        if (wordSet.contains(s)) {
             return true;
         }
-
         // recursion
-        for (int i = minLen; i <= word.length() - minLen; ++i) {
-            if (wordSet.contains(word.substring(0, i)) &&
-                concatenated(wordSet, minLen, word.substring(i))) {
+        for (int i = minLen; i <= s.length() - minLen; ++i) {
+            if (wordSet.contains(s.substring(0, i)) &&
+                concatenated(wordSet, minLen, s.substring(i))) {
                 return true;
             }
         }
-
         return false;
     }
-    
-    public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        Arrays.sort(words, (a, b) -> a.length() - b.length());
-        Set<String> wordSet = new HashSet<>();
-        int minLen = Math.max(1, words[0].length());
-        List<String> ans = new ArrayList<>();
-
-        for (String word : words) {
-            if (word.length() >= minLen * 2 &&
-                concatenated(wordSet, minLen, word)) {
-                ans.add(word);
-            }
-            wordSet.add(word);
-        }
-
-        return ans;
-    }
-}
-
-//  minLen: 3
-//                                     i
-//   words: cat dog rat cats dogcatsdog catsdogcats ratcatdogcat hippopotamuses
-//                           j
-//       C:                  tfftffttfft
-// wordSet: cat dog rat cats
-//       
-//     ans: 
-//
-
-// 64ms 55.22% 48.6MB 75.37%
-// iterative dynamic programming
-// O(NlgN) + (NM^2)
-// O(NK) + O(M)
-// - N: number of words
-// - M: the maximum word length
-// K: the average length of the words.
-class Solution {
-
-    private boolean concatenated(Set<String> wordSet, int minLen, String word) {
-        // base
-        if (wordSet.isEmpty()) {
-            return false;
-        }
-
-        int n = word.length();
-        // C[i]: substring whose length is i is concatenated?
-        boolean[] C = new boolean[n + 1];
-        C[0] = true;
-
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (C[j] && wordSet.contains(word.substring(j, i))) {
-                    C[i] = true;
-                    break;
-                }
-            }
-        }
-        
-        return C[n];
-    }
-    
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         Arrays.sort(words, (a, b) -> a.length() - b.length());
         Set<String> wordSet = new HashSet<>();
@@ -108,3 +40,56 @@ class Solution {
         return ans;
     }
 }
+
+//  i
+// dogcatsdog
+// j
+
+// 1002ms 59.38% 46.8MB 75.30%
+// dynamic programming
+// O(N*S^3) O(NS)
+//   N: count of words
+//   S: avg length of each word
+class Solution {
+    private boolean concatenated(Set<String> wordSet, int minLen, String s) {
+        // base
+        if (wordSet.isEmpty()) {
+            return false;
+        }
+        // C[i]: substring whose length is i is concatenated?
+        boolean[] C = new boolean[s.length() + 1];
+        C[0] = true;
+        for (int i = 1; i <= s.length(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (C[j] && wordSet.contains(s.substring(j, i))) {
+                    C[i] = true;
+                    break;
+                }
+            }
+        }
+        return C[s.length()];
+    }
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        Set<String> wordSet = new HashSet<>();
+        int minLen = Math.max(1, words[0].length());
+        List<String> ans = new ArrayList<>();
+        for (String word : words) {
+            if (word.length() >= minLen*2 && concatenated(wordSet, minLen, word)) {
+                ans.add(word);
+            }
+            wordSet.add(word);
+        }
+        return ans;
+    }
+}
+
+// public class MainApp {
+//   public static void main(String[] args) {
+//       String[] words = {"cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"};
+//       Arrays.sort(words, (a, b) -> a.length() - b.length());
+//       for (String word : words) {
+//           System.out.println(word);
+//       }             
+//   }
+// }
--- leetcode2/ConcatenatedWords/README.md
+++ leetcode2/ConcatenatedWords/README.md
@@ -2,17 +2,13 @@
 
 [Concatenated Words](https://leetcode.com/problems/concatenated-words/)
 
-문자열의 모음 `words` 가 주어진다. `words` 를 구성하는 단어들 중 다른
-단어 두개 이상으로 조합된 단어들을 구하라.
+문자열의 모음 `words` 가 주어진다. `words` 를 구성하는 단어들 중 다른 단어 두개 이상으로 조합된 단어들을 구하라.
 
 # Idea
 
-예를 들어 `W =
-["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]`
-인 경우를 살펴보자.
+예를 들어 `W = ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]` 인 경우를 살펴보자.
 
-`unordered_set<string> wordSet` 를 선언하여 결합된 문자열을 구성할
-문자열들을 저장한다.
+`unordered_set<string> wordSet` 를 선언하여 결합된 문자열을 구성할 문자열들을 저장한다.
 
 먼저 `W` 를 길이의 오름차순으로 정렬한다.
 
@@ -20,16 +16,11 @@
 ["cat", "dog", "rat", "cats", "dogcatsdog", "catsdogcats", "ratcatdogcat", "hippopotamuses"]
 ```
 
-`int minLen` 을 선언하여 `W[0]` 의 길이를 저장한다. 이제 결합된
-문자열의 최소길이는 `minLen * 2` 보다 크거나 같아야 한다.
+`int minLen` 을 선언하여 `W[0]` 의 길이를 저장한다. 이제 결합된 문자열의 최소길이는 `minLen * 2` 보다 크거나 같아야 한다.
 
-인덱스 `i` 를 이용하여 `W` 를 처음부터 끝까지 순회하며 다음을
-반복한다.
+인덱스 `i` 를 이용하여 `W` 를 처음부터 끝까지 순회하며 다음을 반복한다.
 
-* 인덱스 `j` 를 이용하여 `W[i]` 를 `W[i..j), W[j..]` 형태로
-  나눈다. `W[i..j)` 가 `wordSet` 에 존재한다면 다시 `W[j..]` 를 비슷한
-  방법으로 `W[j..k), W[k..)` 으로 나누고 `W[j..k)` 가 `wordSet` 에
-  존재하는지 확인한다. 
+* 인덱스 `j` 를 이용하여 `W[i]` 를 `W[i..j), W[j..]` 형태로 나눈다. `W[i..j)` 가 `wordSet` 에 존재한다면 다시 `W[j..]` 를 비슷한 방법으로 `W[j..k), W[k..)` 으로 나누고 `W[j..k)` 가 `wordSet` 에 존재하는지 확인한다. 
   * [WordBreak](/leetcode2/WordBreak/README.md) 로 해결할 만 하다.
 
 # Implementation
