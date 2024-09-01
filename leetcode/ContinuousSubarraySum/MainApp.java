78a41437859136ae4e315e4c3e736c409e39ab47
David Sun <iamslash@gmail.com>
2024-09-01 10:29:58 +0900

    Accept ContinuousSubarraySum

--- leetcode/ContinuousSubarraySum/MainApp.java
+++ leetcode/ContinuousSubarraySum/MainApp.java
@@ -1,34 +1,40 @@
-// Copyright (C) 2021 by iamslash
+// Copyright (C) 2024 by iamslash
 
 import java.util.*;
 
-// 15ms 73.72% 55.4MB 66.50%
-// partital su, hash map
+// Idea: hash map
+//
+// S[i..j]: sum of nums[i]..nums[j]
+// S[i..j]: S[0..j] - S[0..i-1]
+// S[i..j] % k == 0 means
+//   (S[0..j] - S[0..i-1]) % k == 0
+//   S[0..j] % k == S[0..i-1] % k
+
+// 25ms 38.31% 57.2MB 93.79%
+// hash map
 // O(N) O(N)
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         int n = nums.length;
-        // { mode : index }
-        Map<Integer, Integer> modMap = new HashMap<>();
-        modMap.put(0, -1);
+
+        Map<Integer, Integer> modIdxMap = new HashMap<>();
+        modIdxMap.put(0, -1);
+
         for (int i = 0; i < n; ++i) {
             if (i > 0) {
                 nums[i] += nums[i-1];
             }
+
             int mod = nums[i] % k;
-            if (modMap.containsKey(mod)) {
-                if (i - modMap.get(mod) >= 2) {
+            if (modIdxMap.containsKey(mod)) {
+                if (i - modIdxMap.get(mod) >= 2) {
                     return true;
                 }
             } else {
-                modMap.put(mod, i);
+                modIdxMap.put(mod, i);
             }
         }
+
         return false;
     }
-}
-
-public class MainApp {
-  public static void main(String[] args) {
-  }
 }
