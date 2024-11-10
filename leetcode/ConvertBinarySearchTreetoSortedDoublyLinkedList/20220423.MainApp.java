1ce8e852e2583f476ab9b0c6145e7ab7aaef3ebe
David Sun <iamslash@gmail.com>
2024-11-10 09:03:39 +0900

    Accept ConvertBinarySearchTreetoSortedDoublyLinkedList

--- leetcode/ConvertBinarySearchTreetoSortedDoublyLinkedList/20220423.MainApp.java
+++ leetcode/ConvertBinarySearchTreetoSortedDoublyLinkedList/20220423.MainApp.java
@@ -1,19 +1,15 @@
-// Copyright (C) 2024 by iamslash
+// Copyright (C) 2022 by iamslash
 
 import java.util.*;
 
-// Definition for a Node.
 class Node {
     public int val;
     public Node left;
     public Node right;
-
     public Node() {}
-
     public Node(int _val) {
         val = _val;
     }
-
     public Node(int _val,Node _left,Node _right) {
         val = _val;
         left = _left;
@@ -21,44 +17,37 @@ class Node {
     }
 };
 
-// Idea: bst dfs
-//
-// - Define last
-// - Define head
-// - Traverse BST in-order
-// - Update head.right.left = last
-// - Update last.right = head.right
-// - Return head.right
-
-// 0ms 100.00% 42.3MB 7.99%
-// dfs
+// 0ms 100.00% 41.9MB 78.08%
+// in-order traversal, dummy node, prev node
 // O(N) O(lgN)
 class Solution {
-    private Node last;
-
+    private Node prev;
     private void dfs(Node u) {
+        // base
         if (u == null) {
             return;
         }
-
+        // recursion
         dfs(u.left);
-        last.right = u;
-        u.left = last;
-        last = u;
+        prev.right = u;
+        u.left = prev;
+        prev = u;
         dfs(u.right);
     }
-    
     public Node treeToDoublyList(Node root) {
         if (root == null) {
             return root;
         }
-
-        Node head = new Node();
-        last = head;
+        Node dumm = new Node();
+        prev = dumm;
         dfs(root);
-        head.right.left = last;
-        last.right = head.right;
-
-        return head.right;
+        dumm.right.left = prev;
+        prev.right = dumm.right;
+        return dumm.right;
     }
+}
+
+public class MainApp {
+  public static void main(String[] args) {
+  }
 }
--- leetcode/ConvertBinarySearchTreetoSortedDoublyLinkedList/README.md
+++ leetcode/ConvertBinarySearchTreetoSortedDoublyLinkedList/README.md
@@ -2,22 +2,17 @@
 
 [Convert Binary Search Tree to Sorted Doubly Linked List](https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/)
 
-Node `root` 가 주어진다. `root` 는 BST 의 root node 이다.  이 것을
-오름차순으로 정렬한 cirular doubly-linked list 로 변환하라.
+Node `root` 가 주어진다. `root` 는 BST 의 root node 이다.  이 것을 오름차순으로 정렬한 cirular doubly-linked list 로 변환하라.
 
 # Idea
 
 다음과 같은 것들을 생각해 보자.
 
-* BST 는 in-order traversal 하면 오름차순이 된다.
-* in-order traversal 할 때 마다 이번에 방문한 node 를 `prev` 에 저장해
-  보자. 모든 순회를 마치면 `prev` 는 마지막 node 가 저장되어 있다.
-* dummy node 를 만들어서 `dumm` 이라고 하자. `prev` 에 `dumm` 을 저장하고
-  in-order traversal 을 하자. 모든 순회를 마치면 `dumm.right` 은 list 의
-  첫 node 가 저장되어 있다.
+- BST 는 in-order traversal 하면 오름차순이 된다.
+- in-order traversal 할 때 마다 이번에 방문한 node 를 `prev` 에 저장해 보자. 모든 순회를 마치면 `prev` 는 마지막 node 가 저장되어 있다.
+- dummy node 를 만들어서 `dumm` 이라고 하자. `prev` 에 `dumm` 을 저장하고 in-order traversal 을 하자. 모든 순회를 마치면 `dumm.right` 은 list 의 첫 node 가 저장되어 있다.
 
-모든 반복을 마치고 `dumm.right` 과 `prev` 를 연결하면 circular
-doubly-linked list 로 변활할 수 있다.
+모든 반복을 마치고 `dumm.right` 과 `prev` 를 연결하면 circular doubly-linked list 로 변활할 수 있다.
 
 # Keyword
 
@@ -28,7 +23,7 @@ in-order, dummy node, prev node
 # Implementation
 
 * [c++11](a.cpp)
-* [java8](MainApp.java)
+* [java17](MainApp.java)
 
 # Complexity
 
