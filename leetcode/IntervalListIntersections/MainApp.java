d156ea8c3836b1fdebae506699d621af40efdb28
David Sun <iamslash@gmail.com>
2024-11-10 09:21:26 +0900

    Accept IntervalListIntersections

--- leetcode/IntervalListIntersections/MainApp.java
+++ leetcode/IntervalListIntersections/MainApp.java
@@ -1,42 +1,41 @@
-// Copyright (C) 2021 by iamslash
+// Copyright (C) 2024 by iamslash
 
 import java.util.*;
 
-// ____
-//  ____
 //
-// ____
-//      ____
+//  firstList: 0,2 5,10 13,23 24,25
+//                                  i
+// secondList: 1,5 8,12 15,24 25,26
+//                                j
 //
-// ____
-//  __
+//     begMax:     1 5  8 13 15 24 25
+//     endMin:     2 5 10 12 23 24 25
+//                 . .  .     .  .  .
 
-// 6ms 29.53% 51.8MB 8.84%
-// brute force
+// 3ms 98.63% 45.4MB 81.15%
+// two pointers
 // O(N) O(N)
 class Solution {
-	public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-		List<int[]> ans = new ArrayList<>();
-		int m = firstList.length, n = secondList.length, i = 0, j = 0;
-		while (i < m && j < n) {
-			int[] first = firstList[i];
-			int[] second = secondList[j];
-			int x = Math.max(first[0], second[0]);
-			int y = Math.min(first[1], second[1]);
-			if (x <= y) {
-				ans.add(new int[]{x, y});
-			}
-			if (first[1] < second[1]) {
-				++i;
-			} else {
-				++j;
-			}		 
-		}		
-		return ans.toArray(new int[0][]);
-	}
-}
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        List<int[]> mergedList = new ArrayList<>();
+        int m = firstList.length, n = secondList.length;
+        int i = 0, j = 0;
 
-public class MainApp {
-  public static void main(String[] args) {
-  }
+        while (i < m && j < n) {
+            int[] first = firstList[i], second = secondList[j];
+            int begMax = Math.max(first[0], second[0]);
+            int endMin = Math.min(first[1], second[1]);
+            if (begMax <= endMin) {
+                mergedList.add(new int[]{begMax, endMin});
+            }
+
+            if (first[1] <= second[1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return mergedList.toArray(new int[0][]);
+    }
 }
