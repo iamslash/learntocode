56badd615f487c92420ee27c5efdfcd1ba30b176
David Sun <iamslash@gmail.com>
2025-03-22 07:53:54 +0900

    Accept MaximumPointsTouristCanEarn

--- leetcode4/MaximumPointsTouristCanEarn/MainApp.java
+++ leetcode4/MaximumPointsTouristCanEarn/MainApp.java
@@ -74,3 +74,38 @@ class Solution {
         return ans;
     }
 }
+
+// 60ms 97.80% 55.50MB 35.16%
+// iterative dyanmic programming
+// O(NK) O(NK)
+class Solution {
+
+    public int maxScore(int n, int k, int[][] stayScore,
+                        int[][] travelScore) {
+        int[] curr = new int[n];
+        int[] prev = new int[n];
+
+        for (int day = k - 1; day >= 0; --day) {
+            for (int city = 0; city < n; ++city) {
+                int move = 0;
+                for (int i = 0; i < n; ++i) {
+                    move = Math.max(move, prev[i] + travelScore[city][i]);
+                }
+                int stay = Math.max(curr[city], prev[city] +
+                                    stayScore[day][city]);
+                curr[city] = Math.max(move, stay);
+            }
+            int[] temp = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        int maxScore = 0;
+        for (int score : prev) {
+            maxScore = Math.max(maxScore, score);
+        }
+
+        return maxScore;
+    }
+
+}
